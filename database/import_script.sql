f11a7fd4352df02bd64cbab44a8a653459c33efc
Stefan Nicula <stefanicu@gmail.com>
2024-01-18 23:56:40 +0200

    Update import_script.sql

--- database/import_script.sql
+++ database/import_script.sql
@@ -1,52 +1,57 @@
 USE minex_live;
 
 # applications import
-DELETE FROM minexgroup.applications;
-ALTER TABLE minexgroup.applications AUTO_INCREMENT = 1;
+DELETE FROM minexq.applications;
+ALTER TABLE minexq.applications AUTO_INCREMENT = 1;
 
-INSERT INTO minexgroup.applications(old_id, online, language, name, slug, image)
+INSERT INTO minexq.applications(oldid, online, language, name, slug, oldimage)
 SELECT * FROM (
-    SELECT id AS old_id, IF(online > 0, 1, 0) AS online, 'ro' AS language, nume AS name, slug, img AS image FROM `aplicatii` WHERE del='0' and nume!=''
+    SELECT id AS oldid, IF(online > 0, 1, 0) AS online, 'ro' AS language, nume AS name, slug, img AS oldimage FROM `aplicatii` WHERE del='0' and nume!=''
         UNION
-    SELECT id AS old_id, IF(online > 0, 1, 0) AS online, 'en' AS language, nume1 AS name, slug1 AS slug, img AS image FROM `aplicatii` WHERE del='0' and nume1!=''
+    SELECT id AS oldid, IF(online > 0, 1, 0) AS online, 'en' AS language, nume1 AS name, slug1 AS slug, img AS oldimage FROM `aplicatii` WHERE del='0' and nume1!=''
         UNION
-    SELECT id AS old_id, IF(online > 0, 1, 0) AS online, 'bg' AS language, nume2 AS name, slug2 AS slug, img AS image FROM `aplicatii` WHERE del='0' and nume2!=''
+    SELECT id AS oldid, IF(online > 0, 1, 0) AS online, 'bg' AS language, nume2 AS name, slug2 AS slug, img AS oldimage FROM `aplicatii` WHERE del='0' and nume2!=''
 ) AS applications;
 
 
 # categories import
-DELETE FROM minexgroup.categories;
-ALTER TABLE minexgroup.categories AUTO_INCREMENT = 1;
+DELETE FROM minexq.categories;
+ALTER TABLE minexq.categories AUTO_INCREMENT = 1;
 
-INSERT INTO minexgroup.categories(old_id, group_id, online, language, name, slug, image, product_id, product_image, page_views)
+INSERT INTO minexq.categories(online, language, name, slug, oldid, oldgroupid, oldimage, oldproductid, oldproductimg, page_views)
 SELECT * FROM (
-    SELECT id AS old_id, id_gru AS group_id, IF(online > 0, 1, 0) AS online, 'ro' AS language, nume AS name, slug, img_xl AS image, id_pro AS product_id, img_pro AS product_image, viz AS page_views
+    SELECT id AS oldid, id_gru AS oldgroupid, IF(online > 0, 1, 0) AS online, 'ro' AS language, nume AS name, slug, img_xl AS oldimage, id_pro AS oldproductid, img_pro AS oldproductimg, viz AS page_views
     FROM `categorii` WHERE del='0' and nume!=''
         UNION
-    SELECT id AS old_id, id_gru AS group_id, IF(online > 0, 1, 0) AS online, 'en' AS language, nume1 AS name, slug1 AS slug, img_xl AS image, id_pro AS product_id, img_pro AS product_image, viz AS page_views
+    SELECT id AS oldid, id_gru AS oldgroupid, IF(online > 0, 1, 0) AS online, 'en' AS language, nume1 AS name, slug1 AS slug, img_xl AS oldimage, id_pro AS oldproductid, img_pro AS oldproductimg, viz AS page_views
     FROM `categorii` WHERE del='0' and nume1!=''
         UNION
-    SELECT id AS old_id, id_gru AS group_id, IF(online > 0, 1, 0) AS online, 'bg' AS language, nume2 AS name, slug2 AS slug, img_xl AS image, id_pro AS product_id, img_pro AS product_image, viz AS page_views
+    SELECT id AS oldid, id_gru AS oldgroupid, IF(online > 0, 1, 0) AS online, 'bg' AS language, nume2 AS name, slug2 AS slug, img_xl AS oldimage, id_pro AS oldproductid, img_pro AS oldproductimg, viz AS page_views
     FROM `categorii` WHERE del='0' and nume2!=''
 ) AS categories;
 
 
 # application_category import in apl_cat, process, delete apl_cat
-DELETE FROM minexgroup.apl_cat;
+USE minexq;
+CREATE TABLE `apl_cat` (
+    `id_apl` bigint NOT NULL,
+    `id_cat` bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
-INSERT INTO minexgroup.apl_cat(id_apl,id_cat)
+INSERT INTO minexq.apl_cat(id_apl,id_cat)
 SELECT * FROM (
     SELECT id_apl, id_cat FROM `apl_ind_cat` WHERE 1 GROUP BY id_apl, id_cat
 ) AS apl_cat;
 
-USE minexgroup;
-DELETE FROM minexgroup.application_category;
-INSERT INTO minexgroup.application_category(category_id,application_id)
+USE minexq;
+DELETE FROM minexq.application_category;
+INSERT INTO minexq.application_category(category_id,application_id)
 SELECT * FROM (
     SELECT c.id AS category_id, a.id AS application_id FROM `apl_cat` ac
-        INNER JOIN applications a on ac.id_apl = a.old_id
-        INNER JOIN categories c on ac.id_cat = c.old_id AND c.language = a.language
+        INNER JOIN applications a on ac.id_apl = a.oldid
+        INNER JOIN categories c on ac.id_cat = c.oldid AND c.language = a.language
 ) AS application_category;
+DROP TABLE minexq.apl_cat;
 
 
 USE minex_live;
