bac3f23adfd526b4fea9f4c95e6b06388ca2e724
Stefan Nicula <stefanicu@gmail.com>
2024-01-20 02:17:52 +0200

    Update import_script.sql

--- database/import_script.sql
+++ database/import_script.sql
@@ -50,3 +50,32 @@ SELECT * FROM (
         INNER JOIN minexq.categories c on ac.id_cat = c.oldid AND c.language = a.language
 ) AS application_category;
 DROP TABLE IF EXISTS minexq.apl_cat;
+
+
+# blogs import
+DELETE FROM minexq.blogs;
+ALTER TABLE minexq.blogs AUTO_INCREMENT = 1;
+
+INSERT INTO minexq.blogs(online, language, name, slug, content, created_at, oldid, oldimage)
+SELECT * FROM (
+    SELECT IF(online > 0, 1, 0) AS online, 'ro' AS language, nume AS name, slug, content, dt as created_at, id AS oldid, img AS oldimage
+    FROM minex_live.iqindustrial WHERE del='0' and nume!=''
+        UNION
+    SELECT IF(online > 0, 1, 0) AS online, 'en' AS language, nume1 AS name, slug1 AS slug, content1 as content, dt as created_at, id AS oldid, img AS oldimage
+    FROM minex_live.iqindustrial WHERE del='0' and nume1!=''
+        UNION
+    SELECT IF(online > 0, 1, 0) AS online, 'bg' AS language, nume2 AS name, slug2 AS slug, content2 as content, dt as created_at, id AS oldid, img AS oldimage
+    FROM minex_live.iqindustrial WHERE del='0' and nume2!=''
+
+        UNION
+
+    SELECT IF(online > 0, 1, 0) AS online, 'ro' AS language, nume AS name, slug, content, dt as created_at, id AS oldid, img AS oldimage
+    FROM minex_live.noutati WHERE del='0' and nume!=''
+        UNION
+    SELECT IF(online > 0, 1, 0) AS online, 'en' AS language, nume1 AS name, slug1 AS slug, content1 as content, dt as created_at, id AS oldid, img AS oldimage
+    FROM minex_live.noutati WHERE del='0' and nume1!=''
+        UNION
+    SELECT IF(online > 0, 1, 0) AS online, 'bg' AS language, nume2 AS name, slug2 AS slug, content2 as content, dt as created_at, id AS oldid, img AS oldimage
+    FROM minex_live.noutati WHERE del='0' and nume2!=''
+) AS blogs;
+
