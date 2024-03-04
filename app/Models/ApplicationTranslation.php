e1dd267f42a403e9efb7abf59f6a58b92aff919b
Stefan Nicula <stefanicu@gmail.com>
2024-03-04 14:46:47 +0200

    Create ApplicationTranslation.php

---
+++ app/Models/ApplicationTranslation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ApplicationTranslation extends Model
+{
+    public $timestamps = false;
+    protected $fillable = ['online','name','slug'];
+}
