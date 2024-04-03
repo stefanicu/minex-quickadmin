521aae669a80b04e31e8700344ca7a49826df62d
Stefan Nicula <stefanicu@gmail.com>
2024-04-04 00:47:52 +0300

    Update brands.sql

--- database/import_scripts/brands.sql
+++ database/import_scripts/brands.sql
@@ -6,4 +6,4 @@ INSERT INTO minexq.brands(id, online, name, slug, oldid, oldimage)
 SELECT * FROM (
     SELECT id, IF(online > 0, 1, 0) AS online, trim(nume) AS name, trim(slug), id AS oldid, img AS oldimage
     FROM minex_live.branduri WHERE del='0' and nume!=''
-) AS applications;
+) AS brands;
