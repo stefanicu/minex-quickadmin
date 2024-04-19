d1c8fd0f63384ca4def159aafeb8c692412eb45b
Stefan Nicula <stefanicu@gmail.com>
2024-04-20 02:01:49 +0300

    Create blog.blade.php

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.frontend')
+@section('content')
+
+    <h1>BLOG</h1>
+
+@endsection
+@section('scripts')
+    @parent
+    <script src="({{ asset('/js/validate/jquery.validate.min.js') }}))"></script>
+    <script src="{{ asset('/js/validate/set.validate.js') }}"></script>
+    <script src="<{{ asset('/js/countries.js') }}"></script>
+    <script>populateCountries("tara", "jud");</script>
+    <script async defer src="//maps.googleapis.com/maps/api/js?key=AIzaSyCRHi8eiqWm--iQQ-fNTq3AWKev7xCj2RA&callback=initialize"></script>
+    <script src="{{ asset('/js/map.js?v=274625') }}"></script>
+@endsection
