9e937fc97a277406fd28300850270aabaab45f97
Kopa78 <eugen.kovacs@gmail.com>
2024-04-21 03:16:18 +0300

    Update form.blade.php

--- resources/views/partials/form.blade.php
+++ resources/views/partials/form.blade.php
@@ -1,80 +1,80 @@
 <div class="form-row">
 	<div class="col-12 col-md-6 pb-3">
-		<label class="form-label" for="nume"><?=$fnume;?></label>
-		<input class="form-control" type="text" id="nume" name="nume" placeholder="<?=$fnume;?>" autocomplete="off">
+		<label class="form-label" for="name">{{ trans('form.field_name') }}</label>
+		<input class="form-control" type="text" id="name" name="name" placeholder="{{ trans('form.field_name') }}" autocomplete="off">
 	</div>
 	<div class="col-12 col-md-6 pb-3">
-		<label class="form-label" for="pren"><?=$fprenume;?></label>
-		<input class="form-control" type="text" id="pren" name="pren" placeholder="<?=$fprenume;?>" autocomplete="off">
-	</div>
-</div>
-<div class="form-row">
-	<div class="col-12 col-md-6 pb-3">
-		<label class="form-label" for="companie"><?=$fcompanie;?></label>
-		<input class="form-control" type="text" id="companie" name="companie" placeholder="<?=$fcompanie;?>">
-	</div>
-	<div class="col-12 col-md-6 pb-3">
-		<label class="form-label" for="func"><?=$ffunctie;?></label>
-		<input class="form-control" type="text" id="func" name="func" placeholder="<?=$ffunctie;?>" autocomplete="off">
+		<label class="form-label" for="surname">{{ trans('form.field_surname') }}</label>
+		<input class="form-control" type="text" id="surname" name="surname" placeholder="{{ trans('form.field_surname') }}" autocomplete="off">
 	</div>
 </div>
 <div class="form-row">
 	<div class="col-12 col-md-6 pb-3">
-		<label class="form-label" for="dom"><?=$fdomeniu;?></label>
-		<input class="form-control" type="text" id="dom" name="dom" placeholder="<?=$fdomeniu;?>" autocomplete="off">
+		<label class="form-label" for="company">{{ trans('form.field_company') }}</label>
+		<input class="form-control" type="text" id="company" name="company" placeholder="{{ trans('form.field_company') }}">
 	</div>
 	<div class="col-12 col-md-6 pb-3">
-		<label class="form-label" for="email"><?=$femail;?></label>
-		<input class="form-control" type="text" id="email" name="email" placeholder="<?=$femail;?>" autocomplete="off">
+		<label class="form-label" for="job">{{ trans('form.field_job') }}</label>
+		<input class="form-control" type="text" id="job" name="job" placeholder="{{ trans('form.field_job') }}" autocomplete="off">
 	</div>
 </div>
 <div class="form-row">
 	<div class="col-12 col-md-6 pb-3">
-		<label class="form-label" for="email"><?=$ftara;?></label>
-		<select class="form-control" id="tara" name="tara" required>
-			<option value="--" disabled selected><?=$ftara;?></option>
+		<label class="form-label" for="industry">{{ trans('form.field_industry') }}</label>
+		<input class="form-control" type="text" id="industry" name="industry" placeholder="{{ trans('form.field_industry') }}" autocomplete="off">
+	</div>
+	<div class="col-12 col-md-6 pb-3">
+		<label class="form-label" for="email">{{ trans('form.field_email') }}</label>
+		<input class="form-control" type="text" id="email" name="email" placeholder="{{ trans('form.field_email') }}" autocomplete="off">
+	</div>
+</div>
+<div class="form-row">
+	<div class="col-12 col-md-6 pb-3">
+		<label class="form-label" for="district">{{ trans('form.field_district') }}</label>
+		<select class="form-control" id="district" name="district" required>
+			<option value="--" disabled selected>{{ trans('form.field_district') }}</option>
 		</select>
 	</div>
 	<div class="col-12 col-md-6 pb-3">
-		<label class="form-label" for="email"><?=$fjudet;?></label>
-		<select class="form-control" id="jud" name="jud" required>
-			<option value="" disabled selected><?=$fjudet;?></option>
+		<label class="form-label" for="county">{{ trans('form.field_county') }}</label>
+		<select class="form-control" id="county" name="county" required>
+			<option value="" disabled selected>{{ trans('form.field_county') }}</option>
 		</select>
 	</div>
 </div>
 <div class="form-row">
 	<div class="col-12 col-md-6 pb-3">
-		<label class="form-label" for="loca"><?=$flocalitate;?>address</label>
-		<input class="form-control" type="text" id="loca" name="loca" placeholder="<?=$flocalitate;?>">
+		<label class="form-label" for="city">{{ trans('form.field_city') }}address</label>
+		<input class="form-control" type="text" id="city" name="city" placeholder="{{ trans('form.field_city') }}">
 	</div>
 	<div class="col-12 col-md-6 pb-3">
-		<label class="form-label" for="tel"><?=$ftelefon;?></label>
-		<input class="form-control" type="text" id="tel" name="tel" placeholder="<?=$ftelefon;?>">
+		<label class="form-label" for="phone">{{ trans('form.field_phone') }}</label>
+		<input class="form-control" type="text" id="phone" name="phone" placeholder="{{ trans('form.field_phone') }}">
 	</div>
 </div>
 
 <div class="form-row pb-3">
 	<div class="col">
-		<label class="form-label" for="cumati"><?=$fcumatiaflat;?></label>
-		<input class="form-control" type="text" id="cumati" name="cumati" placeholder="<?=$fcumatiaflat;?>" autocomplete="off">
-		<input class="form-control" type="hidden" id="country" name="country" autocomplete="off">
+		<label class="form-label" for="how_about">{{ trans('form.field_how_about') }}</label>
+		<input class="form-control" type="text" id="how_about" name="how_about" placeholder="{{ trans('form.field_how_about') }}" autocomplete="off">
+		<input class="form-control" type="hidden" id="district" name="district" autocomplete="off">
 	</div>
 </div>
 
 <div class="form-row pb-3">
 	<div class="col">
-		<label class="form-label" for="mesaj"><?=$fdetalii;?></label>
-		<textarea class="form-control" rows="3" id="mesaj" name="mesaj" placeholder="<?=$fdetalii;?>"></textarea>
+		<label class="form-label" for="message">{{ trans('form.field_messages') }}</label>
+		<textarea class="form-control" rows="3" id="message" name="message" placeholder="{{ trans('form.field_messages') }}"></textarea>
 	</div>
 </div>
 <div class="form-row pb-3">
 	<div class="col">
 		<div class="p-2 check-bg">
 			<div class="form-check form-check-inline">
-				<label class="form-check-label form-newsletter">						
-					<input class="form-check-input form-checkbox-min" type="checkbox" id="newsletter" name="newsletter" checked>
-					<?=$fnewsletter;?>
-					<a href="<?=THEMEURL;?>gdpr" target="_blank"><strong><?=$gdpr_conf;?></strong></a>
+				<label class="form-check-label form-newsletter">
+					<input class="form-check-input form-checkbox-min" type="checkbox" id="checkbox" name="checkbox" checked>
+					{{ trans('form.field_checkbox') }}
+					<a href="{{ url('') }}/gdpr/" target="_blank"><strong>{{ trans('frontend.gdpr_compliance') }}</strong></a>
 				</label>
 			</div>
 		</div>
