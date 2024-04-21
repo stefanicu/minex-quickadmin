7357743eabfa99d72161d1af13052a2f47502af2
Kopa78 <eugen.kovacs@gmail.com>
2024-04-21 03:16:10 +0300

    Update contact_us.blade.php

--- resources/views/home/contact_us.blade.php
+++ resources/views/home/contact_us.blade.php
@@ -1,7 +1,7 @@
 <section class="container-fluid section--contact py-4 section3">
     <div class="row justify-content-center">
         <div class="col-10">
-            <h2 class="h1 px-0 px-md-4">{{ trans('frontend.contact_us') }}</h2>
+            <h2 class="h1 px-0 px-md-4">{{ $contact_us->name }}</h2>
             <a name="contact" id="contact"></a>
         </div>
     </div>
@@ -12,22 +12,23 @@
     <div class="row justify-content-center">
         <div class="col-12 col-lg-5">
             <form class="px-0 px-md-4" id="contactForm" autocomplete="off" action="{{ url('') }}/contact/'" method="post">
+
                 @include('.partials.form')
+
                 <div class="row">
                     <div class="col-xs-12 col-sm-12">
                         <div class="form-group captcha-error">
 
                             <input type="hidden" id="ip" name="ip" value="<?=$_SERVER['REMOTE_ADDR']; ?>">
 
-                            <?php //if(ENVIRONMENT == 'production'): ?>
+                            @if(app()->isProduction())
                                 <input type="hidden" class="hiddenRecaptcha form-control" name="hiddenRecaptcha" id="hiddenRecaptcha">
-                                <!--<div class="g-recaptcha" data-sitekey="6Le4vigTAAAAAA2rn1tgsj056Bevy-TThTWdMUCp"></div>-->
                                 <div class="g-recaptcha" data-sitekey="6Lc-mScTAAAAAIoo8LHTWRuJhPUXdDFJoGV4ptBg"></div>
-                            <?php //endif; ?>
+                            @endif
                         </div>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary btn-lg btn-block"><?php //echo $buton_formular; ?></button>
+                <button type="submit" class="btn btn-primary btn-lg btn-block">{{ $contact_us->button }}</button>
             </form>
         </div>
         <div class="col-12 col-lg-5">
@@ -35,7 +36,7 @@
             <div id="map"></div>
         </div>
         <div class="col-12 pt-4 text-center address">
-            <p class="mb-0"><?php //echo $text_formular ?></p>
+            <p class="mb-0">{!! $contact_us->first_text !!}</p>
         </div>
     </div>
 @endif
