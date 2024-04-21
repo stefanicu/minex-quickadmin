ede9b25c8eac54ad559d4061405e7813e49a4945
Kopa78 <eugen.kovacs@gmail.com>
2024-04-21 03:15:55 +0300

    Update frontend.php

--- resources/lang/ro/frontend.php
+++ resources/lang/ro/frontend.php
@@ -5,6 +5,5 @@ return [
     'copyright'             => 'Copyright Minex Group',
     'all_rights_reserved'   => 'Toate drepturile rezervate',
     'see_what_we_have_done' => 'Vezi ce am facut pentru',
-    'contact_us'            => 'Formular contact',
     'contact_thank_you'     => 'Mesajul dumneavoastra a fost trimis cu succes, va multumim!',
 ];
