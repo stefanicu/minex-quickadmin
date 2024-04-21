d59c8101e8d0e2faf4ba3d07aca2287fa35720f5
Kopa78 <eugen.kovacs@gmail.com>
2024-04-21 03:15:10 +0300

    Update frontend.php

--- resources/lang/bg/frontend.php
+++ resources/lang/bg/frontend.php
@@ -5,6 +5,5 @@ return [
     'copyright'             => 'Авторско право Minex Group',
     'all_rights_reserved'   => 'Всички права запазени',
     'see_what_we_have_done' => 'Вижте какво сме направили за',
-    'contact_us'            => 'Свържете се с нас',
     'contact_thank_you'     => 'Вашето съобщение беше изпратено успешно, благодаря!',
 ];
