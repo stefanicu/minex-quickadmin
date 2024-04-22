887b7900b2337eed59ea2ed00f4e9e8492f8cd35
Stefan Nicula <stefanicu@gmail.com>
2024-04-23 00:27:45 +0300

    Update contacts.sql

--- database/import_scripts/contacts.sql
+++ database/import_scripts/contacts.sql
@@ -1,8 +1,8 @@
 #contacts import
-DELETE FROM minexq.contacts;
-ALTER TABLE minexq.contacts AUTO_INCREMENT = 1;
+DELETE FROM $minexq.contacts;
+ALTER TABLE $minexq.contacts AUTO_INCREMENT = 1;
 
-INSERT INTO minexq.contacts(id, name, surname, email, job, industry, how_about, message, company, phone, country, county, city, checkbox, product, created_at)
+INSERT INTO $minexq.contacts(id, name, surname, email, job, industry, how_about, message, company, phone, country, county, city, checkbox, product, created_at)
 SELECT * FROM (
     SELECT
         id,
@@ -21,5 +21,5 @@ SELECT * FROM (
         acord AS checkbox,
         id_pro AS product,
         datai AS created_at
-    FROM minex_live.contact WHERE del='0'
+    FROM $minex_live.contact WHERE del='0'
 ) AS contacts;
