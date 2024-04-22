e625ebe77fe76194179e348cb03c14935ea49807
Stefan Nicula <stefanicu@gmail.com>
2024-04-23 00:27:40 +0300

    Update brands.sql

--- database/import_scripts/brands.sql
+++ database/import_scripts/brands.sql
@@ -1,9 +1,9 @@
 #brands import
-DELETE FROM minexq.brands;
-ALTER TABLE minexq.brands AUTO_INCREMENT = 1;
+DELETE FROM $minexq.brands;
+ALTER TABLE $minexq.brands AUTO_INCREMENT = 1;
 
-INSERT INTO minexq.brands(id, online, name, slug, oldid, oldimage)
+INSERT INTO $minexq.brands(id, online, name, slug, oldid, oldimage)
 SELECT * FROM (
     SELECT id, IF(online > 0, 1, 0) AS online, trim(nume) AS name, trim(slug), id AS oldid, img AS oldimage
-    FROM minex_live.branduri WHERE del='0' and nume!=''
+    FROM $minex_live.branduri WHERE del='0' and nume!=''
 ) AS brands;
