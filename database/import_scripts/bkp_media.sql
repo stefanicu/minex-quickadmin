8b83fc026f35bd60609eac5cb108d04886c3cb62
Stefan Nicula <stefanicu@gmail.com>
2024-04-23 00:27:35 +0300

    Update bkp_media.sql

--- database/import_scripts/bkp_media.sql
+++ database/import_scripts/bkp_media.sql
@@ -1,4 +1,4 @@
 # Back Up Media table
-DROP TABLE IF EXISTS `minex_live.media_bkp`;
-CREATE TABLE minex_live.media_bkp AS SELECT * FROM minexq.media;
+DROP TABLE IF EXISTS $minex_live.media_bkp;
+CREATE TABLE $minex_live.media_bkp AS SELECT * FROM $minexq.media;
 
