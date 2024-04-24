bdaac96859fbadaa5c367262095cdbb4788def50
Stefan Nicula <stefanicu@gmail.com>
2024-04-24 03:20:17 +0300

    Create 2024_02_09_000026_create_contactspams_table.php

---
+++ database/migrations/2024_02_09_000026_create_contactspams_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactSpamsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('contact_spams', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email');
+            $table->string('job');
+            $table->string('industry');
+            $table->string('how_about');
+            $table->string('message');
+            $table->string('company')->nullable();
+            $table->string('phone');
+            $table->string('country');
+            $table->string('county');
+            $table->string('city')->nullable();
+            $table->string('checkbox')->nullable();
+            $table->integer('product')->nullable();
+            $table->string('ip')->nullable();;
+            $table->string('district');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+}
