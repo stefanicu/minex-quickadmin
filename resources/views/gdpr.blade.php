44aba01e8808d49ce009d615c0662a987465b222
Stefan Nicula <stefanicu@gmail.com>
2024-04-24 03:21:09 +0300

    Create gdpr.blade.php

---
+++ resources/views/gdpr.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.frontend')
+@section('content')
+
+    <div class="container">
+        <h1 class="py-4">{{ $blog->name }}</h1>
+        <div class="row">
+                <div class="col-12">{!! $blog->content !!}</div>
+        </div>
+    </div>
+
+@endsection
+@section('scripts')
+    @parent
+    <script src="({{ asset('/js/validate/jquery.validate.min.js') }}))"></script>
+    <script src="{{ asset('/js/validate/set.validate.js') }}"></script>
+    <script src="<{{ asset('/js/countries.js') }}"></script>
+    <script>populateCountries("country", "county");</script>
+    <script async defer src="//maps.googleapis.com/maps/api/js?key=AIzaSyCRHi8eiqWm--iQQ-fNTq3AWKev7xCj2RA&callback=initialize"></script>
+    <script src="{{ asset('/js/map.js?v=274625') }}"></script>
+@endsection
