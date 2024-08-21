2b617fd4ee5c69335d5f52817b1955b15fc5de22
Stefan Nicula <stefanicu@gmail.com>
2024-08-21 20:55:11 +0300

    Create ProductTranslation.php

---
+++ app/Models/ProductTranslation.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductTranslation extends Model
+{
+    public $timestamps = false;
+    protected $fillable = [
+        'online',
+        'name',
+        'slug',
+        'description',
+        'specifications',
+        'advantages',
+        'usages',
+        'accessories'
+    ];
+
+    public static $searchable = [
+        'name',
+        'slug',
+        'description',
+        'specifications',
+        'advantages',
+        'usages',
+        'accessories'
+    ];
+}
