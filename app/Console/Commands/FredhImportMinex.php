49e1062170b3be43c1259db652a5b8e48a204f62
Stefan Nicula <stefanicu@gmail.com>
2024-09-06 15:54:56 +0300

    Create FredhImportMinex.php

---
+++ app/Console/Commands/FredhImportMinex.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class FredhImportMinex extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'freshimport:minex';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import data from minex-live table with backup images. This table is need to be updated before to lunch in production.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->output = new BufferedOutput() ;//BufferedOutput;
+
+        $this->call('bkp:Media');
+        $clientid = $this->output->fetch();
+
+        $this->call('migrate:fresh');
+        $clientsecret = $this->output->fetch();
+
+        $this->call('db:seed');
+        $clientsecret = $this->output->fetch();
+
+        $this->call('import:minex');
+        $clientsecret = $this->output->fetch();
+
+    }
+}
