36d0fddefa5cd35cabcd1261f2edad73d2ba36aa
Stefan Nicula <stefanicu@gmail.com>
2024-09-09 01:28:11 +0300

    Create menu_slug.php

---
+++ resources/lang/bg/menu_slug.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'home'   => '',
+    'solutions' => 'application',
+    'brands' => 'partners',
+    'references' => 'references',
+    'testimonials' => 'testimonials',
+    'blog' => 'blog',
+    'contact' => '#contact',
+    'search' => 'search',
+];
