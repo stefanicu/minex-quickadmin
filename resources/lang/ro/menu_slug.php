d6ed230f525feb431de1013c40d0c600a475b1e6
Stefan Nicula <stefanicu@gmail.com>
2024-09-09 01:28:16 +0300

    Create menu_slug.php

---
+++ resources/lang/ro/menu_slug.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'home'   => '',
+    'solutions' => 'aplicatie',
+    'brands' => 'parteneri',
+    'references' => 'referinte',
+    'testimonials' => 'testimoniale',
+    'blog' => 'blog',
+    'contact' => '#contact',
+    'search' => 'search',
+];
