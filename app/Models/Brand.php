ee2e9fa4d96662cba87c8c506225d457dd004e0c
Stefan Nicula <stefanicu@gmail.com>
2024-10-04 17:27:10 +0300

    Update Brand.php

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -6,6 +6,7 @@ use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
@@ -49,8 +50,8 @@ class Brand extends Model implements HasMedia
 
     public function registerMediaConversions(Media $media = null): void
     {
-        $this->addMediaConversion('thumb')->fit('crop', 50, 50);
-        $this->addMediaConversion('preview')->fit('crop', 120, 120);
+        $this->addMediaConversion('thumb')->fit(Manipulations::FIT_MAX, 75)->optimize();
+        $this->addMediaConversion('preview')->fit(Manipulations::FIT_MAX, 120)->optimize();
     }
 
     public function brandProducts()
