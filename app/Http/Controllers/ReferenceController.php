abc11a30e227fe13d76ed1fbe0a140714a1f93c2
Stefan Nicula <stefanicu@gmail.com>
2024-11-08 17:37:06 +0200

    Create ReferenceController.php

---
+++ app/Http/Controllers/ReferenceController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Application;
+use App\Models\Product;
+use App\Models\Reference;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+class ReferenceController extends Controller
+{
+    public function index(Request $request)
+    {
+        $reference_id = Reference::whereTranslation('slug',$request->slug)->first()->id;
+
+        $reference = Reference::leftJoin('reference_translations','references.id', '=', 'reference_translations.reference_id')
+            ->select('references.id','reference_translations.name','reference_translations.slug','references.industries_id')
+            ->where('references.online','=',1)
+            ->where('reference_translations.online','=',1)
+            ->where('reference_translations.reference_id','=',$reference_id)
+            ->where('locale','=',app()->getLocale())
+            ->select(sprintf('%s.*', (new Reference)->table),'reference_translations.name as name','reference_translations.slug as slug')
+            ->first();
+
+        $references = Reference::leftJoin('reference_translations','references.id', '=', 'reference_translations.reference_id')
+            ->select('references.id','reference_translations.name','reference_translations.slug','references.industries_id')
+            ->where('references.online','=',1)
+            ->where('references.industries_id','=',$reference->industries_id)
+            ->where('references.id','<>',$reference->id)
+            ->where('reference_translations.online','=',1)
+            ->where('locale','=',app()->getLocale())
+            ->select(sprintf('%s.*', (new Reference)->table),'reference_translations.name as name','reference_translations.slug as slug')
+            ->orderBy('reference_translations.name')
+            ->get();
+
+        $products = Product::whereHas('references', function (Builder $query) use ($reference){
+                $query->where('reference_id', '=', $reference->id);
+            })
+            ->leftJoin('product_translations','products.id','=','product_translations.product_id')
+            ->where('products.online','=',1)
+            ->where('product_translations.online','=',1)
+            ->where('product_translations.locale','=',app()->getLocale())
+            ->select(sprintf('%s.*', (new Product)->table),'product_translations.name as name','product_translations.slug as slug')
+            ->get();
+
+        return view('reference', compact('references', 'reference', 'products'));
+    }
+}
