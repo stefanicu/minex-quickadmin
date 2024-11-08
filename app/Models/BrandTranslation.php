d78e4950cfd0fa0bc0a6a77992f9d0b38a81d735
Stefan Nicula <stefanicu@gmail.com>
2024-11-08 17:37:09 +0200

    Create BrandTranslation.php

---
+++ app/Models/BrandTranslation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BrandTranslation extends Model
+{
+    protected $table = 'brand_translations';
+
+    public $timestamps = false;
+    protected $fillable = ['online'];
+}
