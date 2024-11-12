b449b91e18ab7f274987d64d1a25ab2fff23c814
Stefan Nicula <stefanicu@gmail.com>
2024-11-12 22:00:20 +0200

    Update BrandsController.php

--- app/Http/Controllers/BrandsController.php
+++ app/Http/Controllers/BrandsController.php
@@ -9,9 +9,16 @@ class BrandsController extends Controller
 {
     public function index()
     {
-        $applications = Application::all();
-        $brands = Brand::select('id','name','slug')->where('online','=',1)->orderBy('name')->get();
+        $brands = Brand::leftJoin('brand_translations', 'brands.id', '=', 'brand_translations.brand_id')
+            ->leftJoin('products','brands.id','=','products.brand_id')
+            ->selectRAW('brands.id, brands.name, brands.slug, count(products.brand_id) as cnt_produse')
+            ->where('brands.online','=',1)
+            ->where('brand_translations.online','=',1)
+            ->groupByRaw('brands.id, brands.name, brands.slug')
+            ->orderBy('brands.name')
+            ->get();
 
-        return view('brands', compact('applications','brands'));
+
+        return view('brands', compact('brands'));
     }
 }
