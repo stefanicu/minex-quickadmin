78b81516923c9baf67084e26ae831b1559dbe7df
Stefan Nicula <stefanicu@gmail.com>
2024-11-12 22:00:25 +0200

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,30 +10,63 @@ class CategoryController extends Controller
 {
     public function index(Request $request)
     {
-        $category_slug = $request->slug;
+        $category = Category::whereTranslation('slug', $request->slug)->whereTranslation('locale',app()->getLocale())->first();
 
-        $application = Application::find($request->id);
-        $applicationId = $application->id;
+        session(['category_id' => $category->id]);
 
-        $category = Category::whereTranslation('slug', $category_slug)->whereTranslation('locale',app()->getLocale())->first();
+        $referrer = $request->headers->get('referer');
+
+        if(session()->has('application_id') && $referrer !== null)
+        {
+            $application = Application::where('id',session('application_id'))->with('translations')->first();
+        }
+        else
+        {
+            $application = Application::whereHas('products', function($query) use ($category) {
+                // Filter products that belong to the specified category
+                $query->whereHas('categories', function($query) use ($category) {
+                    $query->where('categories.id', $category->id);
+                });
+            })
+                ->with('translations') // Load translations for each application
+                ->first();
+        }
+
+        $currentLocale = app()->getLocale();
 
         $products = Category::find($category->id)
             ->products()
+            ->with('media')
+            ->whereHas('translations', function ($query) use ($currentLocale) {
+                $query->where('locale', $currentLocale);
+            })
             ->orderByTranslation('name')
             ->get();
 
-        $categories = Category::whereHas('products')
-            ->whereHas('applications', function($query) use ($applicationId) {
-                $query->where('applications.id', $applicationId);
+        $productIds = Application::find($application->id)
+            ->products()
+            ->whereHas('translations', function ($query) use ($currentLocale) {
+                $query->where('locale', $currentLocale);
             })
+            ->pluck('id')
+            ->toArray();
+
+        $categories = Category::whereHas('products', function ($query) use ($productIds, $currentLocale) {
+            $query->whereIn('products.id', $productIds);
+        })
+            ->with('product_main_image')
             ->orderByTranslation('name')
             ->withCount('products') // Adds a `products_count` attribute to each category
             ->get();
 
+        if($categories->count() === 0){
+            return redirect(url(''));
+        }
+
         if($products->count() == 1)
         {
             $product = $products->first();
-            return view('product', compact( 'category',  'product'));
+            return redirect(route('product.index', ['slug' => $product->slug]));
         }
 
         return view('category', compact( 'category', 'categories', 'products', 'application'));
