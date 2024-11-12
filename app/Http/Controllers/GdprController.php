3ba4ef2cc939cd1f133a4d850a8e3f8bd3255b96
Stefan Nicula <stefanicu@gmail.com>
2024-11-12 22:00:36 +0200

    Update GdprController.php

--- app/Http/Controllers/GdprController.php
+++ app/Http/Controllers/GdprController.php
@@ -9,12 +9,11 @@ class GdprController extends Controller
 {
     public function index()
     {
-        $applications = Application::all();
         $blog = BlogTranslation::select('blog_id','name','content')
             ->where('slug','like','%gdpr%')
             ->where('locale','=',app()->getLocale())
             ->first();
 
-        return view('gdpr', compact('applications','blog'));
+        return view('gdpr', compact('blog'));
     }
 }
