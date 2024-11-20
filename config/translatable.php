07c77969c38fd53285d75fa13c898a568f5a79e4
Stefan Nicula <stefanicu@gmail.com>
2024-11-21 01:55:05 +0200

    Update translatable.php

--- config/translatable.php
+++ config/translatable.php
@@ -40,7 +40,7 @@ return [
     | and never fallback to the translator one.
     |
     */
-    'locale' => 'en',
+    'locale' => null,
 
     /*
     |--------------------------------------------------------------------------
@@ -53,7 +53,7 @@ return [
     | $useTranslationFallback when defined
     |
     */
-    'use_fallback' => true,
+    'use_fallback' => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -65,7 +65,7 @@ return [
     | locale. Note that 'use_fallback' must be enabled.
     |
      */
-    'use_property_fallback' => true,
+    'use_property_fallback' => false,
 
     /*
     |--------------------------------------------------------------------------
