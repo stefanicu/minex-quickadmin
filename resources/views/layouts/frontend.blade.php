5c6c5a0b6238a0d554965b5840cdc00a444e2cff
Stefan Nicula <stefanicu@gmail.com>
2024-11-21 00:38:58 +0200

    Update frontend.blade.php

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -175,9 +175,6 @@ $page = '';
     <script src="{{ asset('js/hide-menu.js') }}"></script>
     <script>
         $(".fixed--top").autoHidingNavbar();
-        <?php if ($page == "referinte") : ?>
-            $(document).ready(() => { let url = location.href.replace(/\/$/, ""); if (location.hash) { const hash = url.split("#"); $('#myTab a[href="#'+hash[1]+'"]').tab("show"); url = location.href.replace(/\/#/, "#"); history.replaceState(null, null, url); setTimeout(() => { $(window).scrollTop(0); }, 400); } $('a[data-toggle="tab"]').on("click", function() { let newUrl; const hash = $(this).attr("href"); if(hash == "#home") { newUrl = url.split("#")[0]; } else { newUrl = url.split("#")[0] + hash; } newUrl += "/"; history.replaceState(null, null, newUrl); }); });
-        <?php endif;?>
     </script>
 
     @yield('scripts')
