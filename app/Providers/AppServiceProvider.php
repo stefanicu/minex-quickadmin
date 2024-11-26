7cfaeee3c6af0cab3ffa8cb2714f9e1595fefa25
Stefan Nicula <stefanicu@gmail.com>
2024-11-27 01:12:18 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Application;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrapFour();
+
         if ( ! request()->is('admin/*')) {
 
             // Share the applications globally for all views
