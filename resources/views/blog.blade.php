cd0c92738da0e60ab87bcf26d51b3a636e5942b5
Stefan Nicula <stefanicu@gmail.com>
2024-11-28 00:40:04 +0200

    Update blog.blade.php

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -29,7 +29,7 @@
 
     <div class="container content">
         <div class="row">
-            <div class="col-12 col-md-8">
+            <div class="col-12 col-lg-8 pb-4 mb-4">
                 <h1 class="h2">{{ $blog->name }}<br> <small class="data">{{ Carbon\Carbon::parse($blog->created_at)->format('d.m.Y') }}</small></h1>
 
                 @if($blog->getImageAttribute() !== null && $blog->getImageAttribute()->count()>0)
@@ -42,41 +42,62 @@
                 @endif
                 <hr>
                 {!! $blog->content !!}
-
-                <hr/>
-
+            </div>
+            <div class="d-none d-lg-block col-md-4 pb-4 mb-4">
+                <h2 class="h3">{{ trans('pages.articles') }}</h2>
+                <ul class="list-group">
+                    @foreach ($blogs10 as $bg)
+                            <li class="list-group-item flex-row {{ $blog->id === $bg->id ? 'active' : '' }}">
+                                <div class="d-flex w-100 justify-content-between">
+                                    @if($bg->getImageAttribute() !== null && $bg->getImageAttribute()->count()>0)
+                                        <div class="blog_image">
+                                            <img
+                                                srcset="{{ $bg->getImageAttribute()->getUrl('thumb') }}"
+                                                alt="{{ $bg->name }}"
+                                                class="lozad">
+                                        </div>
+                                    @endif
+                                    <a href="{{ route('blog.index', ['slug' => $bg->slug, 'page' => request()->get('page')]) }}" class="flex m-auto">
+                                        {{ $bg->name }}
+                                    </a>
+                                </div>
+                            </li>
+                    @endforeach
+                </ul>
+                {!! $blogs10->onEachSide(3)->links() !!}
             </div>
         </div>
     </div>
 
-<div class="container p-4 sm:p-4">
-    <div class="bxslider-related mx-auto">
-        @foreach ($blogs as $blg)
-            <div class="d-flex slider-image-container">
-                <a href="{{ url('') }}/{{ trans('pages_slugs.blog') }}/{{ $blg->slug }}" class="blogim position-relative">
-                    @if($blg->getImageAttribute() !== null && $blg->getImageAttribute()->count()>0)
-                        <img
-                            srcset="{{ $blg->getImageAttribute()->getUrl() }}"
-                            alt="{{ $blg->name }}"
-                            class="mx-auto lozad">
-                    @else
-                        <div class="blog_slider_image_default">No image</div>
-                    @endif
-                    <div class="card-img-overlay blogim_over">
-                        <p class="assets-title row-icons--desc position-absolute">{{ $blg->name }}</p>
-                    </div>
-                </a>
-            </div>
-        @endforeach
+    <div class="container d-block d-lg-none">
+        <hr/>
+        <div class="bxslider-related mx-auto pt-4">
+            @foreach ($blogs as $blg)
+                <div class="d-flex slider-image-container">
+                    <a href="{{ route('blog.index', ['slug' => $blg->slug, 'page' => request()->get('page')]) }}" class="blogim position-relative">
+                        @if($blg->getImageAttribute() !== null && $blg->getImageAttribute()->count()>0)
+                            <img
+                                srcset="{{ $blg->getImageAttribute()->getUrl() }}"
+                                alt="{{ $blg->name }}"
+                                class="mx-auto lozad">
+                        @else
+                            <div class="blog_slider_image_default">No image</div>
+                        @endif
+                        <div class="card-img-overlay blogim_over">
+                            <p class="assets-title row-icons--desc position-absolute">{{ $blg->name }}</p>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
+        </div>
     </div>
-</div>
 
 @endsection
 @section('scripts')
     @parent
     <script>
-        $(document).ready(function(){$(function(){$('.bxslider').bxSlider({mode:'fade',slideWidth:400});});
-            $(function(){$('.bxslider-related').bxSlider({minSlides:1,maxSlides:3,slideWidth:360,slideMargin:5,pager:false});});});
-        $(function(){$('.bxslider-img').bxSlider({mode:'fade',slideWidth:1110,pager:false,controls:true});});
+        $(document).ready(function(){$(function(){$('.bxslider').bxSlider({mode:'fade',slideWidth:345});});
+            $(function(){$('.bxslider-related').bxSlider({minSlides:1,maxSlides:2,slideWidth:345,infiniteLoop:true,slideMargin:5,pager:false});});});
+        $(function(){$('.bxslider-img').bxSlider({mode:'fade',slideWidth:345,pager:true,controls:true});});
     </script>
 @endsection
