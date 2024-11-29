aa8826c23d51587c3effcf1b05c030f4197dd2f8
Stefan Nicula <stefanicu@gmail.com>
2024-11-29 21:27:53 +0200

    Update frontend.php

--- resources/lang/bg/frontend.php
+++ resources/lang/bg/frontend.php
@@ -2,7 +2,7 @@
 
 return [
     'gdpr_compliance'       => 'Съответствие с GDPR',
-    'copyright'             => 'Авторско право Minex Group',
+    'copyright'             => 'Minex Group International',
     'all_rights_reserved'   => 'Всички права запазени',
     'see_what_we_have_done' => 'Вижте какво сме направили за',
     'contact_thank_you'     => 'Вашето съобщение беше изпратено успешно, благодаря!',
