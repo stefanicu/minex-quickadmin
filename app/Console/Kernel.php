d645320ee5e7a59607f4fd10d81f8813a2226ff1
Stefan Nicula <stefanicu@gmail.com>
2024-12-16 13:51:13 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,16 +12,16 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('sitemap:generate')->daily();
     }
-
+    
     /**
      * Register the commands for the application.
      */
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
-
+        
         require base_path('routes/console.php');
     }
 }
