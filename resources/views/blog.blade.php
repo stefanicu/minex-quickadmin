a4d742db0e9760ba715965c10a751b4de2c6b2e7
Stefan Nicula <stefanicu@gmail.com>
2024-12-20 01:26:05 +0200

    Update blog.blade.php

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.frontend')
 @section('content')
-{{--    @dd($blog, $blogs)--}}
+    {{--    @dd($blog, $blogs)--}}
 
 
     <div class="container-fluid cover p-0">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb mb-0">
                 <li class="breadcrumb-item"><a href="/">{{ trans('menu.home') }}</a></li>
-                <li class="breadcrumb-item"><a href="{{ url('') }}/{{ trans('pages_slugs.blog') }}/">{{ trans('pages.blog') }}</a></li>
+                <li class="breadcrumb-item"><a
+                            href="{{ url('') }}/{{ trans('pages_slugs.blog') }}/">{{ trans('pages.blog') }}</a></li>
                 <li class="breadcrumb-item active" aria-current="page">{{ $blog->name }}</li>
             </ol>
         </nav>
@@ -18,9 +19,12 @@
                 <source data-srcset="{{ asset('/img/headers/aplicatie-lg.jpg') }}" media="(min-width: 992px)">
                 <source data-srcset="{{ asset('/img/headers/aplicatie-md.jpg') }}" media="(min-width: 576px)">
                 <source data-srcset="{{ asset('/img/headers/aplicatie-sm.jpg') }}" media="(max-width: 576px)">
-                <img class="lozad img-fluid section--hero-img lazy-fade" srcset="{{ asset('/img/headers/aplicatie-xl.jpg') }}" alt="{{ trans('pages.references') }}" data-loaded="true">
+                <img class="lozad img-fluid section--hero-img lazy-fade"
+                     srcset="{{ asset('/img/headers/aplicatie-xl.jpg') }}" alt="{{ trans('pages.references') }}"
+                     data-loaded="true">
                 <noscript>
-                    <img class="img-fluid lozad" src="{{ asset('/img/headers/aplicatie-xl.jpg') }}" alt="{{ trans('pages.references') }}">
+                    <img class="img-fluid lozad" src="{{ asset('/img/headers/aplicatie-xl.jpg') }}"
+                         alt="{{ trans('pages.references') }}">
                 </noscript>
             </picture>
         </figure>
@@ -30,13 +34,14 @@
     <div class="container content">
         <div class="row">
             <div class="col-12 col-lg-8 pb-4 mb-4">
-                <h1 class="h2">{{ $blog->name }}<br> <small class="data">{{ Carbon\Carbon::parse($blog->created_at)->format('d.m.Y') }}</small></h1>
+                <h1 class="h2">{{ $blog->name }}<br> <small
+                            class="data">{{ Carbon\Carbon::parse($blog->created_at)->format('d.m.Y') }}</small></h1>
 
                 @if($blog->getImageAttribute() !== null && $blog->getImageAttribute()->count()>0)
                     <img
-                        srcset="{{ $blog->getImageAttribute()->getUrl() }}"
-                        alt="{{ $blog->name }}"
-                        class="img-fluid">
+                            srcset="{{ $blog->getImageAttribute()->getUrl() }}"
+                            alt="{{ $blog->name }}"
+                            class="img-fluid">
                 @else
                     <div class="blog_image_default">No image</div>
                 @endif
@@ -47,21 +52,22 @@
                 <h2 class="h3">{{ trans('pages.articles') }}</h2>
                 <ul class="list-group">
                     @foreach ($blogs10 as $bg)
-                            <li class="list-group-item flex-row {{ $blog->id === $bg->id ? 'active' : '' }}">
-                                <div class="d-flex w-100 justify-content-between">
-                                    @if($bg->getImageAttribute() !== null && $bg->getImageAttribute()->count()>0)
-                                        <div class="blog_image">
-                                            <img
+                        <li class="list-group-item flex-row {{ $blog->id === $bg->id ? 'active' : '' }}">
+                            <div class="d-flex w-100 justify-content-between">
+                                @if($bg->getImageAttribute() !== null && $bg->getImageAttribute()->count()>0)
+                                    <div class="blog_image">
+                                        <img
                                                 srcset="{{ $bg->getImageAttribute()->getUrl('thumb') }}"
                                                 alt="{{ $bg->name }}"
                                                 class="lozad">
-                                        </div>
-                                    @endif
-                                    <a href="{{ route('blog.index', ['slug' => $bg->slug, 'page' => request()->get('page')]) }}" class="flex m-auto">
-                                        {{ $bg->name }}
-                                    </a>
-                                </div>
-                            </li>
+                                    </div>
+                                @endif
+                                <a href="{{ route('blog.index', ['slug' => $bg->slug, 'page' => request()->get('page')]) }}"
+                                   class="flex m-auto">
+                                    {{ $bg->name }}
+                                </a>
+                            </div>
+                        </li>
                     @endforeach
                 </ul>
                 {!! $blogs10->onEachSide(3)->links() !!}
@@ -74,12 +80,13 @@
         <div class="bxslider-related mx-auto pt-4">
             @foreach ($blogs as $blg)
                 <div class="d-flex slider-image-container">
-                    <a href="{{ route('blog.index', ['slug' => $blg->slug, 'page' => request()->get('page')]) }}" class="blogim position-relative">
+                    <a href="{{ route('blog.index', ['slug' => $blg->slug, 'page' => request()->get('page')]) }}"
+                       class="blogim position-relative">
                         @if($blg->getImageAttribute() !== null && $blg->getImageAttribute()->count()>0)
                             <img
-                                srcset="{{ $blg->getImageAttribute()->getUrl() }}"
-                                alt="{{ $blg->name }}"
-                                class="mx-auto lozad">
+                                    srcset="{{ $blg->getImageAttribute()->getUrl() }}"
+                                    alt="{{ $blg->name }}"
+                                    class="mx-auto lozad">
                         @else
                             <div class="blog_slider_image_default">No image</div>
                         @endif
@@ -95,9 +102,5 @@
 @endsection
 @section('scripts')
     @parent
-    <script>
-        $(document).ready(function(){$(function(){$('.bxslider').bxSlider({mode:'fade',slideWidth:345});});
-            $(function(){$('.bxslider-related').bxSlider({minSlides:1,maxSlides:2,slideWidth:345,infiniteLoop:true,slideMargin:5,pager:false});});});
-        $(function(){$('.bxslider-img').bxSlider({mode:'fade',slideWidth:345,pager:true,controls:true});});
-    </script>
+    <script src="{{ asset('/js/bxSlider/blog_setup.js') }}"></script>
 @endsection
