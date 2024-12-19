6bafe7faf644dc41440276afcb3086e3bfa4f101
Stefan Nicula <stefanicu@gmail.com>
2024-12-20 01:25:35 +0200

    Create home_setup.js

---
+++ public/js/bxSlider/home_setup.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(function () {
+        $('.bxslider').bxSlider({mode: 'fade', slideWidth: 400});
+    });
+    $(function () {
+        $('.bxslider-related').bxSlider({
+            minSlides: 1,
+            maxSlides: 3,
+            slideWidth: 360,
+            slideMargin: 5,
+            pager: false
+        });
+    });
+});
+$(function () {
+    $('.bxslider-img').bxSlider({mode: 'fade', slideWidth: 1110, pager: false, controls: true});
+});
