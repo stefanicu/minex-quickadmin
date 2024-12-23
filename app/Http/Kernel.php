2f2ce3eec7a0800c94f269077a9eca4a8deec529
Stefan Nicula <stefanicu@gmail.com>
2024-12-23 20:50:43 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,6 +27,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'setFrontendLocale' => \App\Http\Middleware\SetFrontendLocale::class,
+        'setBackendLocale' => \App\Http\Middleware\SetBackendLocale::class,
     ];
     
     protected $middlewareGroups = [
@@ -38,7 +40,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\AuthGates::class,
-            \App\Http\Middleware\SetLocale::class,
+            \App\Http\Middleware\SecurityHeaders::class,
         ],
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
