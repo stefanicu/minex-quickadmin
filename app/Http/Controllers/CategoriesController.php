a7bea273d67217f821f983ee575dca511222dc3b
Stefan Nicula <stefanicu@gmail.com>
2024-12-23 04:06:43 +0200

    Update CategoriesController.php

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Application;
+use App\Models\ApplicationTranslation;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -11,40 +12,57 @@ class CategoriesController extends Controller
 {
     public function index(Request $request)
     {
-        $application_slug = $request->slug;
+        if ( ! $request->app_slug) {
+            abort(404);
+        }
+        
+        $currentLocale = app()->getLocale();
+        
+        $application_slug = $request->app_slug;
         
         $application = Application::whereTranslation('slug', $application_slug)->whereTranslation('locale',
-            app()->getLocale())->first();
+            $currentLocale)->first();
         
         if ( ! $application) {
             $application = Application::whereTranslation('slug', $application_slug)->whereTranslation('locale',
                 'en')->first();
         }
         
-        session(['application_id' => $application->id]);
+        $productIds = null;
+        $app_slugs = null;
+        if ($application) {
+            $productIds = Application::find($application->id)
+                ->products()
+                ->whereHas('translations', function ($query) use ($currentLocale) {
+                    $query->where('locale', $currentLocale);
+                })
+                ->pluck('products.id')
+                ->toArray();
+            
+            # add variable for language change
+            $app_slugs_query = ApplicationTranslation::where('application_id', $application->id)->select('locale',
+                'slug')->get();
+            $app_slugs = $app_slugs_query->keyBy('locale')->map(function ($item) {
+                return $item->slug;
+            });
+        }
         
-        $currentLocale = app()->getLocale();
-        
-        $productIds = Application::find($application->id)
-            ->products()
-            ->whereHas('translations', function ($query) use ($currentLocale) {
-                $query->where('locale', $currentLocale);
-            })
-            ->pluck('products.id')
-            ->toArray();
-        
-        $categories = Category::with('product_main_image', 'product_main_image.media')
-            ->whereHas('products', function ($query) use ($productIds) {
-                $query->whereIn('products.id', $productIds);
-            })
-            ->orderByTranslation('name')
-            ->get();
+        if ( ! $productIds) {
+            $categories = Category::with('product_main_image', 'product_main_image.media')
+                ->orderByTranslation('name')
+                ->get();
+        } else {
+            $categories = Category::with('product_main_image', 'product_main_image.media')
+                ->whereHas('products', function ($query) use ($productIds) {
+                    $query->whereIn('products.id', $productIds);
+                })
+                ->orderByTranslation('name')
+                ->get();
+        }
         
         if ($categories->count() == 1) {
             $category = $categories->first();
             $category_id = $category->id;
-            
-            session(['category_id' => $category->id]);
             
             $products = Product::with([
                 'translations' => function ($query) use ($currentLocale) {
@@ -59,14 +77,16 @@ class CategoriesController extends Controller
                 })
                 ->get();
             
-            if ($products->count() == 1) {
+            if ($products->count() === 1) {
                 $product = $products->first();
-                return redirect(route('product.index', ['slug' => $product->slug]));
+                return redirect(route('product.'.app()->getLocale(),
+                    ['app_slug' => $application->slug, 'cat_slug' => $category->slug, 'prod_slug' => $product->slug]));
             }
             
-            return redirect(route('category.index', ['slug' => $category->slug]));
+            return redirect(route('products.'.app()->getLocale(),
+                ['app_slug' => $application->slug, 'cat_slug' => $category->slug]));
         }
         
-        return view('categories', compact('application', 'categories'));
+        return view('categories', compact('application', 'categories', 'app_slugs'));
     }
 }
