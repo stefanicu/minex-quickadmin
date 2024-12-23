ad242319e318c553465f7dafe0410cca07eb4422
Stefan Nicula <stefanicu@gmail.com>
2024-12-23 20:51:29 +0200

    Update pages.php

--- resources/lang/bg/pages.php
+++ resources/lang/bg/pages.php
@@ -3,12 +3,12 @@
 return [
     'brand' => 'Партньор',
     'brands' => 'Партньори',
-
+    
     'search' => 'Search',
     'search_for' => 'Търсене на дума',
     'search_error' => 'Търсената дума трябва да има поне 3 знака',
     'search_not_found' => 'No results found',
-
+    
     'product' => 'Продукт',
     'products' => 'Продукти',
     'specifications' => 'Спецификации',
@@ -21,27 +21,29 @@ return [
     'contact_form' => 'Контакти',
     'send_form' => 'Формуляр за изпращане',
     'similar_products' => 'Продукти, които може да харесате',
-
+    
     'category' => 'Категория',
     'categories' => 'Категории',
     'chose_category' => 'Изберете категория',
+    'all_categories' => 'Всички категории',
+    
     'product_categories' => 'Продуктови категории',
     'product_applications' => 'Продуктови Приложения',
-
+    
     'reference' => 'Референц',
     'references' => 'Референции',
     'other_references' => 'Други референции',
-
+    
     'no_products' => 'В момента няма налични продукти',
-
+    
     'no_translated_title' => 'Тя не е преведена на български език',
     'no_translated_message' => 'За момента страницата не е преведена на български език. За повече информация, моля изпратете ни съобщение и ние ще се свържем с Вас възможно най-скоро.',
-
+    
     'no_brand_default_message' => 'Продуктът не се предлага във вашата страна.',
-
+    
     'testimonials' => 'Отзиви',
     'testimonial_question' => 'Какво казват клиентите за нас?',
-
+    
     'blog' => 'Блог',
     'articles' => 'Статии',
     'next' => 'Next',
