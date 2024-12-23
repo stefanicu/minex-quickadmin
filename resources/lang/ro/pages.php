83cacd5d33fe9378ddf149ca0e9a44a4acf9e3a0
Stefan Nicula <stefanicu@gmail.com>
2024-12-23 20:51:36 +0200

    Update pages.php

--- resources/lang/ro/pages.php
+++ resources/lang/ro/pages.php
@@ -3,13 +3,13 @@
 return [
     'brand' => 'Brand',
     'brands' => 'Branduri',
-
+    
     'search' => 'Search',
     'search_for' => 'Cautare dupa cuvantul',
     'search_error' => 'Cuvantul cautat trebuie sa aiba cel putin 3 caractere',
     'search_not_found' => 'No results found',
     'articles' => 'Articole',
-
+    
     'product' => 'Produs',
     'products' => 'Produse',
     'specifications' => 'Specificatii',
@@ -22,27 +22,29 @@ return [
     'contact_form' => 'Formular de Contact',
     'send_form' => 'Trimite Formularul',
     'similar_products' => 'Alte produse care te-ar putea interesa',
-
+    
     'category' => 'Categorie',
     'categories' => 'Categorii',
     'chose_category' => 'Alege o categorie',
+    'all_categories' => 'Toate Categoriile',
+    
     'product_categories' => 'Categorii Produse',
     'product_applications' => 'Aplicatii Produse',
-
+    
     'reference' => 'Referinta',
     'references' => 'Referinte',
     'other_references' => 'Alte referinte',
-
+    
     'no_products' => 'Nu sunt produse disponibile',
-
+    
     'no_translated_title' => 'Nu a fost tradus in romana',
     'no_translated_message' => 'Momentan, pagina nu a fost tradusa in romana. Pentru mai multe informatii, va rugam sa ne trimiteti un mesaj si va vom contacta cat mai curand posibil.',
-
+    
     'no_brand_default_message' => 'Produsul nu este disponibil in tara dumneavoastra.',
-
+    
     'testimonials' => 'Testimoniale',
     'testimonial_question' => 'Ce spun clientii si partenerii despre MINEX Group?',
-
+    
     'blog' => 'Blog',
     'articles' => 'Articole',
     'next' => 'Напред',
