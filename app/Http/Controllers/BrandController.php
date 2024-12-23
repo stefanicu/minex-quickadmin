2a5ecacf7134a6c23dbe76fc1db7edeb533f7955
Stefan Nicula <stefanicu@gmail.com>
2024-12-23 04:06:38 +0200

    Update BrandController.php

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Application;
 use App\Models\Brand;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -11,42 +10,50 @@ class BrandController extends Controller
 {
     public function index(Request $request)
     {
+        $currentLocale = app()->getLocale();
+        
         $brand_slug = $request->slug;
-
-        $brand = Brand::select('id','name','slug')
-            ->where('slug','=',$brand_slug)
+        
+        $brand = Brand::select('id', 'name', 'slug')
+            ->where('slug', '=', $brand_slug)
             ->first();
-
+        
         $brands = Brand::leftJoin('brand_translations', 'brands.id', '=', 'brand_translations.brand_id')
             ->leftJoin('products', 'brands.id', '=', 'products.brand_id')
             ->leftJoin('product_translations', 'products.id', '=', 'product_translations.product_id')
             ->selectRaw('brands.id, brands.name, brands.slug, COUNT(products.id) as cnt')
             //->where('brands.online', '=', 1)
             //->where('brand_translations.online', '=', 1)
-            ->where('brand_translations.locale', '=', app()->getLocale())
-            ->where('product_translations.locale', '=', app()->getLocale())
+            ->where('brand_translations.locale', '=', $currentLocale)
+            ->where('product_translations.locale', '=', $currentLocale)
             ->groupByRaw('brands.id, brands.name, brands.slug')
             ->having('cnt', '>', 0) // Exclude brands with no products
             ->orderBy('brands.name')
             ->get();
-
-        $products = Product::leftJoin('product_translations','products.id','=','product_translations.product_id')
-            ->where('products.online','=',1)
-            ->where('product_translations.online','=',1)
-            ->where('brand_id','=',$brand->id)
-            ->where('product_translations.locale','=',app()->getLocale())
-            ->select(sprintf('%s.*', (new Product)->table),'product_translations.name as name','product_translations.slug as slug')
+        
+        $products = Product::leftJoin('product_translations', 'products.id', '=', 'product_translations.product_id')
+            ->where('products.online', '=', 1)
+            ->where('product_translations.online', '=', 1)
+            ->where('brand_id', '=', $brand->id)
+            ->where('product_translations.locale', '=', $currentLocale)
+            ->select(sprintf('%s.*', (new Product)->table), 'product_translations.name as name',
+                'product_translations.slug as slug')
             ->orderBy('name')
             ->get();
-
-        if($products->count() == 1)
-        {
+        
+        if ($products->count() == 1) {
             $product = $products->first();
-            $slug = $product->slug;
-            return redirect()->route('product.index', compact('product', 'slug'));
+            return redirect()->route('product_brand.'.app()->getLocale(),
+                ['brand_slug' => $brand->slug, 'prod_slug' => $product->slug]);
         }
-
+        
+        $slugs = null;
+        foreach (config('translatable.locales') as $locale) {
+            $slug_brand = $brand->translate($locale)->slug ?? $brand->id;
+            $slugs[$locale] = $slug_brand;
+        }
+        
         $products = $products->all();
-        return view('brand', compact('brand', 'brands', 'products'));
+        return view('brand', compact('brand', 'brands', 'products', 'slugs'));
     }
 }
