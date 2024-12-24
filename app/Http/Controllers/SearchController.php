5bbe98e633a03c63cb2853f4ddb543227cc7e42f
Stefan Nicula <stefanicu@gmail.com>
2024-12-24 15:18:16 +0200

    Update SearchController.php

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -23,7 +23,7 @@ class SearchController extends Controller
 //            ->whereTranslationLike('name', "%$search%")
 //            ->orWhereTranslationLike('description', "%$search%")
 //            ->get();
-
+        
         $blogs = Blog::with(['translations', 'media'])
             ->whereHas('translations', function ($query) use ($search) {
                 $query->where('name', 'like', "%$search%")
@@ -33,17 +33,24 @@ class SearchController extends Controller
                 $query->where('locale', app()->getLocale());
             })
             ->get();
-
-        $products = Product::with(['translations', 'media'])
-            ->whereHas('translations', function ($query) use ($search) {
-                $query->where('name', 'like', "%$search%")
-                    ->orWhere('description', 'like', "%$search%");
+        
+        $products = Product::with(['translations', 'media', 'brand'])
+            ->leftJoin('product_translations', 'products.id', '=', 'product_translations.product_id')
+            ->leftJoin('brands', 'products.brand_id', '=', 'brands.id')
+            ->where('product_translations.locale', app()->getLocale()) // Filter by locale
+            ->where(function ($query) use ($search) {
+                $query->where('product_translations.name', 'like', "%$search%")
+                    ->orWhere('product_translations.description', 'like', "%$search%");
             })
-            ->whereHas('translations', function ($query) {
-                $query->where('locale', app()->getLocale());
-            })
+            ->select([
+                'products.*',
+                'brands.name as brand_name',
+                'brands.slug as brand_slug',
+                'product_translations.name as translation_name',
+                'product_translations.description as translation_description',
+            ])
             ->get();
-
-        return view('search', compact('blogs','products','search'));
+        
+        return view('search', compact('blogs', 'products', 'search'));
     }
 }
