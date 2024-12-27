6f3cd951fb0681ba8b4172995402a05475dcf981
Stefan Nicula <stefanicu@gmail.com>
2024-12-27 02:03:58 +0200

    Create blogs.blade.php

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.frontend')
+@section('content')
+    {{--    @dd($blog, $blogs)--}}
+
+
+    <div class="container-fluid cover p-0">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb mb-0">
+                <li class="breadcrumb-item"><a href="/">{{ trans('menu.home') }}</a></li>
+                <li class="breadcrumb-item">{{ trans('pages.blog') }}</li>
+            </ol>
+        </nav>
+
+        <figure class="d-flex align-items-center section--figure">
+            <picture data-alt="Branduri">
+                <source data-srcset="{{ asset('/img/headers/aplicatie-xl.jpg') }}" media="(min-width: 1200px)">
+                <source data-srcset="{{ asset('/img/headers/aplicatie-lg.jpg') }}" media="(min-width: 992px)">
+                <source data-srcset="{{ asset('/img/headers/aplicatie-md.jpg') }}" media="(min-width: 576px)">
+                <source data-srcset="{{ asset('/img/headers/aplicatie-sm.jpg') }}" media="(max-width: 576px)">
+                <img class="lozad img-fluid section--hero-img lazy-fade"
+                     srcset="{{ asset('/img/headers/aplicatie-xl.jpg') }}" alt="{{ trans('pages.references') }}"
+                     data-loaded="true">
+                <noscript>
+                    <img class="img-fluid lozad" src="{{ asset('/img/headers/aplicatie-xl.jpg') }}"
+                         alt="{{ trans('pages.references') }}">
+                </noscript>
+            </picture>
+        </figure>
+
+    </div>
+
+    <div class="container content">
+        <div class="row">
+            <div class="col-12 pb-4 mb-4">
+                <h1 class="h2">{{ trans('pages.blog') }}</h1>
+            </div>
+        </div>
+        <div class="row blog">
+            @foreach ($blogs as $blg)
+                <div class="col-12 col-sm-8 col-md-6 col-lg-4 mb-4">
+                    <div class="card">
+                        <div class="card-image d-flex align-items-center justify-content-center">
+                            <a href="{{ route('blog.'.app()->getLocale(), ['slug' => $blg->slug ?? null]) }}"
+                               class="d-flex align-items-center w-100 h-100">
+                                @if($blg->getImageAttribute() !== null && $blg->getImageAttribute()->count()>0)
+                                    <img srcset="{{ $blg->getImageAttribute()->getUrl('preview') }}"
+                                         alt="{{ $blg->name }}"
+                                         class="mx-auto card-img lozad">
+                                @else
+                                    <div class="blog_slider_image_default">No image</div>
+                                @endif
+                            </a>
+                        </div>
+                        <div class="card-body">
+                            <a href="{{ route('blog.'.app()->getLocale(), ['slug' => $blg->slug ?? null]) }}"
+                               class="card-title">{{ $blg->name }}</a>
+                        </div>
+                        <div class="card-footer text-muted d-flex justify-content-between bg-transparent border-top-0">
+                            <div class="views">{{ Carbon\Carbon::parse($blg->created_at)->format('d.m.Y') }}</div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        <div class="row">
+            <div class="mx-auto my-4">
+                {{ $blogs->links() }}
+            </div>
+        </div>
+    </div>
+
+@endsection
+@section('scripts')
+    @parent
+    <script src="{{ asset('/js/bxSlider/blog_setup.js') }}"></script>
+@endsection
