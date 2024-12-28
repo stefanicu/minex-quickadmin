b3317a0a7093e52c9184427dacfe7847c079ca39
Stefan Nicula <stefanicu@gmail.com>
2024-12-28 19:10:31 +0200

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,27 +15,29 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media;
 class Product extends Model implements HasMedia, TranslatableContract
 {
     use SoftDeletes, InteractsWithMedia, HasFactory, Translatable;
-
+    
     public $table = 'products';
-
-    public $translatedAttributes = ['online','name','slug','description','specifications','advantages','usages','accessories'];
-
+    
+    public $translatedAttributes = [
+        'online', 'name', 'slug', 'description', 'specifications', 'advantages', 'usages', 'accessories'
+    ];
+    
     protected $indexes = [
         'name',
         'online',
     ];
-
+    
     protected $appends = [
         'photo',
         'main_photo',
     ];
-
+    
     protected $dates = [
         'created_at',
         'updated_at',
         'deleted_at',
     ];
-
+    
     protected $fillable = [
         'online',
         'brand_id',
@@ -45,76 +47,76 @@ class Product extends Model implements HasMedia, TranslatableContract
         'updated_at',
         'deleted_at',
     ];
-
-    protected function serializeDate(DateTimeInterface $date)
+    
+    protected function serializeDate(DateTimeInterface $date): string
     {
         return $date->format('Y-m-d H:i:s');
     }
-
+    
     public function registerMediaConversions(Media $media = null): void
     {
         $this->addMediaConversion('thumb')->fit('crop', 50, 50);
         $this->addMediaConversion('preview')->fit('crop', 120, 120);
     }
-
+    
     public function brand()
     {
         return $this->belongsTo(Brand::class, 'brand_id');
     }
-
+    
     public function categories()
     {
         return $this->belongsToMany(Category::class, 'category_product', 'product_id', 'category_id');
     }
-
+    
     public function applications()
     {
         return $this->belongsToMany(Application::class, 'application_product', 'product_id', 'application_id');
     }
-
+    
     public function Productfiles()
     {
         return $this->hasMany(Productfile::class, 'product_id', 'id');
     }
-
+    
     public function getPhotoAttribute()
     {
         $files = $this->getMedia('photo');
         $files->each(function ($item) {
-            $item->url       = $item->getUrl();
+            $item->url = $item->getUrl();
             $item->thumbnail = $item->getUrl('thumb');
-            $item->preview   = $item->getUrl('preview');
+            $item->preview = $item->getUrl('preview');
         });
-
+        
         return $files;
     }
-
+    
     public function getFirstPhotoAttribute()
     {
         $file = $this->getMedia('photo')->first();
         if ($file) {
-            $file->url       = $file->getUrl();
+            $file->url = $file->getUrl();
             $file->thumbnail = $file->getUrl('thumb');
-            $file->preview   = $file->getUrl('preview');
+            $file->preview = $file->getUrl('preview');
         }
-
+        
         return $file;
     }
-
+    
     public function getMainPhotoAttribute()
     {
         $file = $this->getMedia('main_photo')->last();
         if ($file) {
-            $file->url       = $file->getUrl();
+            $file->url = $file->getUrl();
             $file->thumbnail = $file->getUrl('thumb');
-            $file->preview   = $file->getUrl('preview');
+            $file->preview = $file->getUrl('preview');
         }
-
+        
         return $file;
     }
-
+    
     public function references()
     {
-        return $this->belongsToMany(Reference::class,'product_reference','product_id','reference_id');
+        return $this->belongsToMany(Reference::class, 'product_reference', 'product_id', 'reference_id');
     }
 }
