22851927d911b7337933d1de7e803dc64a9ee402
Stefan Nicula <stefanicu@gmail.com>
2024-12-28 02:29:06 +0200

    Update CategoriesController.php

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -19,12 +19,32 @@ class CategoriesController extends Controller
         
         $application_slug = $request->app_slug;
         
-        $application = Application::whereTranslation('slug', $application_slug)->whereTranslation('locale',
-            $currentLocale)->first();
-        
-        if ( ! $application) {
-            $application = Application::whereTranslation('slug', $application_slug)->whereTranslation('locale',
-                'en')->first();
+        if ( ! is_numeric($application_slug)) {
+            $application = Application::whereTranslation('slug', $application_slug)
+                ->whereTranslation('locale', $currentLocale)
+                ->whereHas('translations', function ($query) {
+                    $query->where('online', 1); // Ensure the translation is marked as online
+                })
+                ->first();
+        } else {
+            $application_id = $application_slug;
+            $application = Application::whereTranslation('application_id', $application_id)
+                ->whereHas('translations', function ($query) {
+                    $query->where('online', 1); // Ensure the translation is marked as online
+                })
+                ->first();
+            
+            $application->name = trans('pages.no_translated_title');
+            $application->description = trans('pages.no_translated_message');
+            
+            $categories = [];
+            
+            $app_slugs = null;
+            foreach (config('translatable.locales') as $locale) {
+                $app_slugs[$locale] = $application->translate($locale)->slug ?? $application->id;
+            }
+            
+            return view('categories', compact('application', 'categories', 'app_slugs'));
         }
         
         $productIds = null;
@@ -32,20 +52,33 @@ class CategoriesController extends Controller
             $productIds = Application::find($application->id)
                 ->products()
                 ->whereHas('translations', function ($query) use ($currentLocale) {
-                    $query->where('locale', $currentLocale);
+                    $query->where('locale', $currentLocale)->where('online', 1);
                 })
                 ->pluck('products.id')
                 ->toArray();
         }
         
         if ( ! $productIds) {
+            // TODO: replace categories with a message
             $categories = Category::with('product_main_image', 'product_main_image.media')
+                ->whereHas('translations', function ($query) use ($currentLocale) {
+                    $query->where('locale', $currentLocale)
+                        ->where('online', 1)
+                        ->whereNotNull('name')
+                        ->where('name', '!=', '')
+                        ->whereNotNull('slug')
+                        ->where('slug', '!=', '');
+                })
                 ->orderByTranslation('name')
+                ->limit(3)
                 ->get();
         } else {
-            $categories = Category::with('product_main_image', 'product_main_image.media')
+            $categories = Category::with('product_main_image.media')
                 ->whereHas('products', function ($query) use ($productIds) {
                     $query->whereIn('products.id', $productIds);
+                })
+                ->whereHas('translations', function ($query) {
+                    $query->where('online', 1);
                 })
                 ->orderByTranslation('name')
                 ->get();
@@ -80,8 +113,7 @@ class CategoriesController extends Controller
         
         $app_slugs = null;
         foreach (config('translatable.locales') as $locale) {
-            $slug_app = $application->translate($locale)->slug ?? $application->id;
-            $app_slugs[$locale] = $slug_app;
+            $app_slugs[$locale] = $application->translate($locale)->slug ?? $application->id;
         }
         
         return view('categories', compact('application', 'categories', 'app_slugs'));
