f69b150c0c74887b93447696f32ea011245e54ca
Stefan Nicula <stefanicu@gmail.com>
2025-01-04 01:23:08 +0200

    Update ApplicationTranslation.php

--- app/Models/ApplicationTranslation.php
+++ app/Models/ApplicationTranslation.php
@@ -7,15 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class ApplicationTranslation extends Model
 {
     public $timestamps = false;
-
-    protected $fillable = ['online','name','slug'];
-
+    
+    protected $fillable = [
+        'online', 'name', 'slug',
+        'meta_title', 'meta_description', 'author', 'robots', 'canonical_url'
+    ];
+    
     protected $indexes = [
         'application_id',
         'name',
         'online',
     ];
-
+    
     public static $searchable = [
         'name',
         'slug',
