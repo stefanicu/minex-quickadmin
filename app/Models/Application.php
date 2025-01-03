d3f8ecbb3160626e77f0be5447165f590a1a49e1
Stefan Nicula <stefanicu@gmail.com>
2025-01-04 01:23:05 +0200

    Update Application.php

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
 use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,32 +11,33 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
-use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
-use Astrotomic\Translatable\Translatable;
 
 class Application extends Model implements HasMedia, TranslatableContract
 {
     use SoftDeletes, InteractsWithMedia, HasFactory, Translatable;
-
+    
     public $table = 'applications';
-
-    public $translatedAttributes = ['online','name','slug'];
-
+    
+    public $translatedAttributes = [
+        'online', 'name', 'slug',
+        'meta_title', 'meta_description', 'author', 'robots', 'canonical_url'
+    ];
+    
     protected $indexes = [
         'name',
         'online',
     ];
-
+    
     protected $appends = [
         'image',
     ];
-
+    
     protected $dates = [
         'created_at',
         'updated_at',
         'deleted_at',
     ];
-
+    
     protected $fillable = [
         'online',
         'oldid',
@@ -50,35 +53,35 @@ class Application extends Model implements HasMedia, TranslatableContract
 //            $builder->all();
 //        });
 //    }
-
+    
     protected function serializeDate(DateTimeInterface $date): string
     {
         return $date->format('Y-m-d H:i:s');
     }
-
+    
     public function registerMediaConversions(Media $media = null): void
     {
         $this->addMediaConversion('thumb')->fit('crop', 50, 50);
         $this->addMediaConversion('preview')->fit('crop', 120, 120);
     }
-
+    
     public function getImageAttribute()
     {
         $file = $this->getMedia('image')->last();
         if ($file) {
-            $file->url       = $file->getUrl();
+            $file->url = $file->getUrl();
             $file->thumbnail = $file->getUrl('thumb');
-            $file->preview   = $file->getUrl('preview');
+            $file->preview = $file->getUrl('preview');
         }
-
+        
         return $file;
     }
-
+    
     public function categories(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
         return $this->belongsToMany(Category::class);
     }
-
+    
     public function products(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
         return $this->belongsToMany(Product::class);
