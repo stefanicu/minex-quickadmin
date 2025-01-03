a1575af7829e12ca91cad9a2bde63567267f83ef
Stefan Nicula <stefanicu@gmail.com>
2025-01-03 21:11:39 +0200

    Update create.blade.php

--- resources/views/admin/applications/create.blade.php
+++ resources/views/admin/applications/create.blade.php
@@ -1,135 +1,144 @@
 @extends('layouts.admin')
 @section('content')
 
-<div class="card">
-    <div class="card-header">
-        {{ trans('global.create') }} {{ trans('cruds.application.title_singular') }}
-    </div>
+    <div class="card">
+        <div class="card-header">
+            {{ trans('global.create') }} {{ trans('cruds.application.title_singular') }}
+        </div>
 
-    <div class="card-body">
-        <form method="POST" action="{{ route("admin.applications.store") }}" enctype="multipart/form-data">
-            @csrf
-{{--            <div class="form-group">--}}
-{{--                <div class="form-check {{ $errors->has('online') ? 'is-invalid' : '' }}">--}}
-{{--                    <input type="hidden" name="online" value="0">--}}
-{{--                    <input class="form-check-input" type="checkbox" name="online" id="online" value="1" {{ old('online', 0) == 1 || old('online') === null ? 'checked' : '' }}>--}}
-{{--                    <label class="form-check-label" for="online">{{ trans('cruds.application.fields.online') }}</label>--}}
-{{--                </div>--}}
-{{--                @if($errors->has('online'))--}}
-{{--                    <span class="text-danger">{{ $errors->first('online') }}</span>--}}
-{{--                @endif--}}
-{{--                <span class="help-block">{{ trans('cruds.application.fields.online_helper') }}</span>--}}
-{{--            </div>--}}
-
-            <div class="form-group">
-                <label class="required" for="name">{{ trans('cruds.application.fields.name') }}</label>
-                <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', '') }}" required>
-                @if($errors->has('name'))
-                    <span class="text-danger">{{ $errors->first('name') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.application.fields.name_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label class="required" for="slug">{{ trans('cruds.application.fields.slug') }}</label>
-                <input class="form-control {{ $errors->has('slug') ? 'is-invalid' : '' }}" type="text" name="slug" id="slug" value="{{ old('slug', '') }}" required>
-                @if($errors->has('slug'))
-                    <span class="text-danger">{{ $errors->first('slug') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.application.fields.slug_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label for="image">{{ trans('cruds.application.fields.image') }}</label>
-                <div class="needsclick dropzone {{ $errors->has('image') ? 'is-invalid' : '' }}" id="image-dropzone">
+        <div class="card-body">
+            <form method="POST" action="{{ route("admin.applications.store") }}" enctype="multipart/form-data">
+                @csrf
+                {{--            <div class="form-group">--}}
+                {{--                <div class="form-check {{ $errors->has('online') ? 'is-invalid' : '' }}">--}}
+                {{--                    <input type="hidden" name="online" value="0">--}}
+                {{--                    <input class="form-check-input" type="checkbox" name="online" id="online" value="1" {{ old('online', 0) == 1 || old('online') === null ? 'checked' : '' }}>--}}
+                {{--                    <label class="form-check-label" for="online">{{ trans('cruds.application.fields.online') }}</label>--}}
+                {{--                </div>--}}
+                {{--                @if($errors->has('online'))--}}
+                {{--                    <span class="text-danger">{{ $errors->first('online') }}</span>--}}
+                {{--                @endif--}}
+                {{--                <span class="help-block">{{ trans('cruds.application.fields.online_helper') }}</span>--}}
+                {{--            </div>--}}
+                <div class="row">
+                    <div class="form-group col-6">
+                        <label class="required" for="name">{{ trans('cruds.application.fields.name') }}</label>
+                        <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text"
+                               name="name" id="name" value="{{ old('name', '') }}" required>
+                        @if($errors->has('name'))
+                            <span class="text-danger">{{ $errors->first('name') }}</span>
+                        @endif
+                        <span class="help-block">{{ trans('cruds.application.fields.name_helper') }}</span>
+                    </div>
+                    <div class="form-group col-6">
+                        <label class="required" for="slug">{{ trans('cruds.application.fields.slug') }}</label>
+                        <input class="form-control {{ $errors->has('slug') ? 'is-invalid' : '' }}" type="text"
+                               name="slug" id="slug" value="{{ old('slug', '') }}" required>
+                        @if($errors->has('slug'))
+                            <span class="text-danger">{{ $errors->first('slug') }}</span>
+                        @endif
+                        <span class="help-block">{{ trans('cruds.application.fields.slug_helper') }}</span>
+                    </div>
                 </div>
-                @if($errors->has('image'))
-                    <span class="text-danger">{{ $errors->first('image') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.application.fields.image_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label for="categories">{{ trans('cruds.application.fields.categories') }}</label>
-                <div style="padding-bottom: 4px">
-                    <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
-                    <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
+
+                {{--                <div class="form-group">--}}
+                {{--                    <label for="image">{{ trans('cruds.application.fields.image') }}</label>--}}
+                {{--                    <div class="needsclick dropzone {{ $errors->has('image') ? 'is-invalid' : '' }}"--}}
+                {{--                         id="image-dropzone">--}}
+                {{--                    </div>--}}
+                {{--                    @if($errors->has('image'))--}}
+                {{--                        <span class="text-danger">{{ $errors->first('image') }}</span>--}}
+                {{--                    @endif--}}
+                {{--                    <span class="help-block">{{ trans('cruds.application.fields.image_helper') }}</span>--}}
+                {{--                </div>--}}
+
+                {{--                <div class="form-group">--}}
+                {{--                    <label for="categories">{{ trans('cruds.application.fields.categories') }}</label>--}}
+                {{--                    <div style="padding-bottom: 4px">--}}
+                {{--                        <span class="btn btn-info btn-xs select-all"--}}
+                {{--                              style="border-radius: 0">{{ trans('global.select_all') }}</span>--}}
+                {{--                        <span class="btn btn-info btn-xs deselect-all"--}}
+                {{--                              style="border-radius: 0">{{ trans('global.deselect_all') }}</span>--}}
+                {{--                    </div>--}}
+                {{--                    <select class="form-control select2 {{ $errors->has('categories') ? 'is-invalid' : '' }}"--}}
+                {{--                            name="categories[]" id="categories" multiple>--}}
+                {{--                        @foreach($categories as $id => $category)--}}
+                {{--                            <option value="{{ $id }}" {{ in_array($id, old('categories', [])) ? 'selected' : '' }}>{{ $category }}</option>--}}
+                {{--                        @endforeach--}}
+                {{--                    </select>--}}
+                {{--                    @if($errors->has('categories'))--}}
+                {{--                        <span class="text-danger">{{ $errors->first('categories') }}</span>--}}
+                {{--                    @endif--}}
+                {{--                    <span class="help-block">{{ trans('cruds.application.fields.categories_helper') }}</span>--}}
+                {{--                </div>--}}
+
+
+                <div class="form-group">
+                    <input type="hidden" name="locale" value="{{app()->getLocale()}}">
+                    <button class="btn btn-danger" type="submit">
+                        {{ trans('global.save') }}
+                    </button>
                 </div>
-                <select class="form-control select2 {{ $errors->has('categories') ? 'is-invalid' : '' }}" name="categories[]" id="categories" multiple>
-                    @foreach($categories as $id => $category)
-                        <option value="{{ $id }}" {{ in_array($id, old('categories', [])) ? 'selected' : '' }}>{{ $category }}</option>
-                    @endforeach
-                </select>
-                @if($errors->has('categories'))
-                    <span class="text-danger">{{ $errors->first('categories') }}</span>
-                @endif
-                <span class="help-block">{{ trans('cruds.application.fields.categories_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <input type="hidden" name="locale" value="{{app()->getLocale()}}">
-                <button class="btn btn-danger" type="submit">
-                    {{ trans('global.save') }}
-                </button>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
-</div>
-
-
 
 @endsection
 
 @section('scripts')
-<script>
-    Dropzone.options.imageDropzone = {
-    url: '{{ route('admin.applications.storeMedia') }}',
-    maxFilesize: 1, // MB
-    acceptedFiles: '.jpeg,.jpg,.png,.gif',
-    maxFiles: 1,
-    addRemoveLinks: true,
-    headers: {
-      'X-CSRF-TOKEN': "{{ csrf_token() }}"
-    },
-    params: {
-      size: 1,
-      width: 300,
-      height: 300
-    },
-    success: function (file, response) {
-      $('form').find('input[name="image"]').remove()
-      $('form').append('<input type="hidden" name="image" value="' + response.name + '">')
-    },
-    removedfile: function (file) {
-      file.previewElement.remove()
-      if (file.status !== 'error') {
-        $('form').find('input[name="image"]').remove()
-        this.options.maxFiles = this.options.maxFiles + 1
-      }
-    },
-    init: function () {
-@if(isset($application) && $application->image)
-      var file = {!! json_encode($application->image) !!}
-          this.options.addedfile.call(this, file)
-      this.options.thumbnail.call(this, file, file.preview ?? file.preview_url)
-      file.previewElement.classList.add('dz-complete')
-      $('form').append('<input type="hidden" name="image" value="' + file.file_name + '">')
-      this.options.maxFiles = this.options.maxFiles - 1
-@endif
-    },
-    error: function (file, response) {
-        if ($.type(response) === 'string') {
-            var message = response //dropzone sends it's own error messages in string
-        } else {
-            var message = response.errors.file
-        }
-        file.previewElement.classList.add('dz-error')
-        _ref = file.previewElement.querySelectorAll('[data-dz-errormessage]')
-        _results = []
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-            node = _ref[_i]
-            _results.push(node.textContent = message)
+    <script>
+        Dropzone.options.imageDropzone = {
+            url: '{{ route('admin.applications.storeMedia') }}',
+            maxFilesize: 1, // MB
+            acceptedFiles: '.jpeg,.jpg,.png,.gif',
+            maxFiles: 1,
+            addRemoveLinks: true,
+            headers: {
+                'X-CSRF-TOKEN': "{{ csrf_token() }}"
+            },
+            params: {
+                size: 1,
+                width: 300,
+                height: 300
+            },
+            success: function (file, response) {
+                $('form').find('input[name="image"]').remove()
+                $('form').append('<input type="hidden" name="image" value="' + response.name + '">')
+            },
+            removedfile: function (file) {
+                file.previewElement.remove()
+                if (file.status !== 'error') {
+                    $('form').find('input[name="image"]').remove()
+                    this.options.maxFiles = this.options.maxFiles + 1
+                }
+            },
+            init: function () {
+                @if(isset($application) && $application->image)
+                var file = {!! json_encode($application->image) !!}
+                this.options.addedfile.call(this, file)
+                this.options.thumbnail.call(this, file, file.preview ?? file.preview_url)
+                file.previewElement.classList.add('dz-complete')
+                $('form').append('<input type="hidden" name="image" value="' + file.file_name + '">')
+                this.options.maxFiles = this.options.maxFiles - 1
+                @endif
+            },
+            error: function (file, response) {
+                if ($.type(response) === 'string') {
+                    var message = response //dropzone sends it's own error messages in string
+                } else {
+                    var message = response.errors.file
+                }
+                file.previewElement.classList.add('dz-error')
+                _ref = file.previewElement.querySelectorAll('[data-dz-errormessage]')
+                _results = []
+                for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                    node = _ref[_i]
+                    _results.push(node.textContent = message)
+                }
+
+                return _results
+            }
         }
 
-        return _results
-    }
-}
-
-</script>
+    </script>
 @endsection
