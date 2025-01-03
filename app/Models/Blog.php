fd679a88a353298834ecf5ddfc37877eb2a81ca7
Stefan Nicula <stefanicu@gmail.com>
2025-01-03 21:12:27 +0200

    Update Blog.php

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
 use DateTimeInterface;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use RalphJSmit\Laravel\SEO\Support\HasSEO;
 use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
@@ -15,21 +15,22 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Blog extends Model implements HasMedia, TranslatableContract
 {
-    use SoftDeletes, InteractsWithMedia, HasFactory, Translatable;
-
+    use SoftDeletes, InteractsWithMedia, Translatable;
+    
     public $table = 'blogs';
-    public $translatedAttributes = ['online','name','slug','content','image_text'];
-
+    
+    public array $translatedAttributes = ['online', 'name', 'slug', 'content', 'image_text'];
+    
     protected $appends = [
         'image',
     ];
-
+    
     protected $dates = [
         'created_at',
         'updated_at',
         'deleted_at',
     ];
-
+    
     protected $fillable = [
         'online',
         'oldid',
@@ -39,30 +40,30 @@ class Blog extends Model implements HasMedia, TranslatableContract
         'updated_at',
         'deleted_at',
     ];
-
+    
     protected function serializeDate(DateTimeInterface $date)
     {
         return $date->format('Y-m-d H:i:s');
     }
-
+    
     public function registerMediaConversions(Media $media = null): void
     {
-        $this->addMediaConversion('thumb')->width( 75);
-        $this->addMediaConversion('preview')->width( 288);
-        $this->addMediaConversion('thumb_webp')->width( 75)->format(Manipulations::FORMAT_WEBP);
-        $this->addMediaConversion('preview_webp')->width( 288)->format(Manipulations::FORMAT_WEBP);
+        $this->addMediaConversion('thumb')->width(75);
+        $this->addMediaConversion('preview')->width(288);
+        $this->addMediaConversion('thumb_webp')->width(75)->format(Manipulations::FORMAT_WEBP);
+        $this->addMediaConversion('preview_webp')->width(288)->format(Manipulations::FORMAT_WEBP);
         $this->addMediaConversion('original_webp')->format(Manipulations::FORMAT_WEBP);
     }
-
+    
     public function getImageAttribute()
     {
         $file = $this->getMedia('image')->last();
         if ($file) {
-            $file->url       = $file->getUrl();
+            $file->url = $file->getUrl();
             $file->thumbnail = $file->getUrl('thumb');
-            $file->preview   = $file->getUrl('preview');
+            $file->preview = $file->getUrl('preview');
         }
-
+        
         return $file;
     }
 }
