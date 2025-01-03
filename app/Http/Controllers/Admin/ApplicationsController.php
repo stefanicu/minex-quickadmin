0a74c4050eb0f365a7649241725bfa4a90d5e9f4
Stefan Nicula <stefanicu@gmail.com>
2025-01-03 21:14:30 +0200

    Update ApplicationsController.php

--- app/Http/Controllers/Admin/ApplicationsController.php
+++ app/Http/Controllers/Admin/ApplicationsController.php
@@ -8,10 +8,10 @@ use App\Http\Requests\MassDestroyApplicationRequest;
 use App\Http\Requests\StoreApplicationRequest;
 use App\Http\Requests\UpdateApplicationRequest;
 use App\Models\Application;
-use App\Models\Category;
 use App\Models\CategoryTranslation;
 use Gate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Symfony\Component\HttpFoundation\Response;
 use Yajra\DataTables\Facades\DataTables;
@@ -19,16 +19,26 @@ use Yajra\DataTables\Facades\DataTables;
 class ApplicationsController extends Controller
 {
     use MediaUploadingTrait;
-
+    
     public function index(Request $request)
     {
         abort_if(Gate::denies('application_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
+        
         if ($request->ajax()) {
-            $query = Application::with(['media','translations'])
-                ->join('application_translations','applications.id','=','application_translations.application_id')
-                ->where('application_translations.locale','=',app()->getLocale())
-                ->select(sprintf('%s.*', (new Application)->table));
+//            $query = Application::with(['media', 'translations'])
+//                ->join('application_translations', 'applications.id', '=', 'application_translations.application_id')
+//                ->where('application_translations.locale', '=', app()->getLocale())
+//                ->select(sprintf('%s.*', (new Application)->table));
+            
+            $query = Application::with(['media', 'translations'])
+                ->leftJoin('application_translations', function ($join) {
+                    $join->on('applications.id', '=', 'application_translations.application_id')
+                        ->where('application_translations.locale', '=', app()->getLocale());
+                })
+                ->select([
+                    sprintf('%s.*', (new Application)->getTable()),
+                    DB::raw("COALESCE(application_translations.name, '---NO TRANSLATION---') as name")
+                ]);
 
 
 //            foreach ($query->get() as $application) {
@@ -44,19 +54,19 @@ class ApplicationsController extends Controller
 //                    }
 //                }
 //            }
-
-
+            
+            
             $table = Datatables::of($query);
-
+            
             $table->addColumn('placeholder', '&nbsp;');
             $table->addColumn('actions', '&nbsp;');
-
+            
             $table->editColumn('actions', function ($row) {
-                $viewGate      = 'application_show';
-                $editGate      = 'application_edit';
-                $deleteGate    = 'application_delete';
+                $viewGate = 'application_show';
+                $editGate = 'application_edit';
+                $deleteGate = 'application_delete';
                 $crudRoutePart = 'applications';
-
+                
                 return view('partials.datatablesActions', compact(
                     'viewGate',
                     'editGate',
@@ -65,12 +75,12 @@ class ApplicationsController extends Controller
                     'row'
                 ));
             });
-
+            
             $table->editColumn('id', function ($row) {
                 return $row->id ? $row->id : '';
             });
             $table->editColumn('online', function ($row) {
-                return '<input type="checkbox" disabled ' . ($row->online ? 'checked' : null) . '>';
+                return '<input type="checkbox" disabled '.($row->online ? 'checked' : null).'>';
             });
             $table->editColumn('name', function ($row) {
                 return $row->name ? $row->name : '';
@@ -83,100 +93,107 @@ class ApplicationsController extends Controller
                         $photo->thumbnail
                     );
                 }
-
+                
                 return '';
             });
-
+            
             $table->rawColumns(['actions', 'placeholder', 'online', 'image']);
-
+            
             return $table->make(true);
         }
-
+        
         return view('admin.applications.index');
     }
-
+    
     public function create()
     {
         abort_if(Gate::denies('application_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
-        $categories = CategoryTranslation::where('locale',app()->getLocale() )->orderBy('name','asc')->pluck('name', 'id');
-
+        
+        $categories = CategoryTranslation::where('locale', app()->getLocale())->orderBy('name', 'asc')->pluck('name',
+            'id');
+        
         return view('admin.applications.create', compact('categories'));
     }
-
+    
     public function store(StoreApplicationRequest $request)
     {
         $application = Application::create($request->all());
+        
         $application->categories()->sync($request->input('categories', []));
+        
         if ($request->input('image', false)) {
-            $application->addMedia(storage_path('tmp/uploads/' . basename($request->input('image'))))->toMediaCollection('image');
+            $application->addMedia(storage_path('tmp/uploads/'.basename($request->input('image'))))->toMediaCollection('image');
         }
-
+        
         if ($media = $request->input('ck-media', false)) {
             Media::whereIn('id', $media)->update(['model_id' => $application->id]);
         }
-
+        
         return redirect()->route('admin.applications.index');
     }
-
+    
     public function edit(Application $application)
     {
         abort_if(Gate::denies('application_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
-        $categories = CategoryTranslation::where('locale',app()->getLocale() )->orderBy('name','asc')->pluck('name', 'id');
-
+        
+        $categories = CategoryTranslation::where('locale', app()->getLocale())->orderBy('name', 'asc')->pluck('name',
+            'id');
+        
         $application->load('categories');
-
+        
         return view('admin.applications.edit', compact('application', 'categories'));
     }
-
+    
     public function update(UpdateApplicationRequest $request, Application $application)
     {
         $application->update($request->all());
+        
         $application->categories()->sync($request->input('categories', []));
+        
         if ($request->input('image', false)) {
-            if (! $application->image || $request->input('image') !== $application->image->file_name) {
+            if ( ! $application->image || $request->input('image') !== $application->image->file_name) {
                 if ($application->image) {
                     $application->image->delete();
                 }
-                $application->addMedia(storage_path('tmp/uploads/' . basename($request->input('image'))))->toMediaCollection('image');
+                $application->addMedia(storage_path('tmp/uploads/'.basename($request->input('image'))))->toMediaCollection('image');
             }
         } elseif ($application->image) {
             $application->image->delete();
         }
-
+        
         return redirect()->route('admin.applications.index');
     }
-
+    
     public function destroy(Application $application)
     {
         abort_if(Gate::denies('application_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
+        
         $application->delete();
-
+        
         return back();
     }
-
+    
     public function massDestroy(MassDestroyApplicationRequest $request)
     {
         $applications = Application::find(request('ids'));
-
+        
         foreach ($applications as $application) {
             $application->delete();
         }
-
+        
         return response(null, Response::HTTP_NO_CONTENT);
     }
-
+    
     public function storeCKEditorImages(Request $request)
     {
-        abort_if(Gate::denies('application_create') && Gate::denies('application_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
-        $model         = new Application();
-        $model->id     = $request->input('crud_id', 0);
+        abort_if(Gate::denies('application_create') && Gate::denies('application_edit'), Response::HTTP_FORBIDDEN,
+            '403 Forbidden');
+        
+        $model = new Application();
+        $model->id = $request->input('crud_id', 0);
         $model->exists = true;
-        $media         = $model->addMediaFromRequest('upload')->toMediaCollection('ck-media');
-
+        $media = $model->addMediaFromRequest('upload')->toMediaCollection('ck-media');
+        
         return response()->json(['id' => $media->id, 'url' => $media->getUrl()], Response::HTTP_CREATED);
     }
 }
