b21585fb253cfbc341ff9479578ac7d3e6a0bbc7
Stefan Nicula <stefanicu@gmail.com>
2025-01-03 21:12:08 +0200

    Update FrontPage.php

--- app/Models/FrontPage.php
+++ app/Models/FrontPage.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
 use DateTimeInterface;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
@@ -13,21 +12,22 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class FrontPage extends Model implements HasMedia, TranslatableContract
 {
-    use InteractsWithMedia, HasFactory, Translatable;
-
+    use InteractsWithMedia, Translatable;
+    
     public $table = 'front_pages';
-    public $translatedAttributes = ['name','first_text','second_text','quote','button'];
-
+    
+    public array $translatedAttributes = ['name', 'first_text', 'second_text', 'quote', 'button'];
+    
     protected $appends = [
         'image',
     ];
-
+    
     protected $dates = [
         'created_at',
         'updated_at',
         'deleted_at',
     ];
-
+    
     protected $fillable = [
         'oldid',
         'oldimage',
@@ -35,27 +35,27 @@ class FrontPage extends Model implements HasMedia, TranslatableContract
         'updated_at',
         'deleted_at',
     ];
-
+    
     protected function serializeDate(DateTimeInterface $date)
     {
         return $date->format('Y-m-d H:i:s');
     }
-
+    
     public function registerMediaConversions(Media $media = null): void
     {
         $this->addMediaConversion('thumb')->fit('crop', 50, 50);
         $this->addMediaConversion('preview')->fit('crop', 120, 120);
     }
-
+    
     public function getImageAttribute()
     {
         $file = $this->getMedia('image')->last();
         if ($file) {
-            $file->url       = $file->getUrl();
+            $file->url = $file->getUrl();
             $file->thumbnail = $file->getUrl('thumb');
-            $file->preview   = $file->getUrl('preview');
+            $file->preview = $file->getUrl('preview');
         }
-
+        
         return $file;
     }
 }
