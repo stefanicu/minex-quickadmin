c0dd5a2f27772ed4647ad598c08404a056b954c6
Stefan Nicula <stefanicu@gmail.com>
2025-01-04 05:18:21 +0200

    Update ProductTranslation.php

--- app/Models/ProductTranslation.php
+++ app/Models/ProductTranslation.php
@@ -6,28 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductTranslation extends Model
 {
-    protected $table = 'product_translations';
-
     public $timestamps = false;
-
-    protected $indexes = [
-        'product_id',
-        'name',
-        'online',
-    ];
-
+    
     protected $fillable = [
         'online',
-        'name',
-        'slug',
-        'description',
-        'specifications',
-        'advantages',
-        'usages',
-        'accessories'
-    ];
-
-    public static $searchable = [
         'name',
         'slug',
         'description',
