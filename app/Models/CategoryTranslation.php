5b9298ab37b3b7e2b2fd7983ba2981369105c2b6
Stefan Nicula <stefanicu@gmail.com>
2025-01-04 04:33:16 +0200

    Update CategoryTranslation.php

--- app/Models/CategoryTranslation.php
+++ app/Models/CategoryTranslation.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class CategoryTranslation extends Model
 {
+    protected $table = 'category_translations';
+    
     public $timestamps = false;
     
     protected $fillable = [
