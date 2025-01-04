465189613a557cedf07b7510a71c78089a987aac
Stefan Nicula <stefanicu@gmail.com>
2025-01-04 05:18:17 +0200

    Update ProductfileTranslation.php

--- app/Models/ProductfileTranslation.php
+++ app/Models/ProductfileTranslation.php
@@ -6,19 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductfileTranslation extends Model
 {
-    protected $table = 'productfile_translations';
-
     public $timestamps = false;
-
-    protected $indexes = [
-        'productfiles_id',
-        'title',
-        'online',
-    ];
-
+    
     protected $fillable = [
         'online',
         'title',
     ];
-
+    
 }
