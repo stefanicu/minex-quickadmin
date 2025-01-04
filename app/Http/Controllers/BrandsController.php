acd8ea2f718d77afb18301e8b63c51ae27620f36
Stefan Nicula <stefanicu@gmail.com>
2025-01-04 12:47:45 +0200

    Update BrandsController.php

--- app/Http/Controllers/BrandsController.php
+++ app/Http/Controllers/BrandsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Application;
 use App\Models\Brand;
 
 class BrandsController extends Controller
@@ -13,13 +12,37 @@ class BrandsController extends Controller
             $join->on('brands.id', '=', 'brand_translations.brand_id')
                 //->where('brand_translations.online', '=', 1)
                 ->where('brand_translations.locale', '=', app()->getLocale());
-            })
+        })
 //            ->whereHas('products')
             ->selectRaw('brands.id, brands.name, brands.slug')
             ->groupByRaw('brands.id, brands.name, brands.slug')
             ->orderBy('brands.name')
             ->get();
-
-        return view('brands', compact('brands'));
+        
+        $meta_title = trans('pages.brands');
+        $meta_description = trans('pages.brands');
+        $canonical_url = null;
+        $author = 'Minex Group International';
+        $robots = null;
+        $meta_image_url = null;
+        $meta_image_width = null;
+        $meta_image_height = null;
+        $meta_image_name = null;
+        $og_type = 'website';
+        
+        
+        return view(
+            'brands',
+            compact(
+                'brands',
+                'meta_title',
+                'meta_description',
+                'author',
+                'robots',
+                'canonical_url',
+                'meta_image_name',
+                'og_type',
+            )
+        );
     }
 }
