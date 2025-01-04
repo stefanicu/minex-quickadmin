d545aeda9216ec055fc01b916e08a106b0d501fc
Stefan Nicula <stefanicu@gmail.com>
2025-01-04 05:18:25 +0200

    Update ReferenceTranslation.php

--- app/Models/ReferenceTranslation.php
+++ app/Models/ReferenceTranslation.php
@@ -7,11 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class ReferenceTranslation extends Model
 {
     public $timestamps = false;
-    protected $fillable = ['online','name','slug','content','text_img1','text_img2','text_img3','text_img4','text_img5'];
-
-    public static $searchable = [
-        'name',
-        'slug',
-        'content',
-    ];
+    
+    protected $fillable = ['online', 'name', 'slug', 'content', 'text_img1', 'text_img2', 'text_img3', 'text_img4', 'text_img5'];
 }
