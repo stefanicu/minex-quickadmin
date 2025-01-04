2658f8e940e9305b5c36149e8f8e69d2c4a3c945
Stefan Nicula <stefanicu@gmail.com>
2025-01-04 12:46:20 +0200

    Update create.blade.php

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -9,18 +9,18 @@
         <div class="card-body">
             <form method="POST" action="{{ route("admin.products.store") }}" enctype="multipart/form-data">
                 @csrf
-                <div class="form-group">
-                    <div class="form-check {{ $errors->has('online') ? 'is-invalid' : '' }}">
-                        <input type="hidden" name="online" value="0">
-                        <input class="form-check-input" type="checkbox" name="online" id="online"
-                               value="0">
-                        <label class="form-check-label" for="online">{{ trans('cruds.product.fields.online') }}</label>
-                    </div>
-                    @if($errors->has('online'))
-                        <span class="text-danger">{{ $errors->first('online') }}</span>
-                    @endif
-                    <span class="help-block">{{ trans('cruds.product.fields.online_helper') }}</span>
-                </div>
+                {{--                <div class="form-group">--}}
+                {{--                    <div class="form-check {{ $errors->has('online') ? 'is-invalid' : '' }}">--}}
+                {{--                        <input type="hidden" name="online" value="0">--}}
+                {{--                        <input class="form-check-input" type="checkbox" name="online" id="online"--}}
+                {{--                               value="0">--}}
+                {{--                        <label class="form-check-label" for="online">{{ trans('cruds.product.fields.online') }}</label>--}}
+                {{--                    </div>--}}
+                {{--                    @if($errors->has('online'))--}}
+                {{--                        <span class="text-danger">{{ $errors->first('online') }}</span>--}}
+                {{--                    @endif--}}
+                {{--                    <span class="help-block">{{ trans('cruds.product.fields.online_helper') }}</span>--}}
+                {{--                </div>--}}
 
                 <div class="row">
                     <div class="form-group col-4">
