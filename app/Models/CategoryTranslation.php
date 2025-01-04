12af7659ab3dbaa3f7563789c795488e23480184
Stefan Nicula <stefanicu@gmail.com>
2025-01-04 05:17:54 +0200

    Update CategoryTranslation.php

--- app/Models/CategoryTranslation.php
+++ app/Models/CategoryTranslation.php
@@ -6,23 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class CategoryTranslation extends Model
 {
-    protected $table = 'category_translations';
-    
     public $timestamps = false;
     
     protected $fillable = [
         'online', 'name', 'slug',
         'meta_title', 'meta_description', 'author', 'robots', 'canonical_url'
-    ];
-    
-    protected $indexes = [
-        'category_id',
-        'name',
-        'online',
-    ];
-    
-    public static $searchable = [
-        'name',
-        'slug',
     ];
 }
