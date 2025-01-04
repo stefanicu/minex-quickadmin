f15fe5527f151f3350a4eb2c9c6cbd43e9ee15ce
Stefan Nicula <stefanicu@gmail.com>
2025-01-04 05:18:37 +0200

    Update create.blade.php

--- resources/views/admin/brands/create.blade.php
+++ resources/views/admin/brands/create.blade.php
@@ -9,17 +9,19 @@
         <div class="card-body">
             <form method="POST" action="{{ route("admin.brands.store") }}" enctype="multipart/form-data">
                 @csrf
-                {{--            <div class="form-group">--}}
-                {{--                <div class="form-check {{ $errors->has('online') ? 'is-invalid' : '' }}">--}}
-                {{--                    <input type="hidden" name="online" value="0">--}}
-                {{--                    <input class="form-check-input" type="checkbox" name="online" id="online" value="1" {{ old('online', 0) == 1 || old('online') === null ? 'checked' : '' }}>--}}
-                {{--                    <label class="form-check-label" for="online">{{ trans('cruds.brand.fields.online') }}</label>--}}
-                {{--                </div>--}}
-                {{--                @if($errors->has('online'))--}}
-                {{--                    <span class="text-danger">{{ $errors->first('online') }}</span>--}}
-                {{--                @endif--}}
-                {{--                <span class="help-block">{{ trans('cruds.brand.fields.online_helper') }}</span>--}}
-                {{--            </div>--}}
+                <div class="form-group">
+                    <div class="form-check {{ $errors->has('online') ? 'is-invalid' : '' }}">
+                        <input type="hidden" name="online" value="0">
+                        <input class="form-check-input" type="checkbox" name="online" id="online"
+                               value="0">
+                        <label class="form-check-label" for="online">{{ trans('cruds.brand.fields.online') }}</label>
+                    </div>
+                    @if($errors->has('online'))
+                        <span class="text-danger">{{ $errors->first('online') }}</span>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.brand.fields.online_helper') }}</span>
+                </div>
+
                 <div class="row">
                     <div class="form-group col-12 col-sm-6">
                         <label class="required" for="name">{{ trans('cruds.brand.fields.name') }}</label>
@@ -40,16 +42,34 @@
                         <span class="help-block">{{ trans('cruds.brand.fields.slug_helper') }}</span>
                     </div>
                 </div>
-                <div class="form-group">
-                    <label class="required" for="photo">{{ trans('cruds.brand.fields.photo') }}</label>
-                    <div class="needsclick dropzone {{ $errors->has('photo') ? 'is-invalid' : '' }}"
-                         id="photo-dropzone">
+
+                <div class="row align-items-center">
+
+                    <div class="form-group col-3">
+                        <label class="required" for="photo">{{ trans('cruds.brand.fields.photo') }}</label>
+                        <div class="needsclick dropzone {{ $errors->has('photo') ? 'is-invalid' : '' }} w-max text-center"
+                             id="photo-dropzone">
+                        </div>
+                        @if($errors->has('photo'))
+                            <span class="text-danger">{{ $errors->first('photo') }}</span>
+                        @endif
+                        <span class="help-block">{{ trans('cruds.brand.fields.photo_helper') }}</span>
                     </div>
-                    @if($errors->has('photo'))
-                        <span class="text-danger">{{ $errors->first('photo') }}</span>
-                    @endif
-                    <span class="help-block">{{ trans('cruds.brand.fields.photo_helper') }}</span>
+
+                    <div class="form-group col-9">
+                        <label class="" for="offline_message">{{ trans('cruds.brand.fields.offline_message') }}</label>
+                        <textarea
+                                class="form-control ckeditor {{ $errors->has('offline_message') ? 'is-invalid' : '' }}"
+                                name="offline_message"
+                                id="offline_message">{!! old('offline_message', '') !!}</textarea>
+                        @if($errors->has('offline_message'))
+                            <span class="text-danger">{{ $errors->first('offline_message') }}</span>
+                        @endif
+                        <span class="help-block">{{ trans('cruds.brand.fields.offline_message_helper') }}</span>
+                    </div>
+
                 </div>
+
                 <div class="form-group">
                     <button class="btn btn-danger" type="submit">
                         {{ trans('global.save') }}
@@ -62,6 +82,74 @@
 @endsection
 
 @section('scripts')
+    <script>
+        $(document).ready(function () {
+            function SimpleUploadAdapter(editor) {
+                editor.plugins.get('FileRepository').createUploadAdapter = function (loader) {
+                    return {
+                        upload: function () {
+                            return loader.file
+                                .then(function (file) {
+                                    return new Promise(function (resolve, reject) {
+                                        // Init request
+                                        var xhr = new XMLHttpRequest();
+                                        xhr.open('POST', '{{ route('admin.blogs.storeCKEditorImages') }}', true);
+                                        xhr.setRequestHeader('x-csrf-token', window._token);
+                                        xhr.setRequestHeader('Accept', 'application/json');
+                                        xhr.responseType = 'json';
+
+                                        // Init listeners
+                                        var genericErrorText = `Couldn't upload file: ${file.name}.`;
+                                        xhr.addEventListener('error', function () {
+                                            reject(genericErrorText)
+                                        });
+                                        xhr.addEventListener('abort', function () {
+                                            reject()
+                                        });
+                                        xhr.addEventListener('load', function () {
+                                            var response = xhr.response;
+
+                                            if (!response || xhr.status !== 201) {
+                                                return reject(response && response.message ? `${genericErrorText}\n${xhr.status} ${response.message}` : `${genericErrorText}\n ${xhr.status} ${xhr.statusText}`);
+                                            }
+
+                                            $('form').append('<input type="hidden" name="ck-media[]" value="' + response.id + '">');
+
+                                            resolve({default: response.url});
+                                        });
+
+                                        if (xhr.upload) {
+                                            xhr.upload.addEventListener('progress', function (e) {
+                                                if (e.lengthComputable) {
+                                                    loader.uploadTotal = e.total;
+                                                    loader.uploaded = e.loaded;
+                                                }
+                                            });
+                                        }
+
+                                        // Send request
+                                        var data = new FormData();
+                                        data.append('upload', file);
+                                        data.append('crud_id', '{{ $blog->id ?? 0 }}');
+                                        xhr.send(data);
+                                    });
+                                })
+                        }
+                    };
+                }
+            }
+
+            var allEditors = document.querySelectorAll('.ckeditor');
+            for (var i = 0; i < allEditors.length; ++i) {
+                ClassicEditor.create(
+                    allEditors[i], {
+                        extraPlugins: [SimpleUploadAdapter]
+                    }
+                );
+            }
+        });
+    </script>
+
     <script>
         Dropzone.options.photoDropzone = {
             url: '{{ route('admin.brands.storeMedia') }}',
