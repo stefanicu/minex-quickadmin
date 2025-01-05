0c2e086cb205ac49f95514b7bb68f105a68007ae
Stefan Nicula <stefanicu@gmail.com>
2025-01-05 13:06:14 +0200

    Update langRoute.php

--- app/Helpers/langRoute.php
+++ app/Helpers/langRoute.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( ! function_exists('langRoute')) {
-    function langRoute($name, $parameters = [], $absolute = true)
+    function langRoute($name, $parameters = [], $absolute = true): string
     {
         $parameters['lang'] = app()->getLocale();
         return route($name, $parameters, $absolute);
