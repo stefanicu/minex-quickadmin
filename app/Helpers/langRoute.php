f9ae3fee24c14e32fceb95ac8834428ff15151fa
Stefan Nicula <stefanicu@gmail.com>
2025-01-06 17:05:50 +0200

    Delete langRoute.php

--- app/Helpers/langRoute.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( ! function_exists('langRoute')) {
-    function langRoute($name, $parameters = [], $absolute = true): string
-    {
-        $parameters['lang'] = app()->getLocale();
-        return route($name, $parameters, $absolute);
-    }
-}
