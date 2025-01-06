e41fb7ee5d5f07ecec9c8d657992b596999eda02
Stefan Nicula <stefanicu@gmail.com>
2025-01-06 17:06:48 +0200

    Create seo.php

---
+++ resources/lang/en/seo.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'home.title' => 'Minex Group International – Insights on Surface Treatment & Metal Processing',
+    'home.description' => 'Minex Group International – your reliable partner in developing complex technical projects for surface treatment and metal processing. We offer you everything from designing and construction, to equipping and maintenance',
+    
+    'brands.title' => 'Brands | Minex Group International – Insights on Surface Treatment & Metal Processing',
+    'brands.description' => 'Explore Minex Group International’s brands: leaders in surface treatment and metal processing. Discover our design, construction, equipment, and maintenance services.',
+    
+    'references.title' => 'References | Minex Group International – Insights on Surface Treatment & Metal Processing',
+    'references.description' => 'Explore Minex Group International’s References: Showcasing successful projects in surface treatment and metal processing, including MARUB Brasov.',
+    
+    'testimonials.title' => 'Testimonials | Minex Group International – Insights on Surface Treatment & Metal Processing',
+    'testimonials.description' => 'Discover Minex Group International’s Testimonials: Hear from our clients about our expertise in surface treatment and metal processing solutions.',
+    
+    'blogs.title' => 'Blog | Minex Group International – Insights on Surface Treatment & Metal Processing',
+    'blogs.description' => 'Explore Minex Group International’s Blog. Read articles, insights, and news on surface treatment, metal processing, and industry innovations.',
+];
