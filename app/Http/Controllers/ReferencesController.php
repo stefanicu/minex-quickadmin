3016c0364acf4b2307957aef5a6ccc3bb8a97ff2
Stefan Nicula <stefanicu@gmail.com>
2025-01-06 17:06:32 +0200

    Update ReferencesController.php

--- app/Http/Controllers/ReferencesController.php
+++ app/Http/Controllers/ReferencesController.php
@@ -26,7 +26,12 @@ class ReferencesController extends Controller
         
         $slug = trans('pages_slugs.references', [], app()->getLocale());
         
-        return view('references', compact('references', 'slug', 'industries', 'industries_in_tab'));
+        $metaData = getStaticMetaData([
+            'meta_title' => trans('seo.references.title'),
+            'meta_description' => trans('seo.references.description'),
+        ]);
+        
+        return view('references', compact('references', 'slug', 'industries', 'industries_in_tab', 'metaData'));
     }
     
     public static function industries_id_in_tab()
