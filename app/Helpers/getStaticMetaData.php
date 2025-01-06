da04a298e4c658ddf9e722e87126e59a634070af
Stefan Nicula <stefanicu@gmail.com>
2025-01-06 17:05:42 +0200

    Update getStaticMetaData.php

--- app/Helpers/getStaticMetaData.php
+++ app/Helpers/getStaticMetaData.php
@@ -1,20 +1,22 @@
 <?php
 
-function getStaticMetaData(array $overrides = []): array
-{
-    // Default meta data
-    $defaultMeta = [
-        'meta_title' => null,
-        'meta_description' => null,
-        'canonical_url' => null,
-        'author' => 'Minex Group International',
-        'meta_image_url' => null,
-        'meta_image_width' => null,
-        'meta_image_height' => null,
-        'meta_image_name' => null,
-        'og_type' => 'website',
-    ];
-    
-    // Allow overriding defaults
-    return array_merge($defaultMeta, $overrides);
+if ( ! function_exists('getStaticMetaData')) {
+    function getStaticMetaData(array $overrides = []): array
+    {
+        // Default meta data
+        $defaultMeta = [
+            'meta_title' => null,
+            'meta_description' => null,
+            'canonical_url' => null,
+            'author' => 'Minex Group International',
+            'meta_image_url' => null,
+            'meta_image_width' => null,
+            'meta_image_height' => null,
+            'meta_image_name' => null,
+            'og_type' => 'website',
+        ];
+        
+        // Allow overriding defaults
+        return array_merge($defaultMeta, $overrides);
+    }
 }
