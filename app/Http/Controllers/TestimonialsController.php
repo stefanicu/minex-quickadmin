5e3318de539df7f88b91792bc299074a5da70be7
Stefan Nicula <stefanicu@gmail.com>
2025-01-06 17:06:35 +0200

    Update TestimonialsController.php

--- app/Http/Controllers/TestimonialsController.php
+++ app/Http/Controllers/TestimonialsController.php
@@ -2,19 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Application;
 use App\Models\Testimonial;
 
 class TestimonialsController extends Controller
 {
     public function index()
     {
-        $testimonials = Testimonial::leftJoin('testimonial_translations','testimonials.id', '=', 'testimonial_translations.testimonial_id')
-            ->where('testimonial_translations.online','=',1)
-            ->where('locale','=',app()->getLocale())
-            ->select('testimonials.id as id','testimonial_translations.company')
+        $testimonials = Testimonial::leftJoin('testimonial_translations', 'testimonials.id', '=', 'testimonial_translations.testimonial_id')
+            ->where('testimonial_translations.online', '=', 1)
+            ->where('locale', '=', app()->getLocale())
+            ->select('testimonials.id as id', 'testimonial_translations.company')
             ->orderBy('company')->get();
-
-        return view('testimonials', compact('testimonials'));
+        
+        $metaData = getStaticMetaData([
+            'meta_title' => trans('seo.testimonials.title'),
+            'meta_description' => trans('seo.testimonials.description'),
+        ]);
+        
+        return view('testimonials', compact('testimonials', 'metaData'));
     }
 }
