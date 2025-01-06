3188c81c0f69e7b7ee54b7577124543b31e0866d
Stefan Nicula <stefanicu@gmail.com>
2025-01-06 17:06:56 +0200

    Create seo.php

---
+++ resources/lang/ro/seo.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'home.title' => 'Minex Group International – Informații despre Tratarea Suprafațelor și Prelucrarea Metalelor',
+    'home.description' => 'Minex Group International – partenerul dumneavoastră de încredere în dezvoltarea proiectelor tehnice complexe pentru tratarea suprafețelor și prelucrarea metalelor. Oferim totul, de la proiectare și construcție, la echipare și întreținere.',
+    
+    'brands.title' => 'Mărci | Minex Group International – Informații despre Tratarea Suprafațelor și Prelucrarea Metalelor',
+    'brands.description' => 'Explorați mărcile Minex Group International: lideri în tratarea suprafețelor și prelucrarea metalelor. Descoperiți serviciile noastre de proiectare, construcție, echipamente și întreținere.',
+    
+    'references.title' => 'Referințe | Minex Group International – Informații despre Tratarea Suprafațelor și Prelucrarea Metalelor',
+    'references.description' => 'Explorați referințele Minex Group International: prezentăm proiecte de succes în tratarea suprafețelor și prelucrarea metalelor, inclusiv MARUB Brașov.',
+    
+    'testimonials.title' => 'Testimoniale | Minex Group International – Informații despre Tratarea Suprafațelor și Prelucrarea Metalelor',
+    'testimonials.description' => 'Descoperiți testimoniale Minex Group International: ascultați de la clienții noștri despre expertiza noastră în soluții de tratare a suprafețelor și prelucrare a metalelor.',
+    
+    'blogs.title' => 'Blog | Minex Group International – Informații despre Tratarea Suprafațelor și Prelucrarea Metalelor',
+    'blogs.description' => 'Descoperiți Blogul Minex Group International. Citiți articole, informații și știri despre tratarea suprafețelor, prelucrarea metalelor și inovații din industrie.',
+];
