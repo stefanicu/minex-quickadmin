c38dc67aece10225a8c30624a8f916d9b9a86ee8
Stefan Nicula <stefanicu@gmail.com>
2025-01-11 21:34:07 +0200

    Update 500.blade.php

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -10,26 +10,30 @@
             text-align: center;
             margin: 50px;
         }
+
         h1 {
             font-size: 3rem;
             color: #333;
         }
+
         p {
             font-size: 1.2rem;
             color: #666;
         }
+
         a {
             color: #007bff;
             text-decoration: none;
         }
+
         a:hover {
             text-decoration: underline;
         }
     </style>
 </head>
 <body>
-    <h1>Error 500</h1>
-    <p>Internal Server Error: Something went wrong on our end.</p>
-    <a href="https://minex-quickadmin.test">Go to Homepage</a>
+<h1>Error 500</h1>
+<p>Internal Server Error: Something went wrong on our end.</p>
+<a href="{{ route('home.'.app()->getLocale()) }}">Go to Homepage</a>
 </body>
 </html>
