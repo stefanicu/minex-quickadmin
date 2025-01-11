aa7632d017d5f934595450e1da3f1e3375857f7d
Stefan Nicula <stefanicu@gmail.com>
2025-01-11 21:34:03 +0200

    Update 403.blade.php

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -10,26 +10,30 @@
             text-align: center;
             margin: 50px;
         }
+
         h1 {
             font-size: 3rem;
             color: #333;
         }
+
         p {
             font-size: 1.2rem;
             color: #666;
         }
+
         a {
             color: #007bff;
             text-decoration: none;
         }
+
         a:hover {
             text-decoration: underline;
         }
     </style>
 </head>
 <body>
-    <h1>Error 403</h1>
-    <p>Forbidden: You do not have permission to access this page.</p>
-    <a href="https://minex-quickadmin.test">Go to Homepage</a>
+<h1>Error 403</h1>
+<p>Forbidden: You do not have permission to access this page.</p>
+<a href="{{ route('home.'.app()->getLocale()) }}">Go to Homepage</a>
 </body>
 </html>
