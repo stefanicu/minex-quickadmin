600f46dbe0c03c18d97165cf84077ac456ce1088
Stefan Nicula <stefanicu@gmail.com>
2025-01-11 21:34:05 +0200

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,26 +10,30 @@
             text-align: center;
             margin: 50px;
         }
+
         h1 {
             font-size: 3rem;
             color: #333;
         }
+
         p {
             font-size: 1.2rem;
             color: #666;
         }
+
         a {
             color: #007bff;
             text-decoration: none;
         }
+
         a:hover {
             text-decoration: underline;
         }
     </style>
 </head>
 <body>
-    <h1>Error 404</h1>
-    <p>Page Not Found: The page you are looking for does not exist.</p>
-    <a href="https://minex-quickadmin.test">Go to Homepage</a>
+<h1>Error 404</h1>
+<p>Page Not Found: The page you are looking for does not exist.</p>
+<a href="{{ route('home.'.app()->getLocale()) }}">Go to Homepage</a>
 </body>
 </html>
