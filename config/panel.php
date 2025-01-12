f39a6921c427bdfa0111c7030243e59ddfcbe017
Stefan Nicula <stefanicu@gmail.com>
2025-01-13 00:08:24 +0200

    Update panel.php

--- config/panel.php
+++ config/panel.php
@@ -1,13 +1,23 @@
 <?php
 
 return [
-    'date_format'         => 'd.m.Y',
-    'time_format'         => 'H:i:s',
-    'primary_language'    => 'en',
+    'date_format' => 'd.m.Y',
+    'time_format' => 'H:i:s',
+    'primary_language' => 'en',
     'available_languages' => [
         'en' => 'English',
         'ro' => 'Romanian',
         'bg' => 'Bulgarian',
+        'lt' => 'Lithuanian',
+//        'lv' => 'Latvian',
+//        'et' => 'Estonian',
+//        'sr' => 'Serbian',
+//        'hr' => 'Croatian',
+//        'sl' => 'Slovenian',
+//        'bs' => 'Bosnian',
+//        'mk' => 'Macedonian',
+//        'hu' => 'Hungarian',
+//        'uk' => 'Ukrainian',
     ],
 
 ];
