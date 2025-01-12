fb287a8238e0588616081fb841d13950b01db065
Stefan Nicula <stefanicu@gmail.com>
2025-01-13 00:11:11 +0200

    Create admin.php

---
+++ resources/lang/en/admin.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'translation_type' => 'The type of translation',
+    'translation_total_online_items_count' => 'Total online items',
+    'translation_items' => 'Items',
+    'translation_db_models' => 'DB models',
+    'translation_static_strings' => 'Static strings',
+    
+    'translation_home' => 'Front Page Sections',
+    'translation_applications' => 'Applications',
+    'translation_categories' => 'Categories',
+    'translation_brands' => 'Brands',
+    'translation_products' => 'Products',
+    'translation_references' => 'References',
+    'translation_industries' => 'Industries',
+    'translation_testimonials' => 'Testimonials',
+    'translation_blog' => 'Articles',
+];
