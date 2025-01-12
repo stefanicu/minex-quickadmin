278fe31fc0fd03ffaec8d69e75680fffccc2da9f
Stefan Nicula <stefanicu@gmail.com>
2025-01-13 00:28:26 +0200

    Update strings.blade.php

--- resources/views/admin/translations/strings.blade.php
+++ resources/views/admin/translations/strings.blade.php
@@ -16,31 +16,38 @@
                 @endforeach
             </ul>
 
-            <div class="tab-content mt-3">
-                @foreach($languages as $lang)
-                    <div class="tab-pane fade @if($loop->first) show active @endif" id="{{ $lang }}">
-                        @foreach($translations[$lang] as $file => $keys)
-                            <h4>{{ $file }}</h4>
+            <div class="form-group">
+                <label for="file-select">Select File:</label>
+                <select id="file-select" class="form-control" onchange="location.href='?file='+this.value;">
+                    <option value="">-- Select File --</option>
+                    @foreach($translations[$languages[0]] as $file => $keys)
+                        <option value="{{ $file }}" {{ request('file') == $file ? 'selected' : '' }}>{{ $file }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            @if(request('file'))
+                <div class="tab-content mt-3">
+                    @foreach($languages as $lang)
+                        <div class="tab-pane fade @if($loop->first) show active @endif" id="{{ $lang }}">
+                            <h4>{{ ucfirst($lang) }}</h4>
                             <form method="POST" action="{{ route('admin.translation.strings.update', $lang) }}">
                                 @csrf
-                                <input type="hidden" name="file" value="{{ $file }}">
+                                <input type="hidden" name="file" value="{{ request('file') }}">
 
-                                @foreach($keys as $key => $value)
+                                @foreach($translations[$lang][request('file')] as $key => $value)
                                     <div class="form-group">
                                         <label>{{ $key }}</label>
-                                        <input type="text"
-                                               name="translations[{{ $key }}]"
-                                               value="{{ $value }}"
-                                               class="form-control">
+                                        <input type="text" name="translations[{{ $key }}]" value="{{ $value }}" class="form-control">
                                     </div>
                                 @endforeach
 
                                 <button type="submit" class="btn btn-primary">Save</button>
                             </form>
-                        @endforeach
-                    </div>
-                @endforeach
-            </div>
+                        </div>
+                    @endforeach
+                </div>
+            @endif
 
         </div>
     </div>
