c0e744fb948ab5df1063a36bf0ce42bde3c23309
Stefan Nicula <stefanicu@gmail.com>
2025-01-20 13:10:04 +0200

    Update seo.php

--- resources/lang/lt/seo.php
+++ resources/lang/lt/seo.php
@@ -1,14 +1,14 @@
 <?php
 
 return array (
-  'home_title' => '',
-  'home_description' => '',
-  'brand_title' => '',
-  'brand_description' => '',
-  'reference_title' => '',
-  'reference_description' => '',
-  'testimonial_title' => '',
-  'testimonial_description' => '',
-  'blog_title' => '',
-  'blog_description' => '',
+  'home_title' => 'Minex Group International – Įžvalgos apie paviršiaus apdorojimą ir metalo apdirbimą',
+  'home_description' => 'Minex Group International – jūsų patikimas partneris kuriant sudėtingus techninius projektus paviršių apdorojimui ir metalo apdirbimui. Mes siūlome jums viską nuo projektavimo ir statybos iki įrangos ir priežiūros.',
+  'brand_title' => 'Prekės ženklai | Minex Group International – Įžvalgos apie paviršiaus apdorojimą ir metalo apdirbimą',
+  'brand_description' => 'Ištirkite Minex Group International prekės ženklus: paviršiaus apdorojimo ir metalo apdirbimo lyderius. Atraskite mūsų projektavimo, statybos, įrangos ir priežiūros paslaugas.',
+  'reference_title' => 'Nuorodos | Minex Group International – Įžvalgos apie paviršiaus apdorojimą ir metalo apdirbimą',
+  'reference_description' => 'Tyrinėkite „Minex Group International“ nuorodas: Pavyzdžiai sėkmingų paviršiaus apdorojimo ir metalo apdirbimo projektų, įskaitant MARUB Brašovas.',
+  'testimonial_title' => 'Atsiliepimai | Minex Group International – Įžvalgos apie paviršiaus apdorojimą ir metalo apdirbimą',
+  'testimonial_description' => 'Atraskite „Minex Group International“ Klientų atsiliepimus: Išgirskite iš mūsų klientų apie mūsų patirtį paviršiaus apdorojimo ir metalo apdorojimo sprendimuose.',
+  'blog_title' => 'Blogas | Minex Group International – Įžvalgos apie Paviršiaus Apdorojimą ir Metalo Apdirbimą',
+  'blog_description' => 'Naršykite Minex Group International tinklaraštį. Skaitykite straipsnius, įžvalgas ir naujienas apie paviršiaus apdorojimą, metalo apdorojimą ir pramonės naujoves.',
 );
