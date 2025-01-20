30f31d32197ea97ef0537471d000691e082cc947
Stefan Nicula <stefanicu@gmail.com>
2025-01-20 13:10:09 +0200

    Update edit.blade.php

--- resources/views/admin/applications/edit.blade.php
+++ resources/views/admin/applications/edit.blade.php
@@ -2,8 +2,23 @@
 @section('content')
 
     <div class="card">
-        <div class="card-header">
-            {{ trans('global.edit') }} {{ trans('cruds.application.title_singular') }}
+        <div class="card-header d-flex justify-content-between">
+
+            <div class="w-50">{{ trans('global.edit') }} {{ trans('cruds.application.title_singular') }}</div>
+            <div class="w-50 text-right">
+                @if(array_key_exists(app()->getLocale(), config('panel.available_languages')) && $application->translate(app()->getLocale()))
+                    <a class="blue" href="{{ route('categories.'.app()->getLocale(), ['app_slug' => $application->slug]) }}" target="_blank">
+                        <svg class="mr-1" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g id="Interface / External_Link">
+                                <path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="#003eff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </g>
+                        </svg>
+                        Preview
+                    </a>
+                @else
+                    {{ __('admin.no_page_link_yet') }}
+                @endif
+            </div>
         </div>
 
         <div class="card-body">
@@ -44,17 +59,17 @@
                     </div>
                 </div>
 
-                {{--                <div class="row align-items-center">--}}
-                {{--                    <div class="form-group col-2 align-items-center">--}}
-                {{--                        <label for="image">{{ trans('cruds.application.fields.image') }}</label>--}}
-                {{--                        <div class="needsclick dropzone {{ $errors->has('image') ? 'is-invalid' : '' }}"--}}
-                {{--                             id="image-dropzone"></div>--}}
-                {{--                        @if($errors->has('image'))--}}
-                {{--                            <span class="text-danger">{{ $errors->first('image') }}</span>--}}
-                {{--                        @endif--}}
-                {{--                        <span class="help-block">{{ trans('cruds.application.fields.image_helper') }}</span>--}}
-                {{--                    </div>--}}
-                {{--                </div>--}}
+                <div class="row align-items-center">
+                    <div class="form-group col-12 align-items-center">
+                        <label for="image">{{ trans('cruds.application.fields.image') }}</label>
+                        <div class="needsclick dropzone {{ $errors->has('image') ? 'is-invalid' : '' }}"
+                             id="image-dropzone"></div>
+                        @if($errors->has('image'))
+                            <span class="text-danger">{{ $errors->first('image') }}</span>
+                        @endif
+                        <span class="help-block">{{ trans('cruds.application.fields.image_helper') }}</span>
+                    </div>
+                </div>
 
                 {{--                <div class="form-group">--}}
                 {{--                    <label for="categories">{{ trans('cruds.application.fields.categories') }}</label>--}}
@@ -99,6 +114,19 @@
                     </button>
                 </div>
             </form>
+
+            <form id="translateButtonForm" method="POST" class="" action="{{ route("admin.translation.granular") }}" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <div class="form-check {{ $errors->has('online') ? 'is-invalid' : '' }}">
+                        <input type="hidden" name="id" id="id" value="{{ $application->id }}">
+                        <input type="hidden" name="model_translation" id="model_translation" value="application_translations">
+                        <input type="hidden" name="foreign_key" id="foreign_key" value="application_id">
+                        <input type="hidden" name="language" id="language" value="{{ app()->getLocale() }}">
+                        <button type="submit" class="btn btn-success">{{ trans('admin.translation_button', ['language' => strtoupper(app()->getLocale())]) }}</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 
@@ -118,8 +146,6 @@
             },
             params: {
                 size: 1,
-                width: 120,
-                height: 120
             },
             success: function (file, response) {
                 $('form').find('input[name="image"]').remove()
