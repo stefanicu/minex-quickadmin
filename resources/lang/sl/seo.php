56a436d9ac937dd8c88cb4cf5de63b6891fd6342
Stefan Nicula <stefanicu@gmail.com>
2025-01-23 00:01:41 +0200

    Update seo.php

--- resources/lang/sl/seo.php
+++ resources/lang/sl/seo.php
@@ -1,14 +1,14 @@
 <?php
 
-return array (
-  'home_title' => 'Minex Group International – Vpogledi v Površinsko Obdelavo in Obdelavo Kovin',
-  'home_description' => 'Minex Group International – vaš zanesljiv partner pri razvoju kompleksnih tehničnih projektov za površinsko obdelavo in predelavo kovin. Ponujamo vam vse od načrtovanja in gradnje, do opremljanja in vzdrževanja.',
-  'brand_title' => 'Blagovne znamke | Minex Group International – Vpogledi v Površinsko Obravnava in Obdelava Kovin',
-  'brand_description' => 'Raziščite blagovne znamke skupine Minex Group International: vodilni na področju obdelave površin in obdelave kovin. Odkrijte našo zasnovo, gradnjo, opremo in storitve vzdrževanja.',
-  'reference_title' => 'Reference | Minex Group International – Pogledi na Površinsko Obdelavo in Obdelavo Kovin',
-  'reference_description' => 'Raziskujte Reference Minex Group International: Prikazuje uspešne projekte pri površinski obdelavi in obdelavi kovin, vključno z MARUB Brașov.',
-  'testimonial_title' => 'Izpovedi | Minex Group International – Vpogledi v površinsko obdelavo in obdelavo kovin',
-  'testimonial_description' => 'Odkrijte mnenja strank Minex Group International: Slišite od naših strank o naši strokovnosti pri rešitvah za površinsko obdelavo in obdelavo kovin.',
-  'blog_title' => 'Blog | Minex Group International – Vpogledi v površinsko obdelavo in obdelavo kovin',
-  'blog_description' => 'Raziščite blog skupine Minex Group International. Preberite članke, vpoglede in novice o površinski obdelavi, obdelavi kovin in inovacijah v industriji.',
+return array(
+    'home_title' => 'Minex Group International – Vpogledi v Površinsko Obdelavo in Obdelavo Kovin',
+    'home_description' => 'Minex Group International – vaš zanesljiv partner pri razvoju kompleksnih tehničnih projektov za površinsko obdelavo in predelavo kovin. Ponujamo vam vse od načrtovanja in gradnje, do opremljanja in vzdrževanja.',
+    'brands_title' => 'Blagovne znamke | Minex Group International – Vpogledi v Površinsko Obravnava in Obdelava Kovin',
+    'brands_description' => 'Raziščite blagovne znamke skupine Minex Group International: vodilni na področju obdelave površin in obdelave kovin. Odkrijte našo zasnovo, gradnjo, opremo in storitve vzdrževanja.',
+    'references_title' => 'Reference | Minex Group International – Pogledi na Površinsko Obdelavo in Obdelavo Kovin',
+    'references_description' => 'Raziskujte Reference Minex Group International: Prikazuje uspešne projekte pri površinski obdelavi in obdelavi kovin, vključno z MARUB Brașov.',
+    'testimonials_title' => 'Izpovedi | Minex Group International – Vpogledi v površinsko obdelavo in obdelavo kovin',
+    'testimonials_description' => 'Odkrijte mnenja strank Minex Group International: Slišite od naših strank o naši strokovnosti pri rešitvah za površinsko obdelavo in obdelavo kovin.',
+    'blogs_title' => 'Blog | Minex Group International – Vpogledi v površinsko obdelavo in obdelavo kovin',
+    'blogs_description' => 'Raziščite blog skupine Minex Group International. Preberite članke, vpoglede in novice o površinski obdelavi, obdelavi kovin in inovacijah v industriji.',
 );
