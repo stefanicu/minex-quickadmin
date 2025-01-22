08ead055432c46d53d578eb00c93ae01319c7731
Stefan Nicula <stefanicu@gmail.com>
2025-01-23 00:01:37 +0200

    Update seo.php

--- resources/lang/rs/seo.php
+++ resources/lang/rs/seo.php
@@ -1,14 +1,14 @@
 <?php
 
-return array (
-  'home_title' => 'Minex Group International – Uvidi u Obradu Površina & Obradu Metala',
-  'home_description' => 'Minex Group International – vaš pouzdan partner u razvoju složenih tehničkih projekata za obradu površina i obradu metala. Nudimo vam sve od projektovanja i izgradnje, do opremanja i održavanja.',
-  'brand_title' => 'Brendovi | Minex Group International – Uvidi u Površinsku Obradu i Obradu Metala',
-  'brand_description' => 'Istražite Minex Group International brendove: lidere u površinskoj obradi i preradi metala. Otkrijte naš dizajn, izgradnju, opremu i uslugu održavanja.',
-  'reference_title' => 'Reference | Minex Group International – Uvidi o površinskoj obradi i preradi metala',
-  'reference_description' => 'Istražite reference kompanije Minex Group International: Prikazivanje uspešnih projekata u površinskoj obradi i obradi metala, uključujući MARUB Brašov.',
-  'testimonial_title' => 'Testimonijali | Minex Group International – Uvidi u Obradu Površina i Obradu Metala',
-  'testimonial_description' => 'Otkrijte Minex Group International-ove Preporuke: Čujte od naših klijenata o našem stručnom znanju u tretmanu površina i rešenju za obradu metala.',
-  'blog_title' => 'Blog | Minex Group International – Uvidi o Površinskoj Obradi i Obradi Metala',
-  'blog_description' => 'Istražite Blog Minex Group International. Čitajte članke, uvide i vesti o površinskoj obradi, obradi metala i inovacijama u industriji.',
+return array(
+    'home_title' => 'Minex Group International – Uvidi u Obradu Površina & Obradu Metala',
+    'home_description' => 'Minex Group International – vaš pouzdan partner u razvoju složenih tehničkih projekata za obradu površina i obradu metala. Nudimo vam sve od projektovanja i izgradnje, do opremanja i održavanja.',
+    'brands_title' => 'Brendovi | Minex Group International – Uvidi u Površinsku Obradu i Obradu Metala',
+    'brands_description' => 'Istražite Minex Group International brendove: lidere u površinskoj obradi i preradi metala. Otkrijte naš dizajn, izgradnju, opremu i uslugu održavanja.',
+    'references_title' => 'Reference | Minex Group International – Uvidi o površinskoj obradi i preradi metala',
+    'references_description' => 'Istražite reference kompanije Minex Group International: Prikazivanje uspešnih projekata u površinskoj obradi i obradi metala, uključujući MARUB Brašov.',
+    'testimonials_title' => 'Testimonijali | Minex Group International – Uvidi u Obradu Površina i Obradu Metala',
+    'testimonials_description' => 'Otkrijte Minex Group International-ove Preporuke: Čujte od naših klijenata o našem stručnom znanju u tretmanu površina i rešenju za obradu metala.',
+    'blogs_title' => 'Blog | Minex Group International – Uvidi o Površinskoj Obradi i Obradi Metala',
+    'blogs_description' => 'Istražite Blog Minex Group International. Čitajte članke, uvide i vesti o površinskoj obradi, obradi metala i inovacijama u industriji.',
 );
