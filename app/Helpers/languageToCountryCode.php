8bd297735bc11554b91f2591de5a32fb76874c9b
Stefan Nicula <stefanicu@gmail.com>
2025-01-22 02:06:09 +0200

    Create languageToCountryCode.php

---
+++ app/Helpers/languageToCountryCode.php
@@ -0,0 +1,30 @@
+<?php
+
+if ( ! function_exists('languageToCountryCode')) {
+    /**
+     * Convert a language code to a country code.
+     *
+     * @param  string  $languageCode
+     * @return string|null
+     */
+    function languageToCountryCode(string $languageCode): ?string
+    {
+        $mapping = [
+            'en' => 'US', // English - USA (or use GB for United Kingdom)
+            'ro' => 'RO', // Romanian - Romania
+            'bg' => 'BG', // Bulgarian - Bulgaria
+            'lt' => 'LT', // Lithuanian - Lithuania
+            'lv' => 'LV', // Latvian - Latvia
+            'et' => 'EE', // Estonian - Estonia
+            'rs' => 'RS', // Serbian - Serbia
+            'hr' => 'HR', // Croatian - Croatia
+            'sl' => 'SI', // Slovenian - Slovenia
+            'bs' => 'BA', // Bosnian - Bosnia and Herzegovina
+            'mk' => 'MK', // Macedonian - North Macedonia
+            'hu' => 'HU', // Hungarian - Hungary
+            'uk' => 'UA', // Ukrainian - Ukraine
+        ];
+        
+        return $mapping[$languageCode] ?? null; // Return country code or null if not found
+    }
+}
