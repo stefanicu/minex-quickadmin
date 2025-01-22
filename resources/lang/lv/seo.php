59afab7197e2e7773f83ff9f2cdcbe5c4ec3a180
Stefan Nicula <stefanicu@gmail.com>
2025-01-23 00:01:29 +0200

    Update seo.php

--- resources/lang/lv/seo.php
+++ resources/lang/lv/seo.php
@@ -1,14 +1,14 @@
 <?php
 
-return array (
-  'home_title' => 'Minex Group International – Ieskati virsmas apstrādē un metālapstrādē',
-  'home_description' => 'Minex Group International – jūsu uzticamais partneris sarežģītu tehnisko projektu izstrādē virsmas apstrādei un metālapstrādei. Mēs piedāvājam jums visu, sākot no projektēšanas un būvniecības līdz aprīkošanai un apkopei.',
-  'brand_title' => 'Zīmoli | Minex Group International – Ieskats Virsmas Apstrādē un Metālapstrādē',
-  'brand_description' => 'Izpētiet Minex Group International zīmolus: līderi virsmas apstrādē un metālapstrādē. Atklājiet mūsu dizaina, būvniecības, aprīkojuma un uzturēšanas pakalpojumus.',
-  'reference_title' => 'Atsauces | Minex Group International – Ieskati virsmas apstrādē un metāla apstrādē',
-  'reference_description' => 'Izpētiet Minex Group International atsauces: Demonstrējot veiksmīgus projektus virsmas apstrādē un metālapstrādē, tostarp MARUB Brasov.',
-  'testimonial_title' => 'Atsauksmes | Minex Group International – Ieskats Virsmas Apstrādē un Metālapstrādē',
-  'testimonial_description' => 'Atklājiet Minex Group International atsauksmes: Uzziniet no mūsu klientiem par mūsu kompetenci virsmas apstrādes un metālapstrādes risinājumos.',
-  'blog_title' => 'Blogs | Minex Group International – Ieskats virsmas apstrādē un metālapstrādē',
-  'blog_description' => 'Izpētīt Minex Group International emuāru. Lasīt rakstus, ieskatus, un jaunumus par virsmas apstrādi, metāla apstrādi, un nozares inovācijām.',
+return array(
+    'home_title' => 'Minex Group International – Ieskati virsmas apstrādē un metālapstrādē',
+    'home_description' => 'Minex Group International – jūsu uzticamais partneris sarežģītu tehnisko projektu izstrādē virsmas apstrādei un metālapstrādei. Mēs piedāvājam jums visu, sākot no projektēšanas un būvniecības līdz aprīkošanai un apkopei.',
+    'brands_title' => 'Zīmoli | Minex Group International – Ieskats Virsmas Apstrādē un Metālapstrādē',
+    'brands_description' => 'Izpētiet Minex Group International zīmolus: līderi virsmas apstrādē un metālapstrādē. Atklājiet mūsu dizaina, būvniecības, aprīkojuma un uzturēšanas pakalpojumus.',
+    'references_title' => 'Atsauces | Minex Group International – Ieskati virsmas apstrādē un metāla apstrādē',
+    'references_description' => 'Izpētiet Minex Group International atsauces: Demonstrējot veiksmīgus projektus virsmas apstrādē un metālapstrādē, tostarp MARUB Brasov.',
+    'testimonials_title' => 'Atsauksmes | Minex Group International – Ieskats Virsmas Apstrādē un Metālapstrādē',
+    'testimonials_description' => 'Atklājiet Minex Group International atsauksmes: Uzziniet no mūsu klientiem par mūsu kompetenci virsmas apstrādes un metālapstrādes risinājumos.',
+    'blogs_title' => 'Blogs | Minex Group International – Ieskats virsmas apstrādē un metālapstrādē',
+    'blogs_description' => 'Izpētīt Minex Group International emuāru. Lasīt rakstus, ieskatus, un jaunumus par virsmas apstrādi, metāla apstrādi, un nozares inovācijām.',
 );
