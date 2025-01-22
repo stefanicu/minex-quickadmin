23e572b63e6495d549c4588b71057cd72b70e2fe
Stefan Nicula <stefanicu@gmail.com>
2025-01-23 00:01:43 +0200

    Update seo.php

--- resources/lang/uk/seo.php
+++ resources/lang/uk/seo.php
@@ -1,14 +1,14 @@
 <?php
 
-return array (
-  'home_title' => 'Minex Group International – Огляд щодо обробки поверхонь та обробки металів',
-  'home_description' => 'Minex Group International – ваш надійний партнер у розробці складних технічних проектів для обробки поверхонь та металообробки. Ми пропонуємо вам все – від проектування та будівництва, до оснащення та обслуговування',
-  'brand_title' => 'Бренди | Minex Group International – Ідеї щодо обробки поверхонь та обробки металу',
-  'brand_description' => 'Досліджуйте бренди Minex Group International: лідери в обробці поверхонь та металообробці. Відкрийте для себе наш дизайн, будівництво, обладнання та обслуговування.',
-  'reference_title' => 'Посилання | Minex Group International – Ідеї щодо обробки поверхонь та обробки металів',
-  'reference_description' => 'Досліджуйте Посилання Minex Group International: Демонстрація успішних проектів у поверхневій обробці та обробці металів, включаючи MARUB Brasov.',
-  'testimonial_title' => 'Відгуки | Minex Group International – Погляди на обробку поверхні та металообробку',
-  'testimonial_description' => 'Відкрийте для себе відгуки Minex Group International: Дізнайтеся від наших клієнтів про наш досвід у галузі обробки поверхонь та рішень для обробки металу.',
-  'blog_title' => 'Блог | Minex Group International – Ідеї щодо обробки поверхні та обробки металу',
-  'blog_description' => 'Досліджуйте блог Minex Group International. Читайте статті, аналітичні матеріали та новини про обробку поверхонь, обробку металу та інновації в галузі.',
+return array(
+    'home_title' => 'Minex Group International – Огляд щодо обробки поверхонь та обробки металів',
+    'home_description' => 'Minex Group International – ваш надійний партнер у розробці складних технічних проектів для обробки поверхонь та металообробки. Ми пропонуємо вам все – від проектування та будівництва, до оснащення та обслуговування',
+    'brands_title' => 'Бренди | Minex Group International – Ідеї щодо обробки поверхонь та обробки металу',
+    'brands_description' => 'Досліджуйте бренди Minex Group International: лідери в обробці поверхонь та металообробці. Відкрийте для себе наш дизайн, будівництво, обладнання та обслуговування.',
+    'references_title' => 'Посилання | Minex Group International – Ідеї щодо обробки поверхонь та обробки металів',
+    'references_description' => 'Досліджуйте Посилання Minex Group International: Демонстрація успішних проектів у поверхневій обробці та обробці металів, включаючи MARUB Brasov.',
+    'testimonials_title' => 'Відгуки | Minex Group International – Погляди на обробку поверхні та металообробку',
+    'testimonials_description' => 'Відкрийте для себе відгуки Minex Group International: Дізнайтеся від наших клієнтів про наш досвід у галузі обробки поверхонь та рішень для обробки металу.',
+    'blogs_title' => 'Блог | Minex Group International – Ідеї щодо обробки поверхні та обробки металу',
+    'blogs_description' => 'Досліджуйте блог Minex Group International. Читайте статті, аналітичні матеріали та новини про обробку поверхонь, обробку металу та інновації в галузі.',
 );
