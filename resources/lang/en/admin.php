5e44ce57b812fcb828e565a9c72d924a445c2e8c
Stefan Nicula <stefanicu@gmail.com>
2025-01-23 18:28:13 +0200

    Update admin.php

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -7,6 +7,8 @@ return [
     'translation_total_online_items_count' => 'Total online items',
     'translation_items' => 'Items',
     'translation_db_models' => 'DB models',
+    'translation_bulk' => 'Translate All models bulk',
+    'translation_reset_language' => 'Reset all data by language',
     'translation_static_strings' => 'Static strings',
     'image_dimensions' => 'The uploaded image does not meet the required dimensions. Expected: :expected_width x :expected_height pixels. Main Photo uploaded: :uploaded_width x :uploaded_height pixels.',
     'multi_image_dimensions' => 'The uploaded image does not meet the required dimensions. Expected: :expected_width x :expected_height pixels. Multi Photo #:index uploaded: :uploaded_width x :uploaded_height pixels.',
