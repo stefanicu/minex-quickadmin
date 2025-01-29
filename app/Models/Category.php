794b57bc66c7dbf10884f3bca2383c20dcf4d145
Stefan Nicula <stefanicu@gmail.com>
2025-01-29 22:50:51 +0200

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,6 @@ use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
 use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
@@ -14,7 +13,7 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Category extends Model implements HasMedia, TranslatableContract
 {
-    use SoftDeletes, InteractsWithMedia, Translatable;
+    use InteractsWithMedia, Translatable;
     
     public $table = 'categories';
     
@@ -63,14 +62,14 @@ class Category extends Model implements HasMedia, TranslatableContract
         $this->addMediaConversion('preview')->width(432);
         
         $this->addMediaConversion('xl')->fit('crop', 1920, 540);
-//        $this->addMediaConversion('lg')->fit( 'crop',1366,400);
-//        $this->addMediaConversion('md')->fit( 'crop',768,400);
-//        $this->addMediaConversion('sm')->fit('crop', 425,200);
+        //        $this->addMediaConversion('lg')->fit( 'crop',1366,400);
+        //        $this->addMediaConversion('md')->fit( 'crop',768,400);
+        //        $this->addMediaConversion('sm')->fit('crop', 425,200);
         
         $this->addMediaConversion('xl_webp')->fit('crop', 1920, 540)->format(Manipulations::FORMAT_WEBP);
-//        $this->addMediaConversion('lg_webp')->fit( 'crop',1366,400)->format(Manipulations::FORMAT_WEBP);
-//        $this->addMediaConversion('md_webp')->fit( 'crop',768,400)->format(Manipulations::FORMAT_WEBP);
-//        $this->addMediaConversion('sm_webp')->fit('crop', 425,200)->format(Manipulations::FORMAT_WEBP);
+        //        $this->addMediaConversion('lg_webp')->fit( 'crop',1366,400)->format(Manipulations::FORMAT_WEBP);
+        //        $this->addMediaConversion('md_webp')->fit( 'crop',768,400)->format(Manipulations::FORMAT_WEBP);
+        //        $this->addMediaConversion('sm_webp')->fit('crop', 425,200)->format(Manipulations::FORMAT_WEBP);
     }
     
     public function products(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
