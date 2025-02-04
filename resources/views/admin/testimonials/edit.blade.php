fef1b4ee3eade1e2a7311eeab897c4320c1060f7
Stefan Nicula <stefanicu@gmail.com>
2025-02-04 03:10:53 +0200

    Update edit.blade.php

--- resources/views/admin/testimonials/edit.blade.php
+++ resources/views/admin/testimonials/edit.blade.php
@@ -109,6 +109,21 @@
                 </div>
             </form>
 
+            @if( app()->getLocale() === 'en' )
+                <form id="translateButtonAllForm" method="POST" class="" action="{{ route("admin.translation.languages") }}" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                        <div class="form-check {{ $errors->has('online') ? 'is-invalid' : '' }}">
+                            <input type="hidden" name="id" id="id" value="{{ $testimonial->id }}">
+                            <input type="hidden" name="model_translation" id="model_translation" value="testimonial_translations">
+                            <input type="hidden" name="foreign_key" id="foreign_key" value="testimonial_id">
+                            <input type="hidden" name="language" id="language" value="{{ app()->getLocale() }}">
+                            <button type="submit" class="btn btn-warning">{{ trans('admin.translation_button_all') }}</button>
+                        </div>
+                    </div>
+                </form>
+            @endif
+
             <form id="translateButtonForm" method="POST" class="" action="{{ route("admin.translation.granular") }}" enctype="multipart/form-data">
                 @csrf
                 <div class="form-group">
