b6cfc58734d536f3f0f3bd103e4d35edc5d72702
Stefan Nicula <stefanicu@gmail.com>
2025-02-25 01:54:03 +0200

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Application;
 use App\Models\Category;
-use App\Models\Product;
 use App\Traits\HasMetaData;
 use Illuminate\Http\Request;
 
@@ -21,103 +20,25 @@ class ApplicationController extends Controller
         
         $application_slug = $request->app_slug;
         
-        if ( ! is_numeric($application_slug)) {
-            $application = Application::whereTranslation('slug', $application_slug)
-                ->whereTranslation('locale', $currentLocale)
-                ->whereHas('translations', function ($query) {
-                    $query->where('online', 1); // Ensure the translation is marked as online
-                })
-                ->first();
-        } else {
-            $application_id = $application_slug;
-            $application = Application::whereTranslation('application_id', $application_id)
-                ->whereHas('translations', function ($query) {
-                    $query->where('online', 1); // Ensure the translation is marked as online
-                })
-                ->first();
-            
-            $application->name = trans('pages.no_translated_title');
-            $application->description = trans('pages.no_translated_message');
-            
-            $categories = [];
-            
-            $app_slugs = null;
-            foreach (config('translatable.locales') as $locale) {
-                $app_slugs[$locale] = $application->translate($locale)->slug ?? $application->id;
-            }
-            
-            return view('application', compact('application', 'categories', 'app_slugs'));
-        }
+        $application = Application::whereHas('translations', function ($query) use ($application_slug, $currentLocale) {
+            $query->where('slug', $application_slug)
+                ->where('locale', $currentLocale)
+                ->where('online', 1); // Ensure the translation is marked as online
+        })
+            ->first();
         
-        $productIds = null;
-        if ($application) {
-            $productIds = Application::find($application->id)
-                ->products()
-                ->whereHas('translations', function ($query) use ($currentLocale) {
-                    $query->where('locale', $currentLocale)->where('online', 1);
-                })
-                ->pluck('products.id')
-                ->toArray();
-        }
+        $categories = Category::where('application_id', $application->id) // Filter by application_id
+        ->whereHas('translations', function ($query) {
+            $query->where('online', 1);
+        })
+            ->orderByTranslation('name') // Order by translated name
+            ->get();
         
-        if ( ! $productIds) {
-            // TODO: replace categories with a message
-            $categories = Category::with('product_main_image', 'product_main_image.media')
-                ->whereHas('translations', function ($query) use ($currentLocale) {
-                    $query->where('locale', $currentLocale)
-                        ->where('online', 1)
-                        ->whereNotNull('name')
-                        ->where('name', '!=', '')
-                        ->whereNotNull('slug')
-                        ->where('slug', '!=', '');
-                })
-                ->orderByTranslation('name')
-                ->limit(3)
-                ->get();
-        } else {
-            $categories = Category::with('product_main_image.media')
-                ->whereHas('products', function ($query) use ($productIds) {
-                    $query->whereIn('products.id', $productIds);
-                })
-                ->whereHas('translations', function ($query) {
-                    $query->where('online', 1);
-                })
-                ->orderByTranslation('name')
-                ->get();
-        }
-        
-        if ($categories->count() == 1) {
-            $category = $categories->first();
-            $category_id = $category->id;
-            
-            $products = Product::with([
-                'translations' => function ($query) use ($currentLocale) {
-                    $query->where('locale', $currentLocale);
-                }
-            ])
-                ->whereHas('translations', function ($query) use ($currentLocale) {
-                    $query->where('locale', $currentLocale);
-                })
-                ->whereHas('categories', function ($query) use ($category_id) {
-                    $query->where('category_id', $category_id);
-                })
-                ->get();
-            
-            if ($products->count() === 1) {
-                $product = $products->first();
-                return redirect(route('product.'.app()->getLocale(),
-                    ['app_slug' => $application->slug, 'cat_slug' => $category->slug, 'prod_slug' => $product->slug]));
-            }
-            
-            return redirect(route('products.'.app()->getLocale(),
-                ['app_slug' => $application->slug, 'cat_slug' => $category->slug]));
-        }
         
         $app_slugs = null;
         foreach (config('translatable.locales') as $locale) {
             $app_slugs[$locale] = $application->translate($locale)->slug ?? $application->id;
         }
-        
         
         $metaData = $this->getMetaData($application);
         
