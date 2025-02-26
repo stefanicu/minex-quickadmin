34526427fc5d3a94e058baaad7c80c8755a601a2
Stefan Nicula <stefanicu@gmail.com>
2025-02-26 22:59:55 +0200

    Update index.blade.php

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.admin')
 @section('content')
-@can('user_create')
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a class="btn btn-success" href="{{ route('admin.users.create') }}">
-                {{ trans('global.add') }} {{ trans('cruds.user.title_singular') }}
-            </a>
+    @can('user_create')
+        <div style="margin-bottom: 10px;" class="row">
+            <div class="col-lg-12">
+                <a class="btn btn-success" href="{{ route('admin.users.create') }}">
+                    {{ trans('global.add') }} {{ trans('cruds.user.title_singular') }}
+                </a>
+            </div>
         </div>
-    </div>
-@endcan
-<div class="card">
-    <div class="card-header">
-        {{ trans('cruds.user.title_singular') }} {{ trans('global.list') }}
-    </div>
+    @endcan
+    <div class="card">
+        <div class="card-header">
+            {{ trans('cruds.user.title_singular') }} {{ trans('global.list') }}
+        </div>
 
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class=" table table-bordered table-striped table-hover datatable datatable-User">
-                <thead>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class=" table table-bordered table-striped table-hover datatable datatable-User">
+                    <thead>
                     <tr>
                         <th width="10">
 
@@ -41,8 +41,8 @@
                             &nbsp;
                         </th>
                     </tr>
-                </thead>
-                <tbody>
+                    </thead>
+                    <tbody>
                     @foreach($users as $key => $user)
                         <tr data-entry-id="{{ $user->id }}">
                             <td>
@@ -90,62 +90,84 @@
 
                         </tr>
                     @endforeach
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
-
-
 
 @endsection
 @section('scripts')
-@parent
-<script>
-    $(function () {
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-@can('user_delete')
-  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
-  let deleteButton = {
-    text: deleteButtonTrans,
-    url: "{{ route('admin.users.massDestroy') }}",
-    className: 'btn-danger',
-    action: function (e, dt, node, config) {
-      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
-          return $(entry).data('entry-id')
-      });
+    @parent
+    <script>
+        $(function () {
+            let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+            {{--@can('user_delete')--}}
+            {{--  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'--}}
+            {{--  let deleteButton = {--}}
+            {{--    text: deleteButtonTrans,--}}
+            {{--    url: "{{ route('admin.users.massDestroy') }}",--}}
+            {{--    className: 'btn-danger',--}}
+            {{--    action: function (e, dt, node, config) {--}}
+            {{--      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {--}}
+            {{--          return $(entry).data('entry-id')--}}
+            {{--      });--}}
 
-      if (ids.length === 0) {
-        alert('{{ trans('global.datatables.zero_selected') }}')
+            {{--      if (ids.length === 0) {--}}
+            {{--        alert('{{ trans('global.datatables.zero_selected') }}')--}}
 
-        return
-      }
+            {{--        return--}}
+            {{--      }--}}
 
-      if (confirm('{{ trans('global.areYouSure') }}')) {
-        $.ajax({
-          headers: {'x-csrf-token': _token},
-          method: 'POST',
-          url: config.url,
-          data: { ids: ids, _method: 'DELETE' }})
-          .done(function () { location.reload() })
-      }
-    }
-  }
-  dtButtons.push(deleteButton)
-@endcan
+            {{--      if (confirm('{{ trans('global.areYouSure') }}')) {--}}
+            {{--        $.ajax({--}}
+            {{--          headers: {'x-csrf-token': _token},--}}
+            {{--          method: 'POST',--}}
+            {{--          url: config.url,--}}
+            {{--          data: { ids: ids, _method: 'DELETE' }})--}}
+            {{--          .done(function () { location.reload() })--}}
+            {{--      }--}}
+            {{--    }--}}
+            {{--  }--}}
+            {{--  dtButtons.push(deleteButton)--}}
+            {{--@endcan--}}
 
-  $.extend(true, $.fn.dataTable.defaults, {
-    orderCellsTop: true,
-    order: [[ 1, 'desc' ]],
-    pageLength: 100,
-  });
-  let table = $('.datatable-User:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-  $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
-      $($.fn.dataTable.tables(true)).DataTable()
-          .columns.adjust();
-  });
-  
-})
+            $.extend(true, $.fn.dataTable.defaults, {
+                orderCellsTop: true,
+                order: [[1, 'desc']],
+                pageLength: 100,
+            });
+            let table = $('.datatable-User:not(.ajaxTable)').DataTable({
+                buttons: dtButtons,
+                stateSave: true,
+                processing: true,
+                serverSide: true,
+                retrieve: true,
+                aaSorting: [],
+                ajax: "{{ route('admin.applications.index') }}",
+                columns: [
+                    {data: 'placeholder', name: 'placeholder'},
+                    {data: 'id', name: 'id'},
+                    {data: 'email', name: 'email'},
+                    {data: 'email_verified_at', name: 'email_verified_at'},
+                    // {data: 'image', name: 'image', class: 'text-center', sortable: false, searchable: false},
+                    {
+                        data: 'actions',
+                        name: '{{ trans('global.actions') }}',
+                        class: 'text-nowrap text-center',
+                        sortable: false,
+                        searchable: false
+                    }
+                ],
+                orderCellsTop: false,
+                order: [[2, 'asc']],
+                pageLength: 5,
+            })
+            $('a[data-toggle="tab"]').on('shown.bs.tab click', function (e) {
+                $($.fn.dataTable.tables(true)).DataTable().columns.adjust();
+            });
 
-</script>
+        })
+
+    </script>
 @endsection
