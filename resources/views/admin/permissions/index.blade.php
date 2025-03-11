86950ece3b4372335691a15f764e0931eec086df
Stefan Nicula <stefanicu@gmail.com>
2025-03-11 02:16:10 +0200

    Update index.blade.php

--- resources/views/admin/permissions/index.blade.php
+++ resources/views/admin/permissions/index.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.admin')
 @section('content')
-@can('permission_create')
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a class="btn btn-success" href="{{ route('admin.permissions.create') }}">
-                {{ trans('global.add') }} {{ trans('cruds.permission.title_singular') }}
-            </a>
+    @can('permission_create')
+        <div style="margin-bottom: 10px;" class="row">
+            <div class="col-lg-12">
+                <a class="btn btn-success" href="{{ route('admin.permissions.create') }}">
+                    {{ trans('global.add') }} {{ trans('cruds.permission.title_singular') }}
+                </a>
+            </div>
         </div>
-    </div>
-@endcan
-<div class="card">
-    <div class="card-header">
-        {{ trans('cruds.permission.title_singular') }} {{ trans('global.list') }}
-    </div>
+    @endcan
+    <div class="card">
+        <div class="card-header">
+            {{ trans('cruds.permission.title_singular') }} {{ trans('global.list') }}
+        </div>
 
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class=" table table-bordered table-striped table-hover datatable datatable-Permission">
-                <thead>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class=" table table-bordered table-striped table-hover datatable datatable-Permission">
+                    <thead>
                     <tr>
                         <th width="10">
 
@@ -32,8 +32,8 @@
                             &nbsp;
                         </th>
                     </tr>
-                </thead>
-                <tbody>
+                    </thead>
+                    <tbody>
                     @foreach($permissions as $key => $permission)
                         <tr data-entry-id="{{ $permission->id }}">
                             <td>
@@ -70,62 +70,59 @@
 
                         </tr>
                     @endforeach
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
-
-
 
 @endsection
 @section('scripts')
-@parent
-<script>
-    $(function () {
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-@can('permission_delete')
-  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
-  let deleteButton = {
-    text: deleteButtonTrans,
-    url: "{{ route('admin.permissions.massDestroy') }}",
-    className: 'btn-danger',
-    action: function (e, dt, node, config) {
-      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
-          return $(entry).data('entry-id')
-      });
+    @parent
+    <script>
+        $(function () {
+            let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+            @can('permission_delete')
+            {{--let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'--}}
+            {{--let deleteButton = {--}}
+            {{--  text: deleteButtonTrans,--}}
+            {{--  url: "{{ route('admin.permissions.massDestroy') }}",--}}
+            {{--  className: 'btn-danger',--}}
+            {{--  action: function (e, dt, node, config) {--}}
+            {{--    var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {--}}
+            {{--        return $(entry).data('entry-id')--}}
+            {{--    });--}}
 
-      if (ids.length === 0) {
-        alert('{{ trans('global.datatables.zero_selected') }}')
+            {{--    if (ids.length === 0) {--}}
+            {{--      alert('{{ trans('global.datatables.zero_selected') }}')--}}
 
-        return
-      }
+            {{--      return--}}
+            {{--    }--}}
 
-      if (confirm('{{ trans('global.areYouSure') }}')) {
-        $.ajax({
-          headers: {'x-csrf-token': _token},
-          method: 'POST',
-          url: config.url,
-          data: { ids: ids, _method: 'DELETE' }})
-          .done(function () { location.reload() })
-      }
-    }
-  }
-  dtButtons.push(deleteButton)
-@endcan
+            {{--    if (confirm('{{ trans('global.areYouSure') }}')) {--}}
+            {{--      $.ajax({--}}
+            {{--        headers: {'x-csrf-token': _token},--}}
+            {{--        method: 'POST',--}}
+            {{--        url: config.url,--}}
+            {{--        data: { ids: ids, _method: 'DELETE' }})--}}
+            {{--        .done(function () { location.reload() })--}}
+            {{--    }--}}
+            {{--  }--}}
+            {{--}--}}
+            {{--dtButtons.push(deleteButton)--}}
+            @endcan
 
-  $.extend(true, $.fn.dataTable.defaults, {
-    orderCellsTop: true,
-    order: [[ 1, 'desc' ]],
-    pageLength: 100,
-  });
-  let table = $('.datatable-Permission:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-  $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
-      $($.fn.dataTable.tables(true)).DataTable()
-          .columns.adjust();
-  });
-  
-})
+            $.extend(true, $.fn.dataTable.defaults, {
+                orderCellsTop: true,
+                order: [[1, 'desc']],
+                pageLength: 100,
+            });
+            let table = $('.datatable-Permission:not(.ajaxTable)').DataTable({buttons: dtButtons})
+            $('a[data-toggle="tab"]').on('shown.bs.tab click', function (e) {
+                $($.fn.dataTable.tables(true)).DataTable().columns.adjust();
+            });
 
-</script>
+        })
+
+    </script>
 @endsection
