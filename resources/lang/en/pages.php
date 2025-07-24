991dc00e07b24cb4956d9cdae46d4ee70e4e4853
Stefan Nicula <stefanicu@gmail.com>
2025-07-24 22:30:11 +0300

    Update pages.php

--- resources/lang/en/pages.php
+++ resources/lang/en/pages.php
@@ -41,4 +41,6 @@ return [
     'copyright' => 'Minex Group International',
     'see_what_we_have_done' => 'See what we\'ve done for',
     'contact_thank_you' => 'Your message was successfully sent, thank you!',
+    'other' => 'Other',
+    'no_image' => 'No image',
 ];
