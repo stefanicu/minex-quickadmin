e5e86ae16530e11852ac41d82cbd7c879bd27af9
Stefan Nicula <stefanicu@gmail.com>
2025-09-03 01:16:44 +0300

    Update Page.php

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -103,7 +103,7 @@ class Page extends Model implements HasMedia, TranslatableContract
     
     public function getMetaImage(): ?array
     {
-        $mainPhoto = $this->getImageAttribute(); // Replace with your logic to get the main photo
+        $mainPhoto = $this->getImageAttribute();
         
         if ($mainPhoto) {
             $image = Image::make($mainPhoto->getPath());
