64e663d130fa1fbcc62c88daa5745e37730014e6
Stefan Nicula <stefanicu@gmail.com>
2025-09-14 15:54:46 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
     
-    'url' => env('APP_URL', 'https://minexgroup.eu'),
+    'url' => env('APP_URL', 'https://www.minexgroup.eu'),
     
     'asset_url' => env('ASSET_URL', '/'),
     
