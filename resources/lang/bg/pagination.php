2beb3f245c53467d8493457c17fa5616015d6381
Stefan Nicula <stefanicu@gmail.com>
2025-09-16 02:35:57 +0300

    Update pagination.php

--- resources/lang/bg/pagination.php
+++ resources/lang/bg/pagination.php
@@ -1,7 +1,7 @@
 <?php
 
-return array(
-    'previous' => '&laquo; Още нови',
-    'next' => 'Още стари &raquo;',
-    'page' => 'Страsница',
+return array (
+  'previous' => '&laquo; Още нови',
+  'next' => 'Още стари &raquo;',
+  'page' => 'Страница',
 );
