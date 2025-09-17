5d1f95793f20bf00c7ab9fc6ae3ca17c5a1ec5b0
Stefan Nicula <stefanicu@gmail.com>
2025-09-17 18:16:35 +0300

    Update panel.php

--- config/panel.php
+++ config/panel.php
@@ -5,19 +5,19 @@ return [
     'time_format' => 'H:i:s',
     'primary_language' => 'en',
     'available_languages' => [
-        'en' => 'English',
-        'bg' => 'Bulgarian',
-        'bs' => 'Bosnian',
-        'et' => 'Estonian',
-        'hr' => 'Croatian',
-        'hu' => 'Hungarian',
-        'lt' => 'Lithuanian',
-        'lv' => 'Latvian',
-        'mk' => 'Macedonian',
-        'ro' => 'Romanian',
-        'sr' => 'Serbian',
-        'sl' => 'Slovenian',
-        'uk' => 'Ukrainian',
+        'en' => 'EN', // English - USA (or use GB for United Kingdom)
+        'bg' => 'BG', // Bulgarian - Bulgaria
+        'bs' => 'BA', // Bosnian - Bosnia and Herzegovina
+        'et' => 'EE', // Estonian - Estonia
+        'hr' => 'HR', // Croatian - Croatia
+        'hu' => 'HU', // Hungarian - Hungary
+        'lt' => 'LT', // Lithuanian - Lithuania
+        'lv' => 'LV', // Latvian - Latvia
+        'mk' => 'MK', // Macedonian - North Macedonia
+        'ro' => 'RO', // Romanian - Romania
+        'sl' => 'SI', // Slovenian - Slovenia
+        'sr' => 'RS', // Serbian - Serbia
+        'uk' => 'UA', // Ukrainian - Ukraine
     ],
 
 ];
