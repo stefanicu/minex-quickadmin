d1c01aeb004ed4b41236f708fbebf3dfb1a7b1b7
Stefan Nicula <stefanicu@gmail.com>
2025-09-17 18:16:41 +0300

    Update languageToCountryCode.php

--- app/Helpers/languageToCountryCode.php
+++ app/Helpers/languageToCountryCode.php
@@ -9,21 +9,7 @@ if ( ! function_exists('languageToCountryCode')) {
      */
     function languageToCountryCode(string $languageCode): ?string
     {
-        $mapping = [
-            'en' => 'EN', // English - USA (or use GB for United Kingdom)
-            'ro' => 'RO', // Romanian - Romania
-            'bg' => 'BG', // Bulgarian - Bulgaria
-            'lt' => 'LT', // Lithuanian - Lithuania
-            'lv' => 'LV', // Latvian - Latvia
-            'et' => 'EE', // Estonian - Estonia
-            'sr' => 'RS', // Serbian - Serbia
-            'hr' => 'HR', // Croatian - Croatia
-            'sl' => 'SI', // Slovenian - Slovenia
-            'bs' => 'BA', // Bosnian - Bosnia and Herzegovina
-            'mk' => 'MK', // Macedonian - North Macedonia
-            'hu' => 'HU', // Hungarian - Hungary
-            'uk' => 'UA', // Ukrainian - Ukraine
-        ];
+        $mapping = config('panel.available_languages');
         
         return $mapping[$languageCode] ?? null; // Return country code or null if not found
     }
