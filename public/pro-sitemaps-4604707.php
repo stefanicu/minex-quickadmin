6f823daafe2d3d9914a2f9bcfc7f97e758e3f466
Stefan Nicula <stefanicu@gmail.com>
2025-09-18 11:01:20 +0300

    Create pro-sitemaps-4604707.php

---
+++ public/pro-sitemaps-4604707.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ *  Self-hosted sitemaps relay script
+ *  (c) 2015-2020 PRO Sitemaps,  https://pro-sitemaps.com
+ *
+ */
+
+ /*
+  * Configuration
+  *
+  * site_id - your website account ID in PRO Sitemaps service
+  * site_url - your website URL
+  * sitemap_self_url - the link to sitemap on your domain
+  * sitemap_remote_url - the link to sitemap on PRO Sitemaps sitemaphosting domain
+  *
+  */
+
+ $script_config = array(
+ 	'ps_api' => '20190604',
+    'site_id' => '4604707',
+    'site_url' => 'https://www.minexgroup.eu/en',
+    'api_key' => 'ps_kCYajhvA.2WYzpfNlQaE68yWYgRNeor9gMzPhdwtBOXkYbbIK5v0cm1f',
+    'sitemap_self_url' => 'https://www.minexgroup.eu/pro-sitemaps-4604707.php',
+    'sitemap_remote_url' => 'https://pro-sitemaps.com/api/'
+ );
+
+ $sitemap_filename = isset($_GET['sn']) ? preg_replace('#[^a-z\d\.\_\-]#i', '', $_GET['sn']) : 'sitemap.xml';
+
+
+
+/*
+ * Retrieve remote sitemap
+ *
+ * Check if cURL library is installed, prepare API request and get response
+ *
+ */
+    if(!function_exists('curl_init')){
+        echo 'Error: cURL library not enabled';
+        exit;
+    }
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $script_config['sitemap_remote_url']);
+
+    $headers = array(
+        'User-Agent: Mozilla/5.0 (compatible; PRO Sitemaps Self-hosted sitemaps script; pro-sitemaps.com) Gecko XML-Sitemaps/1.0',
+    );
+    $fields = array(
+        'method' => 'download_sitemap',
+        'api_ver' => $script_config['ps_api'],
+        'api_key' => $script_config['api_key'],
+        'site_id' => $script_config['site_id'],
+        'sitemap_self_url' => $script_config['sitemap_self_url'],
+        'sitemap_id' => $sitemap_filename,
+
+        'remote_ip' => $_SERVER['REMOTE_ADDR'],
+        'remote_user_agent' => $_SERVER['HTTP_USER_AGENT'],
+    );
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    $result = curl_exec($ch);
+    $info = curl_getinfo($ch);
+    curl_close($ch);
+
+/*
+ * Parse API response and send content to client
+ *
+ */
+ 	header('PRO-Sitemaps-API-v: '.$script_config['ps_api']);
+
+    if(($info['http_code'] != '200') || strstr($info['content_type'], 'json') ){
+        $status_message = '503 Service Temporarily Unavailable';
+        header('HTTP/1.1 '.$status_message);
+        header('Status: '.$status_message);
+        header('Retry-After: 300');
+        echo $status_message;
+    }else {
+        header('Content-Type: ' . $info['content_type']);
+        header('Content-Length: ' . strlen($result));
+        echo $result;
+    }
+    exit;
