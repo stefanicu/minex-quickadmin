5f2d48e28e0b2c8f5c35ed6951fbbb350b855e2f
Stefan Nicula <stefanicu@gmail.com>
2025-09-20 15:43:26 +0300

    Create ReplaceHtmlFromMetaDescription.php

---
+++ app/Console/Commands/ReplaceHtmlFromMetaDescription.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+
+class ReplaceHtmlFromMetaDescription extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'metadescription:update';
+    
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Replace <p></p><strong></strong><table></table> from meta description.';
+    
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $dbName = Config::get('database.minex_live');
+        
+        $RStoSR = file_get_contents('database/sql_scripts/metadescription.sql');
+        $RStoSR = str_replace('$dbName', "$dbName", $RStoSR);
+        DB::unprepared($RStoSR);
+        
+        echo '
+
+        ========================  UPDATE COMPLETED!
+
+        ';
+    }
+}
