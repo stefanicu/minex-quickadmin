1e3fec922d73d54eb4f78ce355ff48d10098ed09
Stefan Nicula <stefanicu@gmail.com>
2025-09-24 23:35:43 +0300

    Update create.blade.php

--- resources/views/admin/blogs/create.blade.php
+++ resources/views/admin/blogs/create.blade.php
@@ -40,6 +40,19 @@
                     <span class="help-block">{{ trans('cruds.blog.fields.content_helper') }}</span>
                 </div>
 
+                <div class="row align-items-center">
+                    <div class="form-group col-12 align-items-center">
+                        <label for="image">{{ trans('cruds.blog.fields.image') }}</label>
+                        <span class="help-block">{{ trans('cruds.blog.fields.image_helper') }}</span>
+                        <div class="needsclick dropzone {{ $errors->has('image') ? 'is-invalid' : '' }} text-center"
+                             id="image-dropzone">
+                        </div>
+                        @if($errors->has('image'))
+                            <span class="text-danger">{{ $errors->first('image') }}</span>
+                        @endif
+                    </div>
+                </div>
+
                 <div class="form-group">
                     <input type="hidden" name="locale" value="{{app()->getLocale()}}">
                     <button class="btn btn-danger" type="submit">
@@ -117,5 +130,59 @@
                 );
             }
         });
+    </script>
+
+    <script>
+        Dropzone.options.imageDropzone = {
+            url: '{{ route('admin.blogs.storeMedia') }}',
+            maxFilesize: 1, // MB
+            acceptedFiles: '.jpeg,.jpg,.png,.gif',
+            maxFiles: 1,
+            addRemoveLinks: true,
+            headers: {
+                'X-CSRF-TOKEN': "{{ csrf_token() }}"
+            },
+            params: {
+                size: 1,
+            },
+            success: function (file, response) {
+                $('form').find('input[name="image"]').remove()
+                $('form').append('<input type="hidden" name="image" value="' + response.name + '">')
+            },
+            removedfile: function (file) {
+                file.previewElement.remove()
+                if (file.status !== 'error') {
+                    $('form').find('input[name="image"]').remove()
+                    this.options.maxFiles = this.options.maxFiles + 1
+                }
+            },
+            init: function () {
+                @if(isset($blog) && $blog->image)
+                var file = {!! json_encode($blog->image) !!}
+                this.options.addedfile.call(this, file)
+                this.options.thumbnail.call(this, file, file.preview ?? file.preview_url)
+                file.previewElement.classList.add('dz-complete')
+                $('form').append('<input type="hidden" name="image" value="' + file.file_name + '">')
+                this.options.maxFiles = this.options.maxFiles - 1
+                @endif
+            },
+            error: function (file, response) {
+                if ($.type(response) === 'string') {
+                    var message = response //dropzone sends it's own error messages in string
+                } else {
+                    var message = response.errors.file
+                }
+                file.previewElement.classList.add('dz-error')
+                _ref = file.previewElement.querySelectorAll('[data-dz-errormessage]')
+                _results = []
+                for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                    node = _ref[_i]
+                    _results.push(node.textContent = message)
+                }
+
+                return _results
+            }
+        }
+
     </script>
 @endsection
