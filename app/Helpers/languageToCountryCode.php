dccdb419d058ad14b6677568aeed2547ab3ad49b
Stefan Nicula <stefanicu@gmail.com>
2025-09-29 19:22:46 +0300

    Update languageToCountryCode.php

--- app/Helpers/languageToCountryCode.php
+++ app/Helpers/languageToCountryCode.php
@@ -7,9 +7,13 @@ if ( ! function_exists('languageToCountryCode')) {
      * @param  string  $languageCode
      * @return string|null
      */
-    function languageToCountryCode(string $languageCode): ?string
+    function languageToCountryCode(string $languageCode, bool $names = false): ?string
     {
         $mapping = config('panel.available_languages');
+        
+        if ($names) {
+            $mapping = config('panel.languages_names');
+        }
         
         return $mapping[$languageCode] ?? null; // Return country code or null if not found
     }
