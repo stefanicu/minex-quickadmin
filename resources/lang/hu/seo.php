61613521381613b889887d4a6294634bc24a844b
Stefan Nicula <stefanicu@gmail.com>
2025-10-02 21:49:58 +0300

    Update seo.php

--- resources/lang/hu/seo.php
+++ resources/lang/hu/seo.php
@@ -1,14 +1,14 @@
 <?php
 
-return array(
-    'home_title' => 'Minex Group International – Betekintések a felületkezelés és fémfeldolgozás területére',
-    'home_description' => 'Minex Group International – az ön megbízható partnere a komplex műszaki projektek fejlesztésében felületkezelés és fémfeldolgozás terén. Mindent kínálunk, a tervezéstől és kivitelezéstől kezdve, a felszerelésen át, a karbantartásig.',
-    'brands_title' => 'Márkák | Minex Group International – Betekintések a Felületkezelés & Fémfeldolgozás terén',
-    'brands_description' => 'Fedezze fel a Minex Group International márkáit: vezetők a felületkezelés és a fémfeldolgozás terén. Ismerje meg tervezési, építési, berendezés- és karbantartási szolgáltatásainkat.',
-    'references_title' => 'Referenciák | Minex Group International – Betekintés a Felszíni Kezelésbe és Fémfeldolgozásba',
-    'references_description' => 'Fedezze fel a Minex Group International referenciáit: Sikeres projektek bemutatása a felületkezelés és fémmegmunkálás terén, beleértve a MARUB Brassót.',
-    'testimonials_title' => 'Vélemények | Minex Group International – Betekintések a Felületi Kezelés és Fémfeldolgozás területén',
-    'testimonials_description' => 'Fedezze fel a Minex Group International ajánlásait: Hallgassa meg ügyfeleink véleményét felületi kezelés és fémfeldolgozási megoldásainkról.',
-    'blogs_title' => 'Blog | Minex Group International – Ötletek a felületkezelésről és fémmegmunkálásról',
-    'blogs_description' => 'Fedezze fel a Minex Group International Blogot. Olvassa el a cikkeket, betekintéseket, és híreket a felületkezelésről, a fémfeldolgozásról, és az ipari innovációról.',
+return array (
+  'home_title' => 'Minex Group International – Betekintések a felületkezelés és fémfeldolgozás területére',
+  'home_description' => 'Minex Group International – az ön megbízható partnere a komplex műszaki projektek fejlesztésében felületkezelés és fémfeldolgozás terén. Mindent kínálunk, a tervezéstől és kivitelezéstől kezdve, a felszerelésen át, a karbantartásig.',
+  'brands_title' => 'Márkák | Minex Group International – Betekintések a Felületkezelés & Fémfeldolgozás terén',
+  'brands_description' => 'Fedezze fel a Minex Group International márkáit: vezetők a felületkezelés és a fémfeldolgozás terén. Ismerje meg tervezési, építési, berendezés- és karbantartási szolgáltatásainkat.',
+  'references_title' => 'Referenciák | Minex Group International – Betekintés a Felszíni Kezelésbe és Fémfeldolgozásba',
+  'references_description' => 'Fedezze fel a Minex Group International referenciáit: Sikeres projektek bemutatása a felületkezelés és fémmegmunkálás terén, beleértve a MARUB Brassót.',
+  'testimonials_title' => 'Vélemények | Minex Group International – Betekintések a Felületi Kezelés és Fémfeldolgozás területén',
+  'testimonials_description' => 'Fedezze fel a Minex Group International ajánlásait: Hallgassa meg ügyfeleink véleményét felületi kezelés és fémfeldolgozási megoldásainkról.',
+  'blogs_title' => 'Blog | Minex Group International – Ötletek a felületkezelésről és fémmegmunkálásról',
+  'blogs_description' => 'Fedezze fel a Minex Group International Blogot. Olvassa el a cikkeket, betekintéseket, és híreket a felületkezelésről, a fémfeldolgozásról, és az ipari innovációról.',
 );
