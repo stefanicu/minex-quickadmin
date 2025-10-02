c823f093c8f484891ec5f7656f149278c26c1abe
Stefan Nicula <stefanicu@gmail.com>
2025-10-02 21:49:54 +0300

    Update seo.php

--- resources/lang/en/seo.php
+++ resources/lang/en/seo.php
@@ -1,14 +1,14 @@
 <?php
 
-return [
-    'home_title' => 'Minex Group International – Insights on Surface Treatment & Metal Processing',
-    'home_description' => 'Minex Group International – your reliable partner in developing complex technical projects for surface treatment and metal processing. We offer you everything from designing and construction, to equipping and maintenance',
-    'brands_title' => 'Brands | Minex Group International – Insights on Surface Treatment & Metal Processing',
-    'brands_description' => 'Explore Minex Group International’s brands: leaders in surface treatment and metal processing. Discover our design, construction, equipment, and maintenance service.',
-    'references_title' => 'References | Minex Group International – Insights on Surface Treatment & Metal Processing',
-    'references_description' => 'Explore Minex Group International’s References: Showcasing successful projects in surface treatment and metal processing, including MARUB Brasov.',
-    'testimonials_title' => 'Testimonials | Minex Group International – Insights on Surface Treatment & Metal Processing',
-    'testimonials_description' => 'Discover Minex Group International’s Testimonials: Hear from our clients about our expertise in surface treatment and metal processing solution.',
-    'blogs_title' => 'Blog | Minex Group International – Insights on Surface Treatment & Metal Processing',
-    'blogs_description' => 'Explore Minex Group International’s Blog. Read articles, insights, and news on surface treatment, metal processing, and industry innovation.',
-];
+return array (
+  'home_title' => 'Minex Group International – Insights on Surface Treatment & Metal Processing',
+  'home_description' => 'Minex Group International – your reliable partner in developing complex technical projects for surface treatment and metal processing. We offer you everything from designing and construction, to equipping and maintenance',
+  'brands_title' => 'Brands | Minex Group International – Insights on Surface Treatment & Metal Processing',
+  'brands_description' => 'Explore Minex Group International’s brands: leaders in surface treatment and metal processing. Discover our design, construction, equipment, and maintenance service.',
+  'references_title' => 'References | Minex Group International – Insights on Surface Treatment & Metal Processing',
+  'references_description' => 'Explore Minex Group International’s References: Showcasing successful projects in surface treatment and metal processing, including MARUB Brasov.',
+  'testimonials_title' => 'Testimonials | Minex Group International – Insights on Surface Treatment & Metal Processing',
+  'testimonials_description' => 'Discover Minex Group International’s Testimonials: Hear from our clients about our expertise in surface treatment and metal processing solution.',
+  'blogs_title' => 'Blog | Minex Group International – Insights on Surface Treatment & Metal Processing',
+  'blogs_description' => 'Explore Minex Group International’s Blog. Read articles, insights, and news on surface treatment, metal processing, and industry innovation. ttt',
+);
