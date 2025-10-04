8f17644cec41e23c3f5784ad5a00ef47bfb75d2f
Stefan Nicula <stefanicu@gmail.com>
2025-10-04 17:11:31 +0300

    Update edit.blade.php

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -300,7 +300,7 @@
                             <input type="hidden" name="model_translation" id="model_translation" value="product_translations">
                             <input type="hidden" name="foreign_key" id="foreign_key" value="product_id">
                             <input type="hidden" name="language" id="language" value="{{ app()->getLocale() }}">
-                            <button type="submit" class="btn btn-warning">{{ trans('admin.translation_button_all') }}</button>
+                            <button type="submit" class="btn btn-danger">{{ trans('admin.translation_button_all') }}</button>
                         </div>
                     </div>
                 </form>
@@ -314,7 +314,13 @@
                         <input type="hidden" name="model_translation" id="model_translation" value="product_translations">
                         <input type="hidden" name="foreign_key" id="foreign_key" value="product_id">
                         <input type="hidden" name="language" id="language" value="{{ app()->getLocale() }}">
-                        <button type="submit" class="btn btn-success">{{ trans('admin.translation_button', ['language' => strtoupper(app()->getLocale())]) }}</button>
+                        @if(app()->getLocale() === 'en')
+                            <button type="submit" class="btn btn-primary">{{ trans('admin.translation_button', ['from' => 'RO','to' => 'EN']) }}</button>
+                        @elseif(app()->getLocale() === 'ro')
+                            <button type="submit" class="btn btn-primary">{{ trans('admin.translation_button', ['from' => 'EN','to' => 'RO']) }}</button>
+                        @else
+                            <button type="submit" class="btn btn-primary">{{ trans('admin.translation_button', ['from' => 'EN and RO','to' => strtoupper(app()->getLocale())]) }}</button>
+                        @endif
                     </div>
                 </div>
             </form>
