18c936ed0ade6edbcf112d1893a9721efaa7efba
Stefan Nicula <stefanicu@gmail.com>
2025-10-04 22:37:07 +0300

    Create ViewServiceProvider.php

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+// App\Providers\ViewServiceProvider.php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        View::composer('*', function ($view) {
+            $locale = app()->getLocale();
+            
+            $gdprTranslation = DB::table('page_translations')
+                ->where('page_id', 1) // ID-ul paginii GDPR
+                ->where('locale', $locale)
+                ->first();
+            
+            $view->with('gdprTranslation', $gdprTranslation);
+        });
+    }
+}
