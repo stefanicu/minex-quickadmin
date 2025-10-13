c8f1a1c85fea1fe24f831ac18f8bf80eae0ca692
Stefan Nicula <stefanicu@gmail.com>
2025-10-13 17:07:12 +0300

    Update seo.php

--- resources/lang/hr/seo.php
+++ resources/lang/hr/seo.php
@@ -1,14 +1,14 @@
 <?php
 
-return array(
-    'home_title' => 'Minex Group International – Uvidi o Obradi Površina i Obradi Metala',
-    'home_description' => 'Minex Group International – vaš pouzdan partner u razvoju složenih tehničkih projekata za površinsku obradu i obradu metala. Nudimo vam sve od projektiranja i izgradnje, do opremanja i održavanja.',
-    'brands_title' => 'Marke | Minex Group International – Uvidi o površinskoj obradi i obradi metala',
-    'brands_description' => 'Istražite Minex Group International brendove: lideri u površinskoj obradi i obradi metala. Otkrijte naš dizajn, gradnju, opremu i uslugu održavanja.',
-    'references_title' => 'Reference | Minex Group International – Uvidi o površinskoj obradi i obradi metala',
-    'references_description' => 'Istražite Reference Minex Group International: Prikaz uspješnih projekata u površinskoj obradi i obradi metala, uključujući MARUB Brașov.',
-    'testimonials_title' => 'Svjedočanstva | Minex Grupa Internacional – Uvidi u Obradu Površina i Obradu Metala',
-    'testimonials_description' => 'Otkrijte izjave Minex Group Internationala: Saznajte od naših klijenata o našoj stručnosti u rješenjima za obradu površina i obradu metala.',
-    'blogs_title' => 'Blog | Minex Group International – Uvidi o površinskoj obradi i obradi metala',
-    'blogs_description' => 'Istražite blog Minex Group International. Čitajte članke, uvide i vijesti o površinskoj obradi, obradi metala i inovacijama u industriji.',
+return array (
+  'home_title' => 'Minex – Uvidi o Obradi Površina i Obradi Metala',
+  'home_description' => 'Minex – vaš pouzdan partner u razvoju složenih tehničkih projekata za površinsku obradu i obradu metala. Nudimo vam sve od projektiranja i izgradnje, do opremanja i održavanja.',
+  'brands_title' => 'Oprema za površinsku obradu - brendovi i partneri | Minex',
+  'brands_description' => 'Otkrijte naše OEM partnere i pouzdane proizvođače za pjeskarenje, premazivanje, ventilaciju i više—široka pokrivenost i dokazana vrijednost. Pogledajte sve brendove.',
+  'references_title' => 'Industrijski projektni primjeri i studije slučaja | Minex',
+  'references_description' => 'Pogledajte naše reference: industrijsko pjeskarenje, premazivanje, ventilacija, filtracija i projekti odvlaživanja diljem Europe. Prikaz prema industriji.',
+  'testimonials_title' => 'Iskustva korisnika – Industrijska rješenja za površinsku obradu | Minex',
+  'testimonials_description' => 'Autentična iskustva klijenata iz brodogradilišta i proizvodnih pogona—dokazani rezultati u pjeskarenju, bojanju, ventilaciji i odvlaživanju. Pogledajte korisničke priče.',
+  'blogs_title' => 'Blog – Industrijski uvidi i resursi | Minex',
+  'blogs_description' => 'Istražite blog Minex Group International. Čitajte članke, uvide i vijesti o površinskoj obradi, obradi metala i inovacijama u industriji.',
 );
