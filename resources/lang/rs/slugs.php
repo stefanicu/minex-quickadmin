5a34e0e1159fc710ea11974dfb7116bdc006f118
Stefan Nicula <stefanicu@gmail.com>
2025-10-13 17:08:24 +0300

    Create slugs.php

---
+++ resources/lang/rs/slugs.php
@@ -0,0 +1,14 @@
+<?php
+
+return array (
+  'brand' => 'partner',
+  'brands' => 'partneri',
+  'product' => 'proizvod',
+  'products' => 'proizvodi',
+  'category' => 'kategorija',
+  'categories' => 'kategorije',
+  'reference' => 'referenca',
+  'references' => 'reference',
+  'testimonials' => 'svetstvo',
+  'blog' => 'blog',
+);
