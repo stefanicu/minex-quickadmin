f570ac0eacd644052b33ff0abf2cb48084d5b3d4
Stefan Nicula <stefanicu@gmail.com>
2025-10-15 10:31:03 +0300

    Update edit.blade.php

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -291,7 +291,36 @@
                     for (var i = 0; i < allEditors.length; ++i) {
                         ClassicEditor.create(
                             allEditors[i], {
-                                extraPlugins: [SimpleUploadAdapter]
+                                extraPlugins: [SimpleUploadAdapter],
+                                heading: {
+                                    options: [
+                                        {model: 'paragraph', title: 'Paragraf', class: 'ck-heading_paragraph'},
+                                        {
+                                            model: 'heading1',
+                                            view: 'h1',
+                                            title: 'Heading 1',
+                                            class: 'ck-heading_heading1'
+                                        },
+                                        {
+                                            model: 'heading2',
+                                            view: 'h2',
+                                            title: 'Heading 2',
+                                            class: 'ck-heading_heading2'
+                                        },
+                                        {
+                                            model: 'heading3',
+                                            view: 'h3',
+                                            title: 'Heading 3',
+                                            class: 'ck-heading_heading3'
+                                        },
+                                        {
+                                            model: 'heading4',
+                                            view: 'h4',
+                                            title: 'Heading 4',
+                                            class: 'ck-heading_heading4'
+                                        }
+                                    ]
+                                }
                             }
                         );
                     }
