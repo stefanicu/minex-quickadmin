f2033b0e2df85496cf84f6ef10c35ea57876a57c
Stefan Nicula <stefanicu@gmail.com>
2025-10-15 10:31:13 +0300

    Update edit.blade.php

--- resources/views/admin/pages/edit.blade.php
+++ resources/views/admin/pages/edit.blade.php
@@ -240,7 +240,15 @@
             for (var i = 0; i < allEditors.length; ++i) {
                 ClassicEditor.create(
                     allEditors[i], {
-                        extraPlugins: [SimpleUploadAdapter]
+                        extraPlugins: [SimpleUploadAdapter],
+                        heading: {
+                            options: [
+                                {model: 'paragraph', title: 'Paragraf', class: 'ck-heading_paragraph'},
+                                {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
+                                {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
+                                {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'}
+                            ]
+                        }
                     }
                 );
             }
