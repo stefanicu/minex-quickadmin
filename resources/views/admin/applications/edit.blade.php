ff2a7a48a318bfb87696fd5d715f3abd15534e80
Stefan Nicula <stefanicu@gmail.com>
2025-10-15 10:43:09 +0300

    Update edit.blade.php

--- resources/views/admin/applications/edit.blade.php
+++ resources/views/admin/applications/edit.blade.php
@@ -266,7 +266,7 @@
                                 {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
                                 {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'}
                             ]
-                        }
+                        } // heading ok
                     }
                 );
             }
