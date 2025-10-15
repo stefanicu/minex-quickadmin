01e61e14f4e161242874e781fdc0649a7bfb4f62
Stefan Nicula <stefanicu@gmail.com>
2025-10-16 00:54:04 +0300

    Update create.blade.php

--- resources/views/admin/applications/create.blade.php
+++ resources/views/admin/applications/create.blade.php
@@ -41,6 +41,27 @@
                     </div>
                 </div>
 
+                <div class="row">
+                    <div class="form-group col-6">
+                        <label class="required" for="title">{{ trans('cruds.application.fields.title') }}</label>
+                        <input class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" type="text"
+                               name="title" id="title" value="{{ old('title', '') }}" required>
+                        @if($errors->has('title'))
+                            <span class="text-danger">{{ $errors->first('title') }}</span>
+                        @endif
+                        <span class="help-block">{{ trans('cruds.application.fields.title_helper') }}</span>
+                    </div>
+                    <div class="form-group col-6">
+                        <label for="subtitle">{{ trans('cruds.application.fields.subtitle') }}</label>
+                        <input class="form-control {{ $errors->has('subtitle') ? 'is-invalid' : '' }}" type="text"
+                               name="subtitle" id="subtitle" value="{{ old('subtitle', '') }}">
+                        @if($errors->has('subtitle'))
+                            <span class="text-danger">{{ $errors->first('subtitle') }}</span>
+                        @endif
+                        <span class="help-block">{{ trans('cruds.application.fields.subtitle_helper') }}</span>
+                    </div>
+                </div>
+
                 <div class="form-group">
                     <label for="content">{{ trans('cruds.application.fields.content') }}</label>
                     <textarea class="form-control ckeditor {{ $errors->has('content') ? 'is-invalid' : '' }}" rows="200"
