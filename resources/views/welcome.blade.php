98ca3902edbe38239a5d4431a2182003cdd4f6a5
Stefan Nicula <stefanicu@gmail.com>
2025-10-16 00:54:13 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
     <script src="{{ asset('/js/validate/set.validate.js') }}"></script>
     <script src="{{ asset('/js/countries.js') }}?x={{ rand(398473,298379283647847) }}"></script>
     <script nonce="{{ session('csp_nonce') }}"
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRHi8eiqWm--iQQ-fNTq3AWKev7xCj2RA&callback=initialize&v=weekly&libraries=marker"
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRHi8eiqWm--iQQ-fNTq3AWKev7xCj2RA&callback=initialize&v=weekly&libraries=marker&loading=async"
             async defer></script>
     <script src="{{ asset('/js/map.js?v=274624') }}"></script>
     <script src="{{ asset('/js/bxSlider/home_setup.js') }}"></script>
