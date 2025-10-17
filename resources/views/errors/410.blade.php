965f072a66ebf4c14c59ea895da9116832a1f507
Stefan Nicula <stefanicu@gmail.com>
2025-10-17 17:24:32 +0300

    Update 410.blade.php

--- resources/views/errors/410.blade.php
+++ resources/views/errors/410.blade.php
@@ -20,8 +20,23 @@
     <title>410 Gone - Page Not Available</title>
 
     <link href="{{ asset('css/tailwind.css') }}" rel="stylesheet">
+    <!-- Google Tag Manager -->
+    <script nonce="{{ session('csp_nonce') }}">(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+            var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-PM3K8LC');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body class="relative min-h-screen w-screen flex items-center justify-center bg-black/70 p-4">
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+    <iframe nonce="{{ session('csp_nonce') }}" src="https://www.googletagmanager.com/ns.html?id=GTM-PM3K8LC" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
 <!-- Orange top bar -->
 <div class="absolute top-0 left-0 w-full bg-orange-500 xopacity-60 z-20 py-2 px-4">
     <svg class="logo-svg" version="1" viewBox="0 0 139 100">
