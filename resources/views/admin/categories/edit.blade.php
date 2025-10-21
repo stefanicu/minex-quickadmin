d5b76eafe4411ca0a62cd6db313c6e3d00876a1e
Stefan Nicula <stefanicu@gmail.com>
2025-10-22 01:04:11 +0300

    Update edit.blade.php

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -56,7 +56,7 @@
 
                 <div class="row">
 
-                    <div class="form-group col-12 col-xl-4">
+                    <div class="form-group col-12 col-xl-5">
                         <label class="required" for="name">{{ trans('cruds.category.fields.name') }}</label>
                         <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text"
                                name="name" id="name" value="{{ old('name', $category->name) }}" required>
@@ -66,7 +66,7 @@
                         <span class="help-block">{{ trans('cruds.category.fields.name_helper') }}</span>
                     </div>
 
-                    <div class="form-group col-12 col-xl-4">
+                    <div class="form-group col-12 col-xl-5">
                         <label for="slug">{{ trans('cruds.category.fields.slug') }}</label>
                         <input class="form-control {{ $errors->has('slug') ? 'is-invalid' : '' }}" type="text"
                                name="slug" id="slug" value="{{ old('slug', $category->slug) }}">
@@ -91,6 +91,26 @@
                             <span class="text-danger">{{ $errors->first('application') }}</span>
                         @endif
                         <span class="help-block">{{ trans('cruds.product.fields.application_helper') }}</span>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="form-group col-5">
+                        <label class="required" for="title">{{ trans('cruds.category.fields.title') }}</label>
+                        <input class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" type="text" name="title" id="title" value="{{ old('title', $category->title) }}" required>
+                        @if($errors->has('title'))
+                            <span class="text-danger">{{ $errors->first('title') }}</span>
+                        @endif
+                        <span class="help-block">{{ trans('cruds.category.fields.title_helper') }}</span>
+                    </div>
+
+                    <div class="form-group col-5">
+                        <label for="subtitle">{{ trans('cruds.category.fields.subtitle') }}</label>
+                        <input class="form-control {{ $errors->has('subtitle') ? 'is-invalid' : '' }}" type="text" name="subtitle" id="subtitle" value="{{ old('subtitle', $category->subtitle) }}">
+                        @if($errors->has('subtitle'))
+                            <span class="text-danger">{{ $errors->first('subtitle') }}</span>
+                        @endif
+                        <span class="help-block">{{ trans('cruds.category.fields.subtitle_helper') }}</span>
                     </div>
 
                     <div class="form-group col-12 col-xl-2">
