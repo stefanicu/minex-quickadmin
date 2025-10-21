f13854d34367b512a0a49165565495db1d2d35ed
Stefan Nicula <stefanicu@gmail.com>
2025-10-22 01:04:15 +0300

    Update application.blade.php

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -42,46 +42,64 @@
 
     <section class="new">
         <div class="container">
-            <div class="col-xs-12">
-                <h1>{{ $application->title }}</h1>
-                @if( $application->subtitle )
-                    <p class="subtitle">{{ $application->subtitle }}</p>
-                @endif
-                <hr>
+            <div class="row">
+                <div class="col-xs-12">
 
-                <ul class="list-unstyled row justify-content-start full-row-prod assets-row mt-4 grid">
-                    @foreach($categories as $category)
-                        <li class="col-12 col-sm-6 col-md-4 py-2">
-                            <a href="{{ route('category.'.app()->getLocale(), ['app_slug' => $application->slug ?? null,'cat_slug' => $category->slug ?? null]) }}" class="d-flex flex-column text-center pb-2">
+                    @if(auth()->check())
+                        <a class="position-absolute mr-2" href="{{ url('').'/admin/applications/'.$application->id.'/edit?lang='.app()->getLocale() }}" target="_blank">Edit</a>
+                    @endif
 
-                                @if($category->getCoverPhotoAttribute() !== null)
-                                    <figure class="category_image">
-                                        <img
-                                                srcset="{{ $category->getCoverPhotoAttribute()->getUrl() }}"
-                                                alt="{{ $category->name }}"
-                                                title="{{ $category->name }}"
-                                                class="img-hover lozad img-fluid lazy-fade">
-                                    </figure>
-                                @else
-                                    <div class="category_image_default">No image</div>
-                                @endif
-                                <h2 class="h5 assets-title row-icons--desc px-2 mt-0">{{ $category->name }}</h2>
-                            </a>
-                        </li>
-                    @endforeach
-                </ul>
+                    @if($application->title)
+                        <h1>{{ $application->title }}</h1>
+                        @if( $application->subtitle )
+                            <p class="subtitle">{{ $application->subtitle }}</p>
+                        @endif
+                    @else
+                        <h1>{{ $application->name }}</h1>
+                        <p class="subtitle">{{ trans('pages.application') }}</p>
+                    @endif
+                    <hr>
 
-                <div class="py-4">
-                    {!! $application->content !!}
+                    <ul class="list-unstyled row justify-content-start full-row-prod assets-row mt-4 grid">
+                        @foreach($categories as $category)
+                            <li class="col-12 col-sm-6 col-md-4 py-2">
+                                <a href="{{ route('category.'.app()->getLocale(), ['app_slug' => $application->slug ?? null,'cat_slug' => $category->slug ?? null]) }}" class="d-flex flex-column text-center pb-2">
+
+                                    @if($category->getCoverPhotoAttribute() !== null)
+                                        <figure class="category_image">
+                                            <img
+                                                    srcset="{{ $category->getCoverPhotoAttribute()->getUrl() }}"
+                                                    alt="{{ $category->name }}"
+                                                    title="{{ $category->name }}"
+                                                    class="img-hover lozad img-fluid lazy-fade">
+                                        </figure>
+                                    @else
+                                        <div class="category_image_default">No image</div>
+                                    @endif
+                                    <h2 class="h5 assets-title row-icons--desc px-2 mt-0">{{ $category->name }}</h2>
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+
+                    <div class="py-4">
+                        {!! $application->content !!}
+                    </div>
                 </div>
+
+                @if($application->call_to_action)
+                    <div class="w-100 py-4">
+                        <p class="text-center p-4 my-4">
+                            <a href="{{ url('') . '/' . app()->getLocale() . '/' . $application->call_to_action_link }}" target="_blank" class="btn btn-primary btn-lg py-2 px-4 mx-auto">{{ $application->call_to_action }}</a>
+                        </p>
+                    </div>
+                @endif
             </div>
 
-            @if($application->call_to_action)
-                <div class="w-100 py-4">
-                    <p class="text-center p-4 my-4">
-                        <a href="{{ url('') . '/' . app()->getLocale() . '/' . $application->call_to_action_link }}" target="_blank" class="btn btn-primary btn-lg py-2 px-4 mx-auto">{{ $application->call_to_action }}</a>
-                    </p>
-                </div>
+            @if(auth()->check())
+                <a class="position-absolute mr-2" href="{{ url('').'/admin/applications/'.$application->id.'/edit?lang='.app()->getLocale() }}" target="_blank">Edit</a>
+                <br>
+                <br>
             @endif
         </div>
     </section>
