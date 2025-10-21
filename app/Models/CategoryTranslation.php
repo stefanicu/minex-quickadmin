8d27a3c4bf9bf66d5d6d7696ad1e5ad2333d5e9f
Stefan Nicula <stefanicu@gmail.com>
2025-10-22 01:03:59 +0300

    Update CategoryTranslation.php

--- app/Models/CategoryTranslation.php
+++ app/Models/CategoryTranslation.php
@@ -9,7 +9,20 @@ class CategoryTranslation extends Model
     public $timestamps = false;
     
     protected $fillable = [
-        'online', 'name', 'slug', 'content', 'call_to_action', 'call_to_action_link',
+        'online', 'name', 'slug', 'title', 'subtitle', 'content', 'call_to_action', 'call_to_action_link',
         'meta_title', 'meta_description', 'author', 'robots', 'canonical_url'
+    ];
+    
+    protected $indexes = [
+        'category_id',
+        'name',
+        'online',
+    ];
+    
+    public static $searchable = [
+        'name',
+        'slug',
+        'title',
+        'subtitle',
     ];
 }
