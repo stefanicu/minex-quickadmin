7bb3133de4d21d3f1d647bcef79996ac377ff2e2
Stefan Nicula <stefanicu@gmail.com>
2026-01-09 01:26:40 +0200

    Update edit.blade.php

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -453,16 +453,7 @@
             var allEditors = document.querySelectorAll('.ckeditor');
             for (var i = 0; i < allEditors.length; ++i) {
                 ClassicEditor.create(allEditors[i], {
-                    extraPlugins: [SimpleUploadAdapter],
-                    heading: {
-                        options: [
-                            {model: 'paragraph', title: 'Paragraf', class: 'ck-heading_paragraph'},
-                            {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
-                            {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
-                            {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
-                            {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'}
-                        ]
-                    }
+                    extraPlugins: [SimpleUploadAdapter]
                 });
             }
         });
