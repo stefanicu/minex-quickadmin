102118752a9968869ce277b70682b42b26ee05b6
Stefan Nicula <stefanicu@gmail.com>
2026-02-04 00:47:12 +0200

    555555

--- resources/views/admin/frontPages/index.blade.php
+++ resources/views/admin/frontPages/index.blade.php
@@ -3,7 +3,7 @@
 
     <div class="card">
         <div class="card-header">
-            {{ trans('cruds.frontPage.title_singular') }} {{ trans('global.list') }} 4444
+            {{ trans('cruds.frontPage.title_singular') }} {{ trans('global.list') }} 555555
         </div>
 
         <div class="card-body">
