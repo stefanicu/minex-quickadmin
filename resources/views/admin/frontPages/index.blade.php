c941ed31f1f529bc1b9839b1ab75cb8fa3c4df6a
Stefan Nicula <stefanicu@gmail.com>
2026-02-03 17:20:08 +0200

    Update index.blade.php

--- resources/views/admin/frontPages/index.blade.php
+++ resources/views/admin/frontPages/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.admin_home')
 @section('content')
 
-<div class="card">
-    <div class="card-header">
-        {{ trans('cruds.frontPage.title_singular') }} {{ trans('global.list') }}
-    </div>
+    <div class="card">
+        <div class="card-header">
+            {{ trans('cruds.frontPage.title_singular') }} {{ trans('global.list') }} --- git actions auto deploy
+        </div>
 
-    <div class="card-body">
-        <table class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-FrontPage">
-            <thead>
+        <div class="card-body">
+            <table class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-FrontPage">
+                <thead>
                 <tr>
                     <th>
                         {{ trans('cruds.frontPage.fields.id') }}
@@ -20,40 +20,44 @@
                         &nbsp;
                     </th>
                 </tr>
-            </thead>
-        </table>
+                </thead>
+            </table>
+        </div>
     </div>
-</div>
-
-
 
 @endsection
 @section('scripts')
-@parent
-<script>
-    $(function () {
-        let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-        let dtOverrideGlobals = {
-            buttons: dtButtons,
-            stateSave: true,
-            processing: true,
-            serverSide: true,
-            retrieve: true,
-            aaSorting: [],
-            ajax: "{{ route('admin.front_pages.index') }}",
-            columns: [
-                { data: 'id', name: 'front_pages.id' },
-                { data: 'name', name: 'front_page_translations.name' },
-                { data: 'actions', name: '{{ trans('global.actions') }}', class: 'text-nowrap text-center', sortable: false, searchable: false  }
-            ],
-            orderCellsTop: true,
-            order: [[ 0, 'asc' ]],
-            pageLength: 25,
-        };
-        let table = $('.datatable-FrontPage').DataTable(dtOverrideGlobals);
-        $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
-            $($.fn.dataTable.tables(true)).DataTable().columns.adjust();
+    @parent
+    <script>
+        $(function () {
+            let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+            let dtOverrideGlobals = {
+                buttons: dtButtons,
+                stateSave: true,
+                processing: true,
+                serverSide: true,
+                retrieve: true,
+                aaSorting: [],
+                ajax: "{{ route('admin.front_pages.index') }}",
+                columns: [
+                    {data: 'id', name: 'front_pages.id'},
+                    {data: 'name', name: 'front_page_translations.name'},
+                    {
+                        data: 'actions',
+                        name: '{{ trans('global.actions') }}',
+                        class: 'text-nowrap text-center',
+                        sortable: false,
+                        searchable: false
+                    }
+                ],
+                orderCellsTop: true,
+                order: [[0, 'asc']],
+                pageLength: 25,
+            };
+            let table = $('.datatable-FrontPage').DataTable(dtOverrideGlobals);
+            $('a[data-toggle="tab"]').on('shown.bs.tab click', function (e) {
+                $($.fn.dataTable.tables(true)).DataTable().columns.adjust();
+            });
         });
-    });
-</script>
+    </script>
 @endsection
