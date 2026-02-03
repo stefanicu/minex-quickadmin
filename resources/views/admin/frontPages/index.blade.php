473c5013324dd15507db4d96025e9182e67bb96b
Stefan Nicula <stefanicu@gmail.com>
2026-02-03 19:18:53 +0200

    Update index.blade.php

--- resources/views/admin/frontPages/index.blade.php
+++ resources/views/admin/frontPages/index.blade.php
@@ -3,7 +3,7 @@
 
     <div class="card">
         <div class="card-header">
-            {{ trans('cruds.frontPage.title_singular') }} {{ trans('global.list') }} 6
+            {{ trans('cruds.frontPage.title_singular') }} {{ trans('global.list') }} 564654651951951
         </div>
 
         <div class="card-body">
