6148e642167bbce56d24e2b5c3a706c296f615f6
Stefan Nicula <stefanicu@gmail.com>
2026-02-03 22:54:40 +0200

    Update index.blade.php

--- resources/views/admin/frontPages/index.blade.php
+++ resources/views/admin/frontPages/index.blade.php
@@ -3,7 +3,7 @@
 
     <div class="card">
         <div class="card-header">
-            {{ trans('cruds.frontPage.title_singular') }} {{ trans('global.list') }} 007 --- SFTP
+            {{ trans('cruds.frontPage.title_singular') }} {{ trans('global.list') }} 007 --- SFTP ??????
         </div>
 
         <div class="card-body">
