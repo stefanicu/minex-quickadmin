97acdfa286d1265cdf27887a1a84e71a2aee9110
Stefan Nicula <stefanicu@gmail.com>
2026-02-03 18:29:59 +0200

    Update index.blade.php

--- resources/views/admin/frontPages/index.blade.php
+++ resources/views/admin/frontPages/index.blade.php
@@ -3,7 +3,7 @@
 
     <div class="card">
         <div class="card-header">
-            {{ trans('cruds.frontPage.title_singular') }} {{ trans('global.list') }} --- deploy NOW AGAIN
+            {{ trans('cruds.frontPage.title_singular') }} {{ trans('global.list') }} 2
         </div>
 
         <div class="card-body">
