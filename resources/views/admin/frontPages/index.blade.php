b173991ab98f149591a4fe4ef511f3e1c44b6d8c
Stefan Nicula <stefanicu@gmail.com>
2026-02-03 17:42:39 +0200

    Update index.blade.php

--- resources/views/admin/frontPages/index.blade.php
+++ resources/views/admin/frontPages/index.blade.php
@@ -3,7 +3,7 @@
 
     <div class="card">
         <div class="card-header">
-            {{ trans('cruds.frontPage.title_singular') }} {{ trans('global.list') }} --- 1. git actions auto deploy
+            {{ trans('cruds.frontPage.title_singular') }} {{ trans('global.list') }} --- 13333. git actions auto deploy
         </div>
 
         <div class="card-body">
